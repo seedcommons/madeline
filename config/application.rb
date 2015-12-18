f833987edbc008ede924c1454315337747eb9244
Melody <meltheadorable@gmail.com>
2015-12-18 17:19:04 -0500

    Adds lib to autoload paths

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,9 @@ module MadelineSystem
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # Additional paths to autoload
+    config.autoload_paths << "#{Rails.root}/lib"
+    
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
