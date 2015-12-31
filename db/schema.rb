8a0239d90d727bbac483cfdcbe7ddc63565ea267
Melody <meltheadorable@gmail.com>
2015-12-31 13:22:38 -0500

    3760: Adds person model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151231162855) do
+ActiveRecord::Schema.define(version: 20151231180615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,14 @@ ActiveRecord::Schema.define(version: 20151231162855) do
     t.datetime "updated_at",      null: false
   end
 
+  create_table "people", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "primary_organization_id"
+    t.date     "birth_date"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -75,4 +83,5 @@ ActiveRecord::Schema.define(version: 20151231162855) do
 
   add_foreign_key "divisions", "currencies"
   add_foreign_key "divisions", "organizations"
+  add_foreign_key "people", "organizations", column: "primary_organization_id"
 end
