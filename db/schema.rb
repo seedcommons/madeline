ba3e603b3fffa6544d2f4fcbbcbf833887c09a55
Melody <meltheadorable@gmail.com>
2015-12-31 14:58:47 -0500

    3760: Updates loan model dependencies
    * Adds countries
    * Cleans up specs
    * Cleans up loan query in controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151231182601) do
+ActiveRecord::Schema.define(version: 20151231192233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "countries", force: :cascade do |t|
+    t.string   "iso_code",            limit: 2
+    t.integer  "default_currency_id"
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+  end
 
   create_table "currencies", force: :cascade do |t|
     t.string   "code"
@@ -110,6 +117,7 @@ ActiveRecord::Schema.define(version: 20151231182601) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "countries", "currencies", column: "default_currency_id"
   add_foreign_key "divisions", "currencies"
   add_foreign_key "divisions", "organizations"
   add_foreign_key "loans", "currencies"
