238087a3dd5e6dbb70e360ccf55d1ed57c71d60c
Elizabeth Mitchell <elizabeth@sassafras.coop>
2016-01-06 17:39:36 -0500

    2213: Restructure scripts location

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( embedded/* )
 Rails.application.config.assets.precompile += %w( admin/* )
+Rails.application.config.assets.precompile += %w( admin/*/* )
