c0e9cd280b5d26425284623376b3089b417f7f07
Elizabeth Mitchell <elizabeth@sassafras.coop>
2016-01-07 15:13:51 -0500

    3742: Make devise template DRY

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -265,6 +265,9 @@ Devise.setup do |config|
 end
 
 Rails.application.config.to_prepare do
-  Devise::SessionsController.layout "base"
-  Devise::PasswordsController.layout "base"
+  Devise::SessionsController.layout "devise"
+  Devise::RegistrationsController.layout proc{ |controller| user_signed_in? ? "application" : "devise" }
+  Devise::ConfirmationsController.layout "devise"
+  Devise::UnlocksController.layout "devise"
+  Devise::PasswordsController.layout "devise"
 end
