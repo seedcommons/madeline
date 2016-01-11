101dc350fd8197833fda0e813126debdc842326b
Elizabeth Mitchell <elizabeth@sassafras.coop>
2016-01-11 17:43:38 -0500

    3748: Add column filters

--- app/controllers/admin/coops_controller.rb
+++ app/controllers/admin/coops_controller.rb
@@ -1,5 +1,9 @@
 class Admin::CoopsController < Admin::AdminController
   def index
-    @organizations_grid = initialize_grid(Organization)
+    @organizations_grid = initialize_grid(
+      Organization,
+      include: :country,
+      order: 'name'
+    )
   end
 end
