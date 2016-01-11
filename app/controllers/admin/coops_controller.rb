817ea3e0270a631198edecc27c309548dfa919db
Elizabeth Mitchell <elizabeth@sassafras.coop>
2016-01-11 13:22:42 -0500

    3748: Add placeholder page

---
+++ app/controllers/admin/coops_controller.rb
@@ -0,0 +1,4 @@
+class Admin::CoopsController < Admin::AdminController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :dashboard, only: [:index]
+    resources :coops, only: [:index]
   end
 
   localized do
