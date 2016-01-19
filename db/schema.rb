5dc10754cf86be13d861d0e137546191810b1c03
Adam Konner <adam.konner@gmail.com>
2016-01-18 22:29:45 -0500

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160110190559) do
+ActiveRecord::Schema.define(version: 20160119014032) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -239,6 +239,7 @@ ActiveRecord::Schema.define(version: 20160110190559) do
     t.string "encrypted_password", default: "", null: false
     t.datetime "last_sign_in_at"
     t.inet "last_sign_in_ip"
+    t.integer "profile_id"
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
@@ -247,6 +248,7 @@ ActiveRecord::Schema.define(version: 20160110190559) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["profile_id"], name: "index_users_on_profile_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "countries", "currencies", column: "default_currency_id"
@@ -269,4 +271,5 @@ ActiveRecord::Schema.define(version: 20160110190559) do
   add_foreign_key "project_logs", "people", column: "agent_id"
   add_foreign_key "project_logs", "project_steps"
   add_foreign_key "project_steps", "people", column: "agent_id"
+  add_foreign_key "users", "people", column: "profile_id"
 end
