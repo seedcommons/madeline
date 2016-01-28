0c9ea3d100409e76d1a1966a63d1b95a632eb9de
Adam Konner <adam.konner@gmail.com>
2016-01-27 19:44:18 -0500

    3754: form action urls

--- app/controllers/admin/organizations_controller.rb
+++ app/controllers/admin/organizations_controller.rb
@@ -11,13 +11,13 @@ class Admin::OrganizationsController < Admin::AdminController
   def show
     @org = Organization.find(params[:id])
     @countries = Country.all
-    @form_url = admin_organization_path
+    @form_action_url = admin_organization_path
   end
 
   def new
     @org = Organization.new
     @countries = Country.all
-    @form_url = new_admin_organization_path
+    @form_action_url = admin_organizations_path
   end
 
   def update
@@ -25,9 +25,8 @@ class Admin::OrganizationsController < Admin::AdminController
     if @org.update(organization_params)
       redirect_to admin_organization_path(@org), notice: "Record updated."
     else
-      # show
       @countries = Country.all
-      @form_url = admin_organization_path
+      @form_action_url = admin_organization_path
       render :show
     end
   end
