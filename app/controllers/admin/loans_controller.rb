ef47a39321f32992828e6f70476171626ca8df55
Elizabeth Mitchell <elizabeth@sassafras.coop>
2016-02-01 13:39:14 -0500

    3983: Ensure loan status is translatable

--- app/controllers/admin/loans_controller.rb
+++ app/controllers/admin/loans_controller.rb
@@ -10,6 +10,7 @@ class Admin::LoansController < Admin::AdminController
       # "loan.name" and "loan[:name]" currently return different, unrelated content.
       # Should return same content.
       # By default wice grid uses "object.option" and not "object[:option]"
+      # Difference may be related to use of FactoryGirl
 
     # TODO: Loan type is currently nil
   end
