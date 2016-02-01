21d5e614927483dda71323b6b92bcaa9a83e3b21
Elizabeth Mitchell <elizabeth@sassafras.coop>
2016-02-01 13:25:57 -0500

    3983: Note parts of grid that are not loading properly

--- app/controllers/admin/loans_controller.rb
+++ app/controllers/admin/loans_controller.rb
@@ -1,5 +1,16 @@
 class Admin::LoansController < Admin::AdminController
   def index
     @loans_grid = initialize_grid(Loan, include: [:division, :organization])
+
+    # TODO:
+      # Only load coops/organizations with loans into loan grid
+      # Filter in view should only show coops with loans
+
+    # TODO:
+      # "loan.name" and "loan[:name]" currently return different, unrelated content.
+      # Should return same content.
+      # By default wice grid uses "object.option" and not "object[:option]"
+
+    # TODO: Loan type is currently nil
   end
 end
