a4785f79f96c9df1325c68d5b0741ffbfe63d453
Elizabeth Mitchell <elizabeth@sassafras.coop>
2016-02-23 13:23:28 -0500

    3763: Include confirm popup in admin layout
    - Revise title of popup to be title of page.

---
+++ app/assets/javascripts/admin/confirm.js
@@ -0,0 +1,9 @@
+$.fn.twitter_bootstrap_confirmbox.defaults = {
+  fade: false,
+  title: window.document.title,
+  cancel: "Cancel",
+  cancel_class: "btn cancel",
+  proceed: "OK",
+  proceed_class: "btn proceed btn-primary",
+  modal_class: ""
+};
