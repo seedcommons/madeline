7b90f0508a0afb5229cf6019563f444ab22741cb
elliottjf <elliottj@pobox.com>
2016-03-03 06:47:07 -0800

    3764 - fix translatable unit tests

--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -19,11 +19,6 @@ FactoryGirl.define do
   factory :note do
     association :notable, factory: :organization
     association :author, factory: :person
-
-    # for now parent must be saved before assigning the text
-    # beware, this currently depends on a Language instance having already been created as a side-effect from other associations
-    after(:create) do |note|
-      note.set_text(Faker::Lorem.sentence)
-    end
+    text { Faker::Lorem.sentence }
   end
 end
