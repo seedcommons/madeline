89cbf5b70d2235e2ead39fb0c0dc8795cd974d3d
elliottjf <elliottj@pobox.com>
2016-03-04 01:46:15 -0800

    3738 - media migration - add foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -330,6 +330,7 @@ ActiveRecord::Schema.define(version: 20160205165630) do
   add_foreign_key "loans", "people", column: "primary_agent_id"
   add_foreign_key "loans", "people", column: "representative_id"
   add_foreign_key "loans", "people", column: "secondary_agent_id"
+  add_foreign_key "media", "people", column: "uploader_id"
   add_foreign_key "option_sets", "divisions"
   add_foreign_key "options", "option_sets"
   add_foreign_key "organizations", "countries"
