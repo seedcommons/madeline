5b6840d44afa52c7a1620e89e5b6134b01185983
Elizabeth Mitchell <elizabeth@sassafras.coop>
2016-03-09 09:58:42 -0500

    3766: Add batch actions in dropdown

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
     resources :organizations
     resources :project_steps
 
+    get 'project_logs/finalize', to: 'project_steps#finalize'
+    get 'project_logs/adjust_dates', to: 'project_steps#adjust_dates'
+
     namespace :raw do
       resources :divisions
       resources :loans
