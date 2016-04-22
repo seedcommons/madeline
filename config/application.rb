d9f1cbb4d80ee5ddbfb340442cca5b0aab95468c
Tom Smyth <tom@sassafras.coop>
2016-04-22 15:50:49 -0400

    3768: Reorganized locales dir

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module MadelineSystem
     config.time_zone = 'Eastern Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir["#{Rails.root.to_s}/config/locales/**/*.{rb,yml}"]
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
