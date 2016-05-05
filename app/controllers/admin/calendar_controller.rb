90eb05b3e2aad07a62dffd9d04960daaa8da8131
elliottjf <elliottj@pobox.com>
2016-05-05 11:53:24 -0700

    3772 - calender event code cleanups

--- app/controllers/admin/calendar_controller.rb
+++ app/controllers/admin/calendar_controller.rb
@@ -3,8 +3,12 @@ class Admin::CalendarController < Admin::AdminController
     @division = current_division
     authorize @division
 
-    #JE Todo: Confirm if we want to support an 'all' view, or just the currently selected division.
-    @calendar_events_url = "/admin/calendar_events/division/#{@division.id}"
-  end
+    # This version of the events url is more RESTful and would support a stable calendar view
+    # context and support multiple calendar views being navigated from in different tabs.
+    # @calendar_events_url = "/admin/calendar_events/division/#{@division.id}"
 
+    # For now, simply honor the currently selected division even if it changes after calendar view
+    # first loaded.
+    @calendar_events_url = "/admin/calendar_events"
+  end
 end
--- app/controllers/admin/calendar_events_controller.rb
+++ app/controllers/admin/calendar_events_controller.rb
@@ -21,43 +21,17 @@ class Admin::CalendarEventsController < Admin::AdminController
   end
 
   def render_for_loan_filter(date_range, loan_filter)
-    puts "loan filter: #{loan_filter}, date range: #{date_range}" #JE Todo: remove
-
-    # events = CalendarEvent.by_loan_date_filter(date_range, loan_policy_scope(Loan.where(filter)))
-    # events += CalendarEvent.by_project_step_date_loan_filter(date_range, filter)
-    #
-    # puts "before select event count: #{events.count}" #JE Todo: remove
-    # # Filter out sibling events outside of our range
-    # events.select!{ |event| date_range === event.start }
-    # puts "after select event count: #{events.count}, first: #{events.first.inspect}" #JE Todo: remove
-
     # JE Todo: apply project_step_scope scope
     events = CalendarEvent.filtered_events(date_range, loan_filter, loan_policy_scope(Loan), ProjectStep)
     events.each{ |event| event.html = render_event(event) }
     render json: events
   end
 
-  # def loan_policy_scope(scope)
-  #   LoanPolicy::Scope.new(current_user, scope).resolve
-  # end
-
-
-  # def filtered_events(loan_filter, date_range)
-  # def loan_filter_with_dates(loan_filter, date_range)
-  #   CalendarEvent.loan_date_filter(date_range).where(loan_filter)
-  # end
-
-  # def steps_by_loan_filter_dates(loan_filter, date_range)
-  #   project_steps = CalendarEvent.project_step_date_filter(date_range).
-  #     where(project_type: 'Loan', project_id: Loan.where(loan_filter).pluck(:id))
-  # end
-
   def date_range(params)
     start_date = date_param(params[:start], Date.today.beginning_of_month)
     end_date = date_param(params[:end], Date.today.end_of_month)
     start_date..end_date
   end
-
 
   def date_param(date_string, default = nil)
     date_string ? Date.iso8601(date_string) : default
