9f540733a07a0b0c8e0c0ef38ecf2ba433cec88e
elliottjf <elliottj@pobox.com>
2016-05-17 17:32:11 -0700

    4507 - git rid of es-AR

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,6 +1,6 @@
 # note, 'es' is needed here to support leveraging the I18n system to translate our locale_name for the
 # language selection drop-down
-Rails.application.config.i18n.available_locales = %w(en es es-AR fr)
+Rails.application.config.i18n.available_locales = %w(en es fr)
 RouteTranslator.config do |config|
   config.disable_fallback = true
 end
