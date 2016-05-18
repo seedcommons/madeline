030dcda945ba7503aaab55589893c56223dc4446
elliottjf <elliottj@pobox.com>
2016-05-18 09:56:13 -0700

    4534 - active loans & members on co-op view

--- app/controllers/admin/organizations_controller.rb
+++ app/controllers/admin/organizations_controller.rb
@@ -90,5 +90,20 @@ class Admin::OrganizationsController < Admin::AdminController
     @countries = Country.all
     @division_choices = division_choices
     @contact_choices = @org.people
+
+    @people_grid = initialize_grid(
+      @org.people,
+      order: 'name',
+      name: 'people',
+      per_page: 10
+    )
+
+    @loans_grid = initialize_grid(
+      @org.active_loans,
+      order: 'loans.signing_date',
+      order_direction: 'desc',
+      name: 'loans',
+      per_page: 10
+    )
   end
 end
