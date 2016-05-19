15b3c34abf08e958936a9d037618c55f77a382e2
elliottjf <elliottj@pobox.com>
2016-05-19 13:08:24 -0700

    3776 - unit test updates for person and person policy

--- spec/factories/people.rb
+++ spec/factories/people.rb
@@ -35,6 +35,7 @@ FactoryGirl.define do
     division { root_division }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
     primary_organization { root_division.organization }
     birth_date { Faker::Date.between(100.years.ago, 18.years.ago) }
   end
@@ -45,5 +46,19 @@ FactoryGirl.define do
     end
   end
 
+  trait :with_member_access do
+    has_system_access true
+    owning_division_role :member
+  end
+
+  trait :with_admin_access do
+    has_system_access true
+    owning_division_role :admin
+  end
+
+  trait :with_password do
+    password 'xxxxxxxx'
+    password_confirmation 'xxxxxxxx'
+  end
 
 end
