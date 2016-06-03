2b127fb91b643ec070cfc4b75c4882b637f46ce0
elliottjf <elliottj@pobox.com>
2016-06-02 21:59:37 -0700

    4571 - factor out authorized_form_field header-with-links style code review cleanups

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -3,4 +3,26 @@ module AdminHelper
     # Todo: Confirm desired sort order.
     [['All', nil]].concat(current_user.accessible_divisions.reject(&:root?).map{ |d| [d.name, d.id] })
   end
+
+
+  def authorized_form_field(simple_form: nil, model: nil, field_name: nil, choices: nil)
+    model_field = model.send(field_name)
+    if model_field
+      policy = "#{model_field.class.name}Policy".constantize.new(current_user, model_field)
+      link_path = url_for([:admin, model_field])
+    end
+
+    render partial: 'admin/common/authorized_form_field', locals: {
+      f: simple_form,
+      model_field: model_field,
+      field_name: field_name,
+      id_field_name: "#{field_name}_id",
+      link_path: link_path,
+      choices: choices,
+      may_show_link: model_field && policy.show?,
+      # Beware, if the 'may_edit' logic changes and might be false even with a non-nil model_field,
+      # then the paratial code will also need updating to make nil safe.
+      may_edit: !model_field || policy.show?
+    }
+  end
 end
