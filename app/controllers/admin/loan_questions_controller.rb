3ca6a11fb12046af0dbb7a75d7dc5b23e00e8532
Adam Konner <linesarefuzzy@gmail.com>
2016-06-08 15:45:10 -0400

    3864: display questions in tree

--- app/controllers/admin/loan_questions_controller.rb
+++ app/controllers/admin/loan_questions_controller.rb
@@ -1,8 +1,9 @@
 class Admin::LoanQuestionsController < Admin::AdminController
   def index
     authorize CustomFieldSet
-    field_sets = CustomFieldSet.where(internal_name: ['loan_criteria', 'loan_post_analysis'])
-    @questions = field_sets.map(&:custom_fields).flatten
-    @json = ActiveModel::ArraySerializer.new(@questions, each_serializer: LoanQuestionSerializer).to_json
+    @questions = CustomField
+        .joins(:custom_field_set)
+        .where(custom_field_sets: {internal_name: ['loan_criteria', 'loan_post_analysis']})
+    @json = ActiveModel::Serializer::CollectionSerializer.new(@questions.roots).to_json
   end
 end
--- app/serializers/custom_field_serializer.rb
+++ app/serializers/custom_field_serializer.rb
@@ -1,7 +1,15 @@
 class CustomFieldSerializer < ActiveModel::Serializer
-  attributes :name
+  attributes :name, :children
 
   def name
     object.label.to_s
+  end
+
+  # jqtree expects children in a `children` key
+  def children
+    if object.children.present?
+      # Recursively apply this serializer to children
+      object.children.map { |node| self.class.new(node) }
+    end
   end
 end
