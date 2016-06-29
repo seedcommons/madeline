93336061fb20ab99df78fe4273425a78e681dd30
Tom Smyth <tom@sassafras.coop>
2016-06-29 15:52:39 -0400

    4301: Removed unused custom set validation rendering code

--- app/controllers/admin/custom_value_sets_controller.rb
+++ app/controllers/admin/custom_value_sets_controller.rb
@@ -1,55 +1,23 @@
 class Admin::CustomValueSetsController < Admin::AdminController
-  # show view includes edit
-  def show
-    @record = CustomValueSet.find(params[:id])
+  def create
+    @record = CustomValueSet.new(record_params)
     authorize @record
-  end
-
-  def new
-    raise "'linkable_type' param required" unless params[:linkable_type].present?
-    raise "'linkable_id' param required" unless params[:linkable_id].present?
-    raise "'linkable_attribute' param required" unless params[:linkable_attribute].present?
-    raise "'field_set_name' param required" unless params[:field_set_name].present?
-
-    linkable = resolve_polymorphic(params[:linkable_type], params[:linkable_id])
-    field_set = CustomFieldSet.find_by(internal_name: params[:field_set_name])
-    linkable_attribute = params[:linkable_attribute]
-    @record = CustomValueSet.new(custom_value_set_linkable: linkable, custom_field_set: field_set,
-      linkable_attribute: linkable_attribute)
-    authorize @record
+    @record.save
+    redirect_to display_path, notice: I18n.t(:notice_created)
   end
 
   def update
     @record = CustomValueSet.find(params[:id])
     authorize @record
-
-    if @record.update(record_params)
-      redirect_to display_path, notice: I18n.t(:notice_updated)
-    else
-      render :show
-    end
-  end
-
-  def create
-    @record = CustomValueSet.new(record_params)
-    authorize @record
-
-    if @record.save
-      redirect_to display_path, notice: I18n.t(:notice_created)
-    else
-      render :new
-    end
+    @record.update(record_params)
+    redirect_to display_path, notice: I18n.t(:notice_updated)
   end
 
   def destroy
     @record = CustomValueSet.find(params[:id])
     authorize @record
-
-    if @record.destroy
-      redirect_to display_path, notice: I18n.t(:notice_deleted)
-    else
-      render :show
-    end
+    @record.destroy
+    redirect_to display_path, notice: I18n.t(:notice_deleted)
   end
 
   private
@@ -68,13 +36,5 @@ class Admin::CustomValueSetsController < Admin::AdminController
 
   def display_path
     admin_loan_path(@record.custom_value_set_linkable) + "#questionnaires"
-
-    # This version will rerender as a top level.  Useful for debugging
-    # if record.custom_value_set_linkable.is_a?(Loan)
-    #   admin_loan_path(record.custom_value_set_linkable)
-    # else
-    #   raise "Unexpected custom_value_set_linkable: #{record.custom_value_set_linkable}"
-    # end
   end
-
 end
