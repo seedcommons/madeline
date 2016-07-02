95e4916e19e0dcd7f2233d1b700a979c7549b37b
Adam Konner <linesarefuzzy@gmail.com>
2016-07-02 16:12:18 -0400

    Fix bug with internal names of custom value sets
    ‘criteria’ had to be changed back to ‘loan_criteria’ in the db in order
    to work symmetrically with ‘loan_post_analysis’. It is still
    referenceable as ‘criteria’ in Rails.

--- app/models/legacy/loan_response_set.rb
+++ app/models/legacy/loan_response_set.rb
@@ -39,12 +39,10 @@ module Legacy
         puts "response id: #{response.id} - question id: #{response.question_id}"
         field = CustomField.find_by(id: response.question_id)
         if field
-          active_code = response.loan_question_active
-          attribute_name = LOAN_QUESTION_ACTIVE_TO_ATTRIBUTE_NAME[active_code]
           puts "question_id: #{response.question_id} - set: #{field.custom_field_set.internal_name}, attr: #{attribute_name}"
           # model = new_loan.fetch_belongs_to_custom(attribute_name, field_set_name: field.custom_field_set.internal_name,
           #                                          owner: new_loan.organization, autocreate: true)
-          model = new_loan.fetch_has_one_custom(attribute_name, field_set_name: field.custom_field_set.internal_name, autocreate: true)
+          model = new_loan.fetch_has_one_custom(field.custom_field_set.internal_name, autocreate: true)
           # note, could be optimized by building entire json blob and storing as a single operations, but this seems fast enough
           puts "update: #{field.id} -> #{response.value_hash}"
           model.update_custom_value(field.id, response.value_hash)
@@ -77,9 +75,6 @@ module Legacy
     #     puts "WARNING - linked loan not found: #{linked_loan_id}"
     #   end
     # end
-
-
-    LOAN_QUESTION_ACTIVE_TO_ATTRIBUTE_NAME = { 1 => :old_criteria, 2 => :criteria, 3 => :post_analysis }
 
   end
 
