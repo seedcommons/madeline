10d4e80b995a3f33a6e0be8a665aaaef65bbc95c
Adam Konner <linesarefuzzy@gmail.com>
2016-07-02 10:09:39 -0400

    3864: sort data types in menu

--- app/controllers/admin/loan_questions_controller.rb
+++ app/controllers/admin/loan_questions_controller.rb
@@ -15,11 +15,11 @@ class Admin::LoanQuestionsController < Admin::AdminController
     field_set = CustomFieldSet.find_by(internal_name: 'loan_' + field_set_name)
     @loan_question = field_set.custom_fields.build
     authorize @loan_question
-    render partial: 'form'
+    render_form
   end
 
   def edit
-    render partial: 'form'
+    render_form
   end
 
   def create
@@ -28,7 +28,7 @@ class Admin::LoanQuestionsController < Admin::AdminController
     if @loan_question.save
       render json: @loan_question.reload
     else
-      render partial: 'form', status: :unprocessable_entity
+      render_form(status: :unprocessable_entity)
     end
   end
 
@@ -36,7 +36,7 @@ class Admin::LoanQuestionsController < Admin::AdminController
     if @loan_question.update(loan_question_params)
       render json: @loan_question.reload
     else
-      render partial: 'form', status: :unprocessable_entity
+      render_form(status: :unprocessable_entity)
     end
   end
 
@@ -74,5 +74,16 @@ class Admin::LoanQuestionsController < Admin::AdminController
     def loan_question_params
       params.require(:custom_field).permit(:label, :data_type, :parent_id, :position,
         :custom_field_set_id, :has_embeddable_media, *translation_params(:label, :explanation))
+    end
+
+    def render_form(status: nil)
+      @data_types = CustomField::DATA_TYPES.map do |i| 
+        [I18n.t("simple_form.options.custom_field.data_type.#{i}"), i]
+      end.sort
+      if status
+        render partial: 'form', status: :status
+      else
+        render partial: 'form'
+      end
     end
 end
