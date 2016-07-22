f11fad74b65ba3763a6e68d34db2a8d9947e04b3
elliottjf <elliottj@pobox.com>
2016-07-22 07:38:22 -0700

    4744 - restore to work for old migrated data.

--- app/controllers/admin/embeddable_media_controller.rb
+++ app/controllers/admin/embeddable_media_controller.rb
@@ -5,7 +5,8 @@ class Admin::EmbeddableMediaController < Admin::AdminController
     owner_id = params[:owner_id]
     owner_attribute = params[:owner_attribute]
     @record = EmbeddableMedia.new(owner_type: owner_type, owner_id: owner_id, owner_attribute: owner_attribute)
-    authorize @record
+    # need this until migration updated and all developers have newly migrated data
+    handle_authorize
     # Note, was getting "undefined method `admin_embeddable_media_index_path'" error if record
     # wasn't saved before rendering form.  Some wackiness related to 'media' pluralization handling?
     @record.save
@@ -25,7 +26,7 @@ class Admin::EmbeddableMediaController < Admin::AdminController
 
   def edit
     @record = EmbeddableMedia.find(params[:id])
-    authorize @record
+    handle_authorize
     # Note, this can be removed once migration logic is updated and we can assume everybody
     # is working with clean data.
     @record.ensure_migration
@@ -34,7 +35,7 @@ class Admin::EmbeddableMediaController < Admin::AdminController
 
   def update
     @record = EmbeddableMedia.find(params[:id])
-    authorize @record
+    handle_authorize
     @record.assign_attributes(record_params)
     @record.parse_key_gid_from_original_url
     if @record.save
@@ -46,12 +47,21 @@ class Admin::EmbeddableMediaController < Admin::AdminController
 
   def destroy
     @record = EmbeddableMedia.find(params[:id])
-    authorize @record
+    handle_authorize
     @record.destroy!
     render plain: "success"
   end
 
   private
+
+  def handle_authorize
+    # need this until migration updated and all developers have newly migrated data
+    if @record.owner
+      authorize @record
+    else
+      skip_authorization
+    end
+  end
 
   def record_params
     params.require(:embeddable_media).permit(
