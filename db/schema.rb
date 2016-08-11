b4bfb7712d58954b84d67b7760561436dbb21dd4
elliottjf <elliottj@pobox.com>
2016-08-10 23:19:18 -0700

    4744 - drop embeddable media table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808025907) do
+ActiveRecord::Schema.define(version: 20160811061305) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -94,16 +94,6 @@ ActiveRecord::Schema.define(version: 20160808025907) do
 
   add_index "divisions", ["currency_id"], name: "index_divisions_on_currency_id", using: :btree
   add_index "divisions", ["organization_id"], name: "index_divisions_on_organization_id", using: :btree
-
-  create_table "embeddable_media", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.integer  "height"
-    t.text     "html"
-    t.string   "original_url"
-    t.datetime "updated_at", null: false
-    t.string   "url"
-    t.integer  "width"
-  end
 
   create_table "loan_response_sets", force: :cascade do |t|
     t.datetime "created_at", null: false
