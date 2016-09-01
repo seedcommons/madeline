01a743da490786347fe7ef2e8b892589a4e8f89c
Adam Konner <linesarefuzzy@gmail.com>
2016-09-01 15:03:51 -0400

    4989: table calculations

--- app/models/breakeven_table_question.rb
+++ app/models/breakeven_table_question.rb
@@ -4,6 +4,38 @@ class BreakevenTableQuestion
   end
 
   def report
-    @breakeven_data
+    report = {
+      revenue: [],
+      total_revenue: 0,
+      cogs: [],
+      total_cogs: 0,
+      gross_margin: 0,
+      fixed_costs: [],
+      total_fixed_costs: 0,
+      net_margin: 0,
+    }
+
+    @breakeven_data['products'].each do |product|
+      report[:revenue] << {
+        name: product['name'],
+        quantity: product['quantity'],
+        amount: product['price'],
+        total: product['price'] * product['quantity'],
+      }
+      report[:cogs] << {
+        name: product['name'],
+        quantity: product['quantity'],
+        amount: product['cost'],
+        total: product['cost'] * product['quantity'],
+      }
+    end
+    report[:total_revenue] = report[:revenue].map { |i| i[:total] }.sum
+    report[:total_cogs] = report[:cogs].map { |i| i[:total] }.sum
+    report[:gross_margin] = report[:total_revenue] - report[:total_cogs]
+    report[:fixed_costs] = @breakeven_data['fixed_costs'].map(&:symbolize_keys)
+    report[:total_fixed_costs] = report[:fixed_costs].map { |i| i[:amount] }.sum
+    report[:net_margin] = report[:gross_margin] - report[:total_fixed_costs]
+
+    report
   end
 end
