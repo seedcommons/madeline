34bc1b8dd1134cb7009d714ab57c64c25ab05654
Adam Konner <linesarefuzzy@gmail.com>
2016-09-02 16:30:29 -0400

    4989: add comments to PORO

--- app/models/breakeven_table_question.rb
+++ app/models/breakeven_table_question.rb
@@ -1,8 +1,46 @@
+# Performs calculations necessary to produce Breakeven Table Report on loan questions
 class BreakevenTableQuestion
+  # Expects JSON input like this:
+  # {
+  #   "products": [
+  #     { "name": "Product 1", "description": "Description", "unit": "Widgets", "price": 100, "cost": 50, "quantity": 800 },
+  #     { "name": "Product 2", "description": "Description", "unit": "Stamp books", "price": 200, "cost": 75, "quantity": 1000 }
+  #   ],
+  #   "fixed_costs": [
+  #     { "name": "Rent", "amount": 15000 },
+  #     { "name": "Stamps", "amount": 2.25 }
+  #   ]
+  # }
   def initialize(breakeven_data)
     @breakeven_data = JSON.parse(breakeven_data) if breakeven_data.present?
   end
 
+  # Returns a ruby hash like this:
+  # {
+  #   revenue: [
+  #     { name: 'Product 1', quantity: 800, amount: 100, total: 80_000 },
+  #     { name: 'Product 2', quantity: 300, amount: 120, total: 36_000 },
+  #     { name: 'Product 3', quantity: 100, amount: 150, total: 15_000 },
+  #   ],
+  #   total_revenue: 131_000,
+  #   cogs: [
+  #     { name: 'Product 1', quantity: 800, amount: 50, total: 40_000 },
+  #     { name: 'Product 2', quantity: 300, amount: 60, total: 18_000 },
+  #     { name: 'Product 3', quantity: 100, amount: 70, total: 7_000 },
+  #   ],
+  #   total_cogs: 65_000,
+  #   gross_margin: 66_000,
+  #   fixed_costs: [
+  #     { name: "Rent", amount: 15_000 },
+  #     { name: "Worker owners", amount: 28_000 },
+  #     { name: "Employees", amount: 10_000 },
+  #     { name: "Sales", amount: 10_000 },
+  #     { name: "Utilities", amount: 2_000 },
+  #     { name: "Insurance", amount: 1_000 },
+  #   ],
+  #   total_fixed_costs: 66_000,
+  #   net_margin: 0,
+  # }
   def report
     report = {
       revenue: [],
