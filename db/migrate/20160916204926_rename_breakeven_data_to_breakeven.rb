98b7b524b4fb935db24ef16c338612b7bad03728
Adam Konner <linesarefuzzy@gmail.com>
2016-09-16 17:02:54 -0400

    5076: migration to rename breakeven in db

---
+++ db/migrate/20160916204926_rename_breakeven_data_to_breakeven.rb
@@ -0,0 +1,20 @@
+class RenameBreakevenDataToBreakeven < ActiveRecord::Migration
+  def change
+    cfs = CustomField.where(data_type: 'breakeven_data')
+    puts "CustomFields: #{cfs.inspect}"
+    cfs.update_all(data_type: 'breakeven')
+
+    cfs.each do |cf|
+      id = cf.id.to_s
+      lrss = LoanResponseSet.select { |i| i.custom_data.keys.include? id }
+      lrss.each do |lrs|
+        if lrs.custom_data[id].keys.include? 'breakeven_data'
+          puts "LoanResponseSet ##{lrs.id}: renaming"
+          lrs.custom_data[id]['breakeven'] = lrs.custom_data[id]['breakeven_data']
+          lrs.custom_data[id].delete 'breakeven_data'
+          lrs.save!
+        end
+      end
+    end
+  end
+end
