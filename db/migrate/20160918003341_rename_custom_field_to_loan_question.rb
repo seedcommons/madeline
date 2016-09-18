2e091fc374d55f09f5d3188c9507d38ae916d8e5
Adam Konner <linesarefuzzy@gmail.com>
2016-09-17 21:56:48 -0400

    custom_field > loan_question 5
    works!

--- db/migrate/20160918003341_rename_custom_field_to_loan_question.rb
+++ db/migrate/20160918003341_rename_custom_field_to_loan_question.rb
@@ -1,3 +1,5 @@
+class Translation < ActiveRecord::Base; end
+
 class RenameCustomFieldToLoanQuestion < ActiveRecord::Migration
   def change
     rename_table :custom_fields, :loan_questions
@@ -7,5 +9,7 @@ class RenameCustomFieldToLoanQuestion < ActiveRecord::Migration
 
     rename_column :loan_questions, :custom_field_set_id, :loan_question_set_id
     rename_column :loan_question_requirements, :custom_field_id, :loan_question_id
+
+    Translation.where(translatable_type: 'CustomField').update_all(translatable_type: 'LoanQuestion')
   end
 end
