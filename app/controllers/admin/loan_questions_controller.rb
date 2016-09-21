c8edac88c4c27cf783195efa815ef8d5f9ae37ec
Elizabeth Mitchell <elizabeth@sassafras.coop>
2016-09-21 16:21:34 -0400

    5066: Restore sorting of loan types

--- app/controllers/admin/loan_questions_controller.rb
+++ app/controllers/admin/loan_questions_controller.rb
@@ -21,11 +21,7 @@ class Admin::LoanQuestionsController < Admin::AdminController
 
   def edit
     @loan_question.build_complete_requirements
-    @requirements = @loan_question.custom_field_requirements
-    # TODO: Restore @requirements to item similar to below
-    # Using this line results in the following error for the below line:
-      # undefined method `label' for nil:NilClass:
-    # @requirements = @loan_question.custom_field_requirements.sort_by { |i| i.loan_type.label.text }
+    @requirements = @loan_question.custom_field_requirements.sort_by { |i| i.loan_type.label.text }
     render_form
   end
 
