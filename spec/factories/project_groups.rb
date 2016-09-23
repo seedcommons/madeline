a92fa5318c49cb51b46ca74ea11cabea51f4f2e2
Tom Smyth <tom@sassafras.coop>
2016-09-23 11:17:40 -0400

    5022: Made timeline factory more comprehensive

--- spec/factories/project_groups.rb
+++ spec/factories/project_groups.rb
@@ -11,20 +11,35 @@ FactoryGirl.define do
       factory :root_project_group_with_descendants do
 
         after(:create) do |root|
-          create_group = ->(attribs = {}) do
-            create(:project_group, attribs.merge(project: root.project, division: root.division))
+          def add_child_group(parent, root)
+            parent.children <<
+              (group = create(:project_group, project: root.project, division: root.division))
+            group
           end
 
-          # Create 3 top level groups, each with 2-4 subgroups, each with 1-4 child steps
-          3.times { root.children << create_group.call }
-          root.children.each do |child|
-            rand(2..4).times { child.children << create_group.call }
-            child.children.each do |grandchild|
-              rand(1..4).times do
-                grandchild.children << create(:project_step, project: root.project, division: root.division)
-              end
-            end
+          def add_child_step(parent, root)
+            parent.children <<
+              (step = create(:project_step, project: root.project, division: root.division))
+            step
           end
+
+          def add_child_steps(parent, root)
+            rand(1..4).times.map { add_child_step(parent, root) }
+          end
+
+          g1 = add_child_group(root, root)
+            g1_s = add_child_steps(g1, root)
+          g2 = add_child_group(root, root)
+            g2_1 = add_child_group(g2, root)
+              g2_1_1 = add_child_group(g2_1, root)
+                g2_1_1_s = add_child_steps(g2_1_1, root)
+              g2_1_2 = add_child_group(g2_1, root)
+                g2_1_2_s = add_child_steps(g2_1_2, root)
+              g2_1_s = add_child_steps(g2_1, root)
+            g2_2 = add_child_group(g2, root)
+              g2_2_s = add_child_steps(g2_2, root)
+          s3 = add_child_step(root, root)
+          g4 = add_child_group(root, root)
         end
       end
     end
