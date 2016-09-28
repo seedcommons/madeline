e02b5ee68838d18ab7a6c4fd2b1b1248cef829c1
Adam Konner <linesarefuzzy@gmail.com>
2016-09-28 18:32:42 -0400

    5126: comments & cleanup

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -13,22 +13,26 @@
 
 class Currency < ActiveRecord::Base
   include ActionView::Helpers::NumberHelper
-  def format_amount(amount, tooltip: true)
-    display_symbol = symbol.sub('$', ' $') # add space before $ (pretty)
-    if tooltip
-      display_symbol = %Q(<a href="#" onclick="return false" data-toggle="tooltip" class="currency_symbol" title="#{plural_name}">#{display_symbol}</a> ).html_safe
-    end
-    return number_to_currency(amount, unit: display_symbol)
-  end
 
   def division
     Division.root # for permissions purposes, assume currency model belongs to root division
   end
 
+  def format_amount(amount, tooltip: true)
+    display_symbol = symbol.sub('$', ' $') # add space before $ (pretty)
+    if tooltip
+      display_symbol = %Q(<a href="#" onclick="return false" data-toggle="tooltip"
+        class="currency_symbol" title="#{plural_name}">#{display_symbol}</a> ).html_safe
+    end
+    return number_to_currency(amount, unit: display_symbol)
+  end
+
   private
 
   def plural_name
-    # I18n.t()
+    # This should obviously be refactored someday. Ideally the currency's name field would
+    # be converted to be translatable. Currently the name is stored only in English.
+    # As of now this is only used in the public front end (which is used in Spanish as well!).
     "#{name}s"
   end
 end
