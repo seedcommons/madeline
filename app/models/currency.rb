483a479e3b5c484b114161837cb271988024552c
Adam Konner <linesarefuzzy@gmail.com>
2016-09-29 15:27:36 -0400

    5126: move formatter to helper

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -12,19 +12,8 @@
 #
 
 class Currency < ActiveRecord::Base
-  include ActionView::Helpers::NumberHelper
-
   def division
     Division.root # for permissions purposes, assume currency model belongs to root division
-  end
-
-  def format_amount(amount, tooltip: true)
-    display_symbol = symbol.sub('$', ' $') # add space before $ (pretty)
-    if tooltip
-      display_symbol = %Q(<a href="#" onclick="return false" data-toggle="tooltip"
-        class="currency_symbol" title="#{plural_name}">#{display_symbol}</a> ).html_safe
-    end
-    return number_to_currency(amount, unit: display_symbol)
   end
 
   private
@@ -32,7 +21,7 @@ class Currency < ActiveRecord::Base
   def plural_name
     # This should obviously be refactored someday. Ideally the currency's name field would
     # be converted to be translatable. Currently the name is stored only in English.
-    # As of now this is only used in the public front end (which is used in Spanish as well!).
+    # As of 9/2016 this is only used in the public front end (which is used in Spanish as well!).
     "#{name}s"
   end
 end
