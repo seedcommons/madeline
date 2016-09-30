e431c3b2a3340261373295e30705572447cafa9b
Adam Konner <linesarefuzzy@gmail.com>
2016-09-30 16:18:45 -0400

    5126: update migration to set nulls to USD

--- db/migrate/20160930194617_require_currency_on_loan.rb
+++ db/migrate/20160930194617_require_currency_on_loan.rb
@@ -1,5 +1,13 @@
+class Loan < ActiveRecord::Base; end
+
 class RequireCurrencyOnLoan < ActiveRecord::Migration
-  def change
+  def up
+    # Set nulls to USD (for development environments)
+    Loan.where(currency_id: nil).update_all(currency_id: 2)
     change_column :loans, :currency_id, :integer, null: false
+  end
+
+  def down
+    change_column :loans, :currency_id, :integer
   end
 end
