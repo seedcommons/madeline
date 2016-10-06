7cdfd8b94601f262111f67e518cfe898170604b6
Tom Smyth <tom@sassafras.coop>
2016-10-06 14:27:57 -0400

    4994: Removed unnecessary policies

--- app/policies/project_group_policy.rb
+++ app/policies/project_group_policy.rb
@@ -1,25 +1,2 @@
 class ProjectGroupPolicy < ApplicationPolicy
-
-  delegate :persisted?, to: :record
-
-  def duplicate?
-    create? && persisted?
-  end
-
-  def batch_destroy?
-    destroy?
-  end
-
-  def adjust_dates?
-    update?
-  end
-
-  def shift_subsequent?
-    update?
-  end
-
-  def finalize?
-    update?
-  end
-
 end
