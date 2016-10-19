71750dd890b3d8c3b9603cf63876213dd0f43dde
Adam Konner <linesarefuzzy@gmail.com>
2016-10-19 15:17:14 -0400

    5299: clean up and optimize loan response set migration

--- app/models/legacy/loan_response_set.rb
+++ app/models/legacy/loan_response_set.rb
@@ -8,9 +8,7 @@ module Legacy
 
     def self.migrate_all
       puts "loan response sets: #{self.count}"
-      LoanResponseSet.uniq.pluck('ResponseSetID').each do |response_set_id|
-        migrate(response_set_id)
-      end
+      all.each(&:migrate)
     end
 
     def self.purge_migrated
@@ -20,25 +18,20 @@ module Legacy
     end
 
 
-    def self.migrate(response_set_id)
-      has_matching_loan = LoanResponseSet.where("ResponseSetID = #{response_set_id} and LoanID = #{response_set_id}").count
-      unless has_matching_loan
-        $stderr.puts "ERROR - matching loan unexpectedly missing for response set id: #{response_set_id}"
-        raise "ERROR - matching loan unexpectedly missing for response set id: #{response_set_id}"
-      end
-      loan_id = response_set_id
-      new_loan = ::Loan.find_by(id: loan_id)
-      unless new_loan
-        $stderr.puts "new loan not found for id: #{loan_id} - skipping"
+    def migrate
+      loan_id = id
+      loan = ::Loan.find_by(id: loan_id)
+      unless loan
+        $stderr.puts "loan not found for id: #{id} - skipping"
         return
       end
 
-      # criteria = new_loan.fetch_has_one_custom('loan_criteria', autocreate: true)
-      # post_analysis = new_loan.fetch_has_one_custom('loan_post_analysis', autocreate: true)
+      # criteria = loan.fetch_has_one_custom('loan_criteria', autocreate: true)
+      # post_analysis = loan.fetch_has_one_custom('loan_post_analysis', autocreate: true)
       # Cache of criteria and post_analysis value sets.
       models = {}
 
-      responses = LoanResponse.where("ResponseSetID = ?", response_set_id)
+      responses = LoanResponse.where("ResponseSetID = ?", id)
       puts "responses count: #{responses.count}"
       responses.each do |response|
         # puts "response id: #{response.id} - question id: #{response.question_id}"
@@ -47,11 +40,10 @@ module Legacy
           # puts "question_id: #{response.question_id} - set: #{field.loan_question_set.internal_name}"
           model = models[field.loan_question_set.internal_name]
           unless model
-#            model = new_loan.fetch_has_one_custom(field.loan_question_set.internal_name, autocreate: true)
             match = /loan_(.*)/.match(field.loan_question_set.internal_name)
             raise "unexpected custom field set name: #{field.loan_question_set.internal_name}" unless match
             attrib = match[1]
-            model = new_loan.send(attrib) || ::LoanResponseSet.new(kind: attrib, loan: new_loan, custom_data: {})
+            model = loan.send(attrib) || ::LoanResponseSet.new(kind: attrib, loan: loan, custom_data: {})
             models[field.loan_question_set.internal_name] = model
           end
           # puts "update: #{field.id} -> #{response.value_hash}"
@@ -70,8 +62,7 @@ module Legacy
         # numeric data so that the record can still be simply re-saved.
         m.save(:validate => false)
       end
-      LoanResponseSet.where("ResponseSetID = ? and ResponseSetID <> LoanID", response_set_id).pluck('LoanID').each do |linked_loan_id|
-        puts "related loan: #{linked_loan_id}"
+      LoanResponseSet.where("ResponseSetID = ? and ResponseSetID <> LoanID", id).pluck('LoanID').each do |linked_loan_id|
         # todo: clone cross-linked response sets
       end
 
