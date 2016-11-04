76bd3c4eca07d18a515dd2d70a30d3fd460b74e2
Adam Konner <linesarefuzzy@gmail.com>
2016-11-04 18:12:13 -0400

    5307: migrate project events in tree traversal order

--- app/models/legacy/project_event.rb
+++ app/models/legacy/project_event.rb
@@ -57,9 +57,9 @@ module Legacy
     def migrate_step
       data = migration_step_data
       puts "ProjectStep[#{data[:id]}] #{data[:project_id]}"
-      step = ::ProjectStep.create(data)
+      step = ::ProjectStep.new(data)
 
-      step.parent = find_parent_group
+      step.parent = find_or_create_parent_group
 
       step.save!
       step
@@ -67,27 +67,32 @@ module Legacy
       $stderr.puts "#{step.class.name}[#{id}] error migrating step, could not find loan #{data[:project_id]}: #{e} - skipping"
     end
 
-    def find_parent_group
+    def find_or_create_parent_group
       return unless loan
-      ProjectGroup.find_by(project_type: "Loan", project_id: loan.id)
+      ProjectGroup.find_or_create_by(project_type: "Loan", project_id: loan.id)
+    end
+
+    def children
+      Legacy::ProjectEvent.migratable.where(milestone_group: id)
     end
 
     def migrate_group
       data = migration_data
       puts "ProjectGroup[#{data[:id]}] #{data[:project_id]}"
       ::ProjectGroup.create!(data)
+      children.each(&:migrate)
     rescue StandardError => e
       $stderr.puts "#{step.class.name}[#{id}] error migrating group: #{e} - skipping"
     end
 
-    def migrate_parent
-      puts "setting #{id}: with parent #{milestone_group}"
-      step = TimelineEntry.find(id)
-      step.parent = TimelineEntry.find(milestone_group)
-      step.save!
-    rescue StandardError => e
-      $stderr.puts "#{step.class.name}[#{id}] error migrating parent: #{e} - skipping"
-    end
+    # def migrate_parent
+    #   puts "setting #{id}: with parent #{milestone_group}"
+    #   step = TimelineEntry.find(id)
+    #   step.parent = TimelineEntry.find(milestone_group)
+    #   step.save!
+    # rescue StandardError => e
+    #   $stderr.puts "#{step.class.name}[#{id}] error migrating parent: #{e} - skipping"
+    # end
 
     def migrate_schedule_parent
       puts "setting #{id}: with schedule parent #{dependent_date}"
@@ -141,6 +146,31 @@ module Legacy
       $stderr.puts e
     end
 
+    def migrate
+      if self.class.group_ids.include? id
+        migrate_group
+      else
+        migrate_step
+      end
+    end
+
+    def self.migratable
+      # note record 10155 has a malformed date (2013-12-00) which was causing low level barfage
+      # migratable = self.where("Type = 'Paso' and #{malformed_date_clause('Completed')}")
+      where("Type = 'Paso' and #{malformed_date_clause('Completed')}").where(project_table: 'Loans')
+    end
+
+    def self.step_children
+      migratable.where.not(milestone_group: nil)
+    end
+
+    def self.group_ids
+      @group_ids ||= step_children.pluck(:milestone_group).uniq
+    end
+
+    def self.roots
+      migratable.where(milestone_group: nil)
+    end
 
     def self.migrate_all
       puts "project steps: #{Legacy::ProjectEvent.count}"
@@ -150,26 +180,14 @@ module Legacy
       puts "setting projects_step_id_seq to: #{max + 1000}"
       ::ProjectStep.recalibrate_sequence(id: max + 1000)
 
-      # note record 10155 has a malformed date (2013-12-00) which was causing low level barfage
-      # step_events = self.where("Type = 'Paso' and #{malformed_date_clause('Completed')}")
-      step_events = self.where("Type = 'Paso' and #{malformed_date_clause('Completed')}").where(project_table: 'Loans')
-
-      step_children = step_events.where.not(milestone_group: nil)
-      parent_ids = step_children.pluck(:milestone_group)
-
       ProjectGroup.skip_callback(:create, :before, :ensure_single_root)
-      step_events.find_each do |event|
-        if parent_ids.include? event.id
-          event.migrate_group
-        else
-          event.migrate_step
-        end
-      end
 
-      step_children.find_each &:migrate_parent
+      roots.find_each(&:migrate)
 
-      step_events.where("ProjectEvents.DependentDate is not null OR ProjectEvents.date is not null").find_each do |event|
-        if parent_ids.include? event.id
+      # step_children.find_each &:migrate_parent
+
+      migratable.where("ProjectEvents.DependentDate is not null OR ProjectEvents.date is not null").find_each do |event|
+        if group_ids.include? event.id
           event.copy_date_from_group_to_step
         else
           event.migrate_schedule_parent if event.dependent_date
