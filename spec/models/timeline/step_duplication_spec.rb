486e0def925e40d0d107c6ef81d9bec707187c30
Tom Smyth <tom@sassafras.coop>
2016-11-16 15:08:50 -0500

    5016: Refactored duplicate step spec

--- spec/models/timeline/step_duplication_spec.rb
+++ spec/models/timeline/step_duplication_spec.rb
@@ -78,39 +78,68 @@ RSpec.describe Timeline::StepDuplication, type: :model do
     end
   end
 
-  context 'when duplicated once daily' do
-    let(:original) { create(:project_step) }
-    let(:duplicate) do
-      duplication = Timeline::StepDuplication.new(original)
-      duplication.perform(default_params)
-    end
-    subject { duplicate.first }
-
-    it_should_behave_like 'a duplicated step', date_offset: 1.day
-  end
-
-  context 'when duplicated twice daily' do
-    let(:original) { create(:project_step) }
-    let(:num_of_occurrences) { '2' }
-    let(:duplicate) do
-      duplication = Timeline::StepDuplication.new(original)
-      duplication.perform(default_params)
-    end
-
-    it 'should create 2 duplicates' do
-      expect(duplicate.count).to eq 2
-    end
-
-    context 'with first step' do
+  describe 'daily duplications' do
+    context 'when duplicated once daily' do
+      let(:original) { create(:project_step) }
+      let(:duplicate) do
+        duplication = Timeline::StepDuplication.new(original)
+        duplication.perform(default_params)
+      end
       subject { duplicate.first }
 
       it_should_behave_like 'a duplicated step', date_offset: 1.day
     end
 
-    context 'with second step' do
-      subject { duplicate[1] }
+    context 'when duplicated twice daily' do
+      let(:original) { create(:project_step) }
+      let(:num_of_occurrences) { '2' }
+      let(:duplicate) do
+        duplication = Timeline::StepDuplication.new(original)
+        duplication.perform(default_params)
+      end
 
-      it_should_behave_like 'a duplicated step', date_offset: 2.days
+      it 'should create 2 duplicates' do
+        expect(duplicate.count).to eq 2
+      end
+
+      context 'with first step' do
+        subject { duplicate.first }
+
+        it_should_behave_like 'a duplicated step', date_offset: 1.day
+      end
+
+      context 'with second step' do
+        subject { duplicate[1] }
+
+        it_should_behave_like 'a duplicated step', date_offset: 2.days
+      end
+    end
+
+    context 'when 3-day long child step is duplicated twice daily' do
+      let(:original_parent) { create(:project_step, :with_children, scheduled_start_date: Date.civil(2016, 3, 18), scheduled_duration_days: 3) }
+      let(:original) { original_parent.schedule_children.first }
+      let(:num_of_occurrences) { '2' }
+      let(:time_unit) { 'days' }
+      let(:duplicate) do
+        duplication = Timeline::StepDuplication.new(original)
+        duplication.perform(default_params)
+      end
+
+      it 'should create 2 duplicates' do
+        expect(duplicate.count).to eq 2
+      end
+
+      context 'with first step' do
+        subject { duplicate.first }
+
+        it_should_behave_like 'a duplicated step', date_offset: 1.day
+      end
+
+      context 'with second step' do
+        subject { duplicate[1] }
+
+        it_should_behave_like 'a duplicated step', date_offset: 2.days
+      end
     end
   end
 
@@ -140,83 +169,45 @@ RSpec.describe Timeline::StepDuplication, type: :model do
     end
   end
 
-  context 'when duplicated twice monthly' do
-    let(:original) { create(:project_step, scheduled_start_date: Date.civil(2016, 3, 18)) }
+  describe 'monthly duplications' do
     let(:num_of_occurrences) { '2' }
     let(:time_unit) { 'months' }
-    let(:duplicate) do
-      duplication = Timeline::StepDuplication.new(original)
-      duplication.perform(default_params.merge('month_repeat_on': '18th day'))
+
+    shared_examples_for 'duplicated twice monthly' do
+      it 'should create 2 duplicates' do
+        expect(duplicate.count).to eq 2
+      end
+
+      context 'with first step' do
+        subject { duplicate.first }
+        it_should_behave_like 'a duplicated step', date_offset: 1.month
+      end
+
+      context 'with second step' do
+        subject { duplicate[1] }
+        it_should_behave_like 'a duplicated step', date_offset: 2.months
+      end
     end
 
-    it 'should create 2 duplicates' do
-      expect(duplicate.count).to eq 2
+    context 'when duplicated twice monthly' do
+      let(:original) { create(:project_step, scheduled_start_date: Date.civil(2016, 3, 18)) }
+      let(:duplicate) do
+        duplication = Timeline::StepDuplication.new(original)
+        duplication.perform(default_params.merge(month_repeat_on: '18th day'))
+      end
+
+      it_behaves_like 'duplicated twice monthly'
     end
 
-    context 'with first step' do
-      subject { duplicate.first }
+    context 'when duplicated monthly into future' do
+      let(:original) { create(:project_step, scheduled_start_date: Date.civil(2016, 3, 10)) }
+      let(:end_occurrence_type) { 'date' }
+      let(:duplicate) do
+        duplication = Timeline::StepDuplication.new(original)
+        duplication.perform(default_params.merge(month_repeat_on: '10th day', end_date: '2016-05-30'))
+      end
 
-      it_should_behave_like 'a duplicated step', date_offset: 1.month
-    end
-
-    context 'with second step' do
-      subject { duplicate[1] }
-
-      it_should_behave_like 'a duplicated step', date_offset: 2.months
-    end
-  end
-
-  context 'when duplicated monthly into future' do
-    let(:original) { create(:project_step, scheduled_start_date: Date.civil(2016, 3, 10)) }
-    let(:num_of_occurrences) { '2' }
-    let(:time_unit) { 'months' }
-    let(:duplicate) do
-      params = default_params.merge('month_repeat_on': '10th day', 'end_occurrence_type': 'date', 'end_date': '2016-05-30')
-      duplication = Timeline::StepDuplication.new(original)
-      duplication.perform(params)
-    end
-
-    it 'should create 2 duplicates' do
-      expect(duplicate.count).to eq 2
-    end
-
-    context 'with first step' do
-      subject { duplicate.first }
-
-      it_should_behave_like 'a duplicated step', date_offset: 1.month
-    end
-
-    context 'with second step' do
-      subject { duplicate[1] }
-
-      it_should_behave_like 'a duplicated step', date_offset: 2.months
-    end
-  end
-
-  context 'when 3-day long child step is duplicated twice daily' do
-    let(:original_parent) { create(:project_step, :with_children, scheduled_start_date: Date.civil(2016, 3, 18), scheduled_duration_days: 3) }
-    let(:original) { original_parent.schedule_children.first }
-    let(:num_of_occurrences) { '2' }
-    let(:time_unit) { 'days' }
-    let(:duplicate) do
-      duplication = Timeline::StepDuplication.new(original)
-      duplication.perform(default_params)
-    end
-
-    it 'should create 2 duplicates' do
-      expect(duplicate.count).to eq 2
-    end
-
-    context 'with first step' do
-      subject { duplicate.first }
-
-      it_should_behave_like 'a duplicated step', date_offset: 1.day
-    end
-
-    context 'with second step' do
-      subject { duplicate[1] }
-
-      it_should_behave_like 'a duplicated step', date_offset: 2.days
+      it_behaves_like 'duplicated twice monthly'
     end
   end
 end
