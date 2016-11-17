4d5a81b2b8bf33a17050dd6242dc7561a7c82593
Adam Konner <linesarefuzzy@gmail.com>
2016-11-17 14:55:14 -0500

    5405: test for division preference

--- spec/controllers/admin/project_logs_controller_spec.rb
+++ spec/controllers/admin/project_logs_controller_spec.rb
@@ -3,24 +3,41 @@ require 'rails_helper'
 RSpec.describe Admin::ProjectLogsController, type: :controller do
   describe "create" do
     before { sign_in_admin }
-    let(:attributes) { build(:project_log).attributes }
+    let(:log) { build(:project_log) }
 
-    context "with notify checked" do
-      it "enqueues and sends notification email" do
-        expect do
-          post :create, project_log: attributes, notify: true
-        end.to change { Delayed::Job.count }.by(1)
-        expect do
-          @dj_result = Delayed::Worker.new.work_off
-        end.to change { ActionMailer::Base.deliveries.size }.by(1)
-        expect(@dj_result).to eq [1, 0] # successes, failures
+    context "with division set to notify" do
+      before { log.division.update(custom_data: { notify_on_new_logs: true }) }
+
+      context "with notify checked" do
+        let(:notify) { true }
+
+        it "enqueues and sends notification email" do
+          expect do
+            post :create, project_log: log.attributes, notify: notify
+          end.to change { Delayed::Job.count }.by(1)
+          expect do
+            @dj_result = Delayed::Worker.new.work_off
+          end.to change { ActionMailer::Base.deliveries.size }.by(1)
+          expect(@dj_result).to eq [1, 0] # successes, failures
+        end
+      end
+
+      context "with notify unchecked" do
+        let(:notify) { false }
+
+        it "doesn't send email" do
+          expect do
+            post :create, project_log: log.attributes, notify: notify
+            Delayed::Worker.new.work_off
+          end.to change { ActionMailer::Base.deliveries.size }.by(0)
+        end
       end
     end
 
-    context "with notify unchecked" do
+    context "with division not set to notify" do
       it "doesn't send email" do
         expect do
-          post :create, project_log: attributes, notify: false
+          post :create, project_log: log.attributes, notify: true
           Delayed::Worker.new.work_off
         end.to change { ActionMailer::Base.deliveries.size }.by(0)
       end
