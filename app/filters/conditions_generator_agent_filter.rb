ce69483bb691833c812000f4275cf4180b655a65
Nathan Lee <nathan@globalphobia.com>
2016-12-02 14:46:51 -0700

    5449: Some cleanup for code review

--- app/filters/conditions_generator_agent_filter.rb
+++ app/filters/conditions_generator_agent_filter.rb
@@ -1,8 +1,7 @@
 class ConditionsGeneratorAgentFilter < Wice::Columns::ConditionsGeneratorColumn
   def generate_conditions(_table_alias, search_string)
     comparator = 'people.name ILIKE ? OR secondary_agents_loans.name ILIKE ?'
-    param = '%' + search_string + '%'
-    val = [comparator, param, param]
-    val
+    param = "%#{search_string}%"
+    [comparator, param, param]
   end
 end
