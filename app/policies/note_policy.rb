846b42a8035876cbb10debf2c2694f402757e477
Adam Konner <linesarefuzzy@gmail.com>
2017-01-04 21:20:32 -0500

    4623: hide edit/delete when forbidden

--- app/policies/note_policy.rb
+++ app/policies/note_policy.rb
@@ -1,11 +1,4 @@
 class NotePolicy < ApplicationPolicy
-  # def create?
-  #   notable_class = @record.notable_class.constantize
-  #   notable = notable_class.find(@record.notable_id)
-  #   policy_class = "#{@record.notable_class}Policy".constantize
-  #   policy_class.new(@user, notable).update?
-  # end
-
   def create?
     Pundit.policy(user, record.notable).update?
   end
