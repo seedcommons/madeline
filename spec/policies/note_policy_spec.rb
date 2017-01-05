545b8ba863e9f90547d89d59fed7912431becbf7
Adam Konner <linesarefuzzy@gmail.com>
2017-01-05 16:26:55 -0500

    4623: note policy spec

--- spec/policies/note_policy_spec.rb
+++ spec/policies/note_policy_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 describe NotePolicy do
-  it_should_behave_like 'base_policy', :note
+  subject { described_class.new(user, record) }
+
+  context "division member" do
+    let(:division) { create(:division) }
+    let(:user) { create(:user, :member, division: division) }
+    let(:org) { create(:organization, division: division) }
+    let(:record) { create(:note, notable: org) }
+
+    it { should permit_action(:create) }
+
+    context "own note" do
+      let(:record) { create(:note, author: user.profile, notable: org) }
+
+      it { should permit_action(:edit) }
+      it { should permit_action(:update) }
+      it { should permit_action(:destroy) }
+    end
+
+  end
 end
