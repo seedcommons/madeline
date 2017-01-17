05949fca6ff3e4e371ba73bda9de6f90ae01a41d
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-01-17 11:53:01 -0500

    5592: Add basic loans grid

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -3,5 +3,20 @@ class Admin::ProjectsController < Admin::AdminController
 
   def index
     authorize Project
+
+    @projects_grid = initialize_grid(
+      policy_scope(Project),
+      order_direction: 'desc',
+      per_page: 50,
+      name: 'projects',
+      enable_export_to_csv: true
+    )
+
+    @csv_mode = true
+
+    export_grid_if_requested do
+      # This block only executes if CSV is not being returned
+      @csv_mode = false
+    end
   end
 end
