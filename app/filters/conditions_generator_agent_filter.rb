92c05c46af2be6b5e3b00a4419d0400a6899010a
Nathan Lee <nathan@globalphobia.com>
2017-01-18 13:20:41 -0700

    5728: Use proper table name, now that Loans are now in the Project table

--- app/filters/conditions_generator_agent_filter.rb
+++ app/filters/conditions_generator_agent_filter.rb
@@ -1,6 +1,6 @@
 class ConditionsGeneratorAgentFilter < Wice::Columns::ConditionsGeneratorColumn
   def generate_conditions(_table_alias, search_string)
-    comparator = 'people.name ILIKE ? OR secondary_agents_loans.name ILIKE ?'
+    comparator = 'people.name ILIKE ? OR secondary_agents_projects.name ILIKE ?'
     param = "%#{search_string}%"
     [comparator, param, param]
   end
