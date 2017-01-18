a2fdbee20fb4fc6a35e87d4444917d609d50b720
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-01-18 13:31:23 -0500

    5593: Ensure project can be deleted

--- app/controllers/admin/basic_projects_controller.rb
+++ app/controllers/admin/basic_projects_controller.rb
@@ -1,8 +1,24 @@
 class Admin::BasicProjectsController < Admin::AdminController
+  def index
+    authorize BasicProject
+  end
+
   def show
     @project = BasicProject.find(params[:id])
     authorize @project
     prep_form_vars
+  end
+
+  def destroy
+    @project = BasicProject.find(params[:id])
+    authorize @project
+
+    if @project.destroy
+      redirect_to admin_basic_projects_path, notice: I18n.t(:notice_deleted)
+    else
+      prep_form_vars
+      render :show
+    end
   end
 
   private
