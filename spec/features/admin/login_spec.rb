cf603c28a6aa73a1a8e505a3fdff765b939f2d48
Tom Smyth <tom@sassafras.coop>
2017-02-01 17:13:14 -0500

    5765: Added happy path manual login spec

---
+++ spec/features/admin/login_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+feature "login" do
+  let(:user) { create(:person, :with_member_access, :with_password).user }
+
+  scenario "should work", js: true do
+    visit("/users/sign_in")
+    fill_in("Email", with: user.email)
+    fill_in("Password", with: user.password)
+    click_on("Login")
+    expect(page).to have_content("Signed in successfully")
+  end
+end
