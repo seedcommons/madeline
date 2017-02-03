eacc643edebffd7b5139e8bcbb1ab842b70c3166
Nathan Lee <nathan@globalphobia.com>
2017-02-03 11:23:21 -0700

    5572 Adding comment describing why the named route helpers are required

--- app/helpers/named_route_overrides_helper.rb
+++ app/helpers/named_route_overrides_helper.rb
@@ -1,3 +1,6 @@
+# These functions exist solely for wice_grid.
+# wice_grid does not support default filter values. So instead we are overriding the
+# params passed to the routes using wice_grid
 module NamedRouteOverridesHelper
   def admin_loans_path(*args)
     # Default to active loans, unless overriden
