04c5cf34bcba6a451a9ccc3e31737c7aa2e95564
Nathan Lee <nathan@globalphobia.com>
2017-02-03 11:20:21 -0700

    5572 Move route override helpers into separate file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -72,20 +72,4 @@ module ApplicationHelper
       capture(&block)
     end
   end
-
-  def admin_loans_path(*args)
-    # Default to active loans, unless overriden
-    options = HashWithIndifferentAccess.new loans: {f:{status_value: ['active']}}
-    options = options.deep_merge(args.pop) if args.last.is_a? Hash
-
-    super(args, options)
-  end
-
-  def admin_people_path(*args)
-    # Default to system users, unless overriden
-    options = HashWithIndifferentAccess.new people: {f:{ has_system_access: ['t']}}
-    options = options.deep_merge(args.pop) if args.last.is_a? Hash
-
-    super(args, options)
-  end
 end
