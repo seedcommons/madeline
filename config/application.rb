810800dec8df386c938cfc69199c4efd293c1b51
Nathan Lee <nathan@globalphobia.com>
2017-02-06 17:27:13 -0700

    5765 fix wice_grid rendering errors in test

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,9 @@ module MadelineSystem
 
     config.action_mailer.default_url_options = { host: ENV['MADELINE_HOSTNAME'] }
   end
+
+  ## beware, for now this seems to be required for proper rendering of all wice_grid views.  (without, view contents is all html escaped)
+  Slim::Engine.set_options pretty: true, sort_attrs: false
 end
 
 puts "Rails.env: #{Rails.env}"
