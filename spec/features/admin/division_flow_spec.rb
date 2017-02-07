7754fbeb2c3876c97d0bae60447054ee51548a0a
Nathan Lee <nathan@globalphobia.com>
2017-02-07 11:15:07 -0700

    5765 ensure division can be changed

--- spec/features/admin/division_flow_spec.rb
+++ spec/features/admin/division_flow_spec.rb
@@ -13,9 +13,7 @@ feature 'division flow' do
     visit(admin_divisions_path)
     expect(page).to have_content(division.name)
 
-    within('#divisions') do
-      click_link(division.id)
-    end
+    find('#divisions').click_link(division.id)
 
     expect(page).to have_content(division.name)
 
@@ -26,5 +24,22 @@ feature 'division flow' do
     click_button 'Update Division'
     expect(page).to have_content('Changed Name')
     expect(page).to have_content('Record was successfully updated.')
+  end
+
+  scenario 'can change change to division and back', js: true do
+    visit(root_path)
+    expect(find('[data-expands="division-dropdown"]')).to have_content 'Select Division'
+
+    # Change to specific division, and ensure the page reloads properly
+    find('[data-expands="division-dropdown"]').click
+    find('.select_division_form').select(division.name)
+    expect(find('[data-expands="division-dropdown"]')).to have_content 'Change Division'
+    expect(find('.without-logo')).to have_content division.name
+
+    # Change back to all divisions, and ensure it reloads properly
+    find('[data-expands="division-dropdown"]').click
+    find('.select_division_form').select('All Divisions')
+    expect(find('.madeline')).to have_content 'Madeline'
+    expect(find('[data-expands="division-dropdown"]')).to have_content 'Select Division'
   end
 end
