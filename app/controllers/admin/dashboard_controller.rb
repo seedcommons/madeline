a45b91858d136a3be637f989f959e9df51d0c384
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-02-10 15:30:34 -0500

    3744: Add initial table of current person's projects

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -7,5 +7,21 @@ class Admin::DashboardController < Admin::AdminController
     # Projects belonging to the current user
     # 15 most recent projects, sorted by created date, then updated date
     @recent_projects = @person.agent_projects.order(created_at: :desc, updated_at: :desc)
+
+    @recent_projects_grid = initialize_grid(
+      @recent_projects,
+      include: [:primary_agent, :secondary_agent],
+      order_direction: "desc",
+      per_page: 50,
+      name: "recent_projects",
+      enable_export_to_csv: true
+    )
+
+    @csv_mode = true
+
+    export_grid_if_requested do
+      # This block only executes if CSV is not being returned
+      @csv_mode = false
+    end
   end
 end
