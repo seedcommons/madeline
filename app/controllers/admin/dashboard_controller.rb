c8c67577b90a58c8ecde501027907a8b79cdec9a
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-02-10 15:08:08 -0500

    3744: Return sorted list of projects

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,7 +4,8 @@ class Admin::DashboardController < Admin::AdminController
     authorize Person
     @person = Person.find(current_user.profile_id)
 
-    # Projects belonging to the current user, 15 most recent
-    @recent_projects = @person.agent_projects
+    # Projects belonging to the current user
+    # 15 most recent projects, sorted by created date, then updated date
+    @recent_projects = @person.agent_projects.order(created_at: :desc, updated_at: :desc)
   end
 end
