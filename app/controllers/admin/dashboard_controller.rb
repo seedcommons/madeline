b2cd1f2598f87a4aff26914f5f6f87dbc48e130e
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-02-10 14:59:23 -0500

    3744: Add initial project list to page
    Application policy change is due to pundit/policy error

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class Admin::DashboardController < Admin::AdminController
   def dashboard
-    skip_authorization
+    authorize Project
+    authorize Person
+    @person = Person.find(current_user.profile_id)
+
+    # Projects belonging to the current user, 15 most recent
+    @recent_projects = @person.agent_projects
   end
 end
