aa24af7c76673ab92926fd1041f07a5eea3b4606
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-02-13 10:43:01 -0500

    3744: Ensure export CSV works on the recent projects grid

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -17,9 +17,8 @@ class Admin::DashboardController < Admin::AdminController
     )
 
     @csv_mode = true
-
-    export_grid_if_requested do
-      # This block only executes if CSV is not being returned
+    export_grid_if_requested(recent_projects: "recent_projects_grid_definition") do
+      # The below only executes if CSV is not being returned
       @csv_mode = false
     end
   end
