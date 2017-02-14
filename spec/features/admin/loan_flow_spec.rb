66c7073734f097fb81cc3b5f00264af58af48a24
Nathan Lee <nathan@globalphobia.com>
2017-02-13 21:11:59 -0700

    5765 Multiple .loan-field classes were causing issues for the loan feature specs

--- spec/features/admin/loan_flow_spec.rb
+++ spec/features/admin/loan_flow_spec.rb
@@ -10,32 +10,33 @@ feature 'loan flow' do
   end
 
   # This should work, but for some reason it fails a lot more often
-  # include_examples :flow do
-  #   let(:model_to_test) { loan }
-  # end
-
-  scenario 'can view index', js: true do
-    visit(admin_loans_path)
-    expect(page).to have_content(loan.name)
-
-    within('#loans') do
-      click_link(loan.id)
-    end
-
-    expect(page).to have_content("##{loan.id}: #{loan.name}")
-
-    visit(admin_loan_path(id: loan.id))
-    expect(page).to have_content("##{loan.id}: #{loan.name}")
-    expect(page).to have_content('Edit Loan')
-
-    find('.edit-action').click
-
-    expect(page).to have_css('#loan_name', visible: true)
-    fill_in('loan[name]', with: 'Changed Loan Name')
-
-    expect(page).to have_css('.update-action', visible: true)
-    click_button 'Update Loan'
-    expect(page).to have_content("##{loan.id}: Changed Loan Name")
-    expect(page).to have_content('Record was successfully updated.')
+  include_examples :flow do
+    let(:model_to_test) { loan }
   end
+
+  # Keeping this code here for now. It tended to be more stable than the shared example.
+  # Can be deleted when we are happy the shared spec is working.
+  # scenario 'can view index', js: true do
+  #   visit(admin_loans_path)
+  #   expect(page).to have_content(loan.name)
+  #
+  #   within('#loans') do
+  #     click_link(loan.id)
+  #   end
+  #
+  #   expect(page).to have_content("##{loan.id}: #{loan.name}")
+  #
+  #   visit(admin_loan_path(id: loan.id))
+  #   expect(page).to have_content("##{loan.id}: #{loan.name}", wait: 10)
+  #   expect(page).to have_content('Edit Loan')
+  #
+  #   find('.edit-action').click
+  #
+  #   expect(page).to have_css('#loan_name', visible: true, wait: 10)
+  #   fill_in('loan[name]', with: 'Changed Loan Name')
+  #
+  #   click_button 'Update Loan', wait: 10
+  #   expect(page).to have_content("##{loan.id}: Changed Loan Name")
+  #   expect(page).to have_content('Record was successfully updated.')
+  # end
 end
