42b9203254f624f18be1a935cfa45a7f6dfb5297
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-02-21 07:50:15 -0500

    5804: Add initial variable for logs

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,7 @@ class Admin::DashboardController < Admin::AdminController
     authorize :dashboard
     @person = Person.find(current_user.profile_id)
     prep_calendar
+    prep_logs
   end
 
   private
@@ -10,5 +11,12 @@ class Admin::DashboardController < Admin::AdminController
   def prep_calendar
     @division = current_division
     @calendar_events_url = "/admin/calendar_events?person_id=#{@person.id}"
+  end
+
+  def prep_logs
+    options = {}
+    @logs = ProjectLog.in_division(selected_division).filter_by(options).
+        order('date IS NULL, date DESC, created_at DESC').
+        page(1).per(10)
   end
 end
