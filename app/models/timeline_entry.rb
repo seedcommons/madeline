bd071f112f9e7dac7715ef397a1eaa933dd003c2
Adam Konner <linesarefuzzy@gmail.com>
2017-03-01 16:48:11 -0500

    Revert "5862 after_save hook to set step's ancestors' dates"
    Switching to a non-persisted strategy.
    
    This reverts commit 1bd00e3ae0022e6e1e18e6c954131fa4d84fb80d.

--- app/models/timeline_entry.rb
+++ app/models/timeline_entry.rb
@@ -49,9 +49,7 @@ class TimelineEntry < ActiveRecord::Base
 
   delegate :division, :division=, to: :project, allow_nil: true
 
-  def self.by_date
-    order("scheduled_start_date is null, scheduled_start_date, scheduled_duration_days")
-  end
+  scope :by_date, -> { order("scheduled_start_date is null desc, scheduled_start_date, scheduled_duration_days") }
 
   def step?
     is_a?(ProjectStep)
