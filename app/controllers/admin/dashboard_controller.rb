4518570bea49a4d5a7adf5e82c39d6214f595217
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-03-13 11:28:43 -0400

    5805: Show all users with projects

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -37,7 +37,7 @@ class Admin::DashboardController < Admin::AdminController
 
   # Prepare grids for all users inside selected division
   def prep_projects_grids
-    @people = @division.people.where.not(id: @person.id).limit(5)
+    @people = @division.people.where.not(id: @person.id)
 
     @people_with_projects = []
     @grids = {}
