152867742fb47c0918f79aecb080ec0b8ac5f218
Nathan Lee <nathan@globalphobia.com>
2017-03-16 08:45:32 -0600

    5828 missed adding a file

---
+++ app/models/accounting/quickbooks/fetcher_base.rb
@@ -0,0 +1,48 @@
+module Accounting
+  module Quickbooks
+    class FetchError < StandardError; end
+
+    # This class is responsible for batching up Quickbooks API calls into separate types.
+    # The API does support batch requests for queries, but quickbooks-ruby does not.
+    class FetcherBase
+      attr_reader :qb_connection
+
+      def initialize(qb_connection = Division.root.qb_connection)
+        @qb_connection = qb_connection
+      end
+
+      def fetch
+        types.each do |type|
+          service(type).all.each do |qb_object|
+            find_or_create_transaction(transaction_type: type, qb_object: qb_object)
+          end
+        end
+      rescue StandardError
+        raise FetchError, 'Problem fetching data from Quickbooks'
+      end
+
+      private
+
+      def find_or_create_transaction(transaction_type:, qb_object:)
+        acc_transaction = Accounting::Transaction.find_or_create_by qb_transaction_type: transaction_type, qb_id: qb_object.id
+        acc_transaction.update_attributes!(quickbooks_data: qb_object.as_json)
+      end
+
+      def populate(qb_objects)
+        @relation.each do |transaction|
+          qb = qb_objects.find { |qbo| qbo.id.to_s == transaction.qb_transaction_id }
+          transaction.qb_object = qb
+        end
+        @relation
+      end
+
+      def types
+        raise NotImplementedError
+      end
+
+      def service(type)
+        ::Quickbooks::Service.const_get(type).new(@qb_connection.auth_details)
+      end
+    end
+  end
+end
