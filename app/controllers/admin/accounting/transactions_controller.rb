c033320cd68040aaba32d66bd7ef13d833a6b056
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-03-22 09:07:52 -0400

    6118: Load wice grid table inside main transactions page

--- app/controllers/admin/accounting/transactions_controller.rb
+++ app/controllers/admin/accounting/transactions_controller.rb
@@ -2,12 +2,16 @@ class Admin::Accounting::TransactionsController < Admin::AdminController
   def index
     authorize :'accounting/transaction', :index?
 
-    Accounting::Quickbooks::AccountFetcher.new.fetch
-    Accounting::Quickbooks::TransactionFetcher.new.fetch
+    begin
+      ::Accounting::Quickbooks::AccountFetcher.new.fetch
+      ::Accounting::Quickbooks::TransactionFetcher.new.fetch
+    rescue Accounting::Quickbooks::FetchError => e
+      Rails.logger.error e
+      Rails.logger.error e.cause
+      flash.now[:error] = 'Error connecting to quickbooks'
+    end
 
-    @transactions = Accounting::Transaction.all
-  rescue
-    flash.now[:error] = 'Error connecting to quickbooks'
-    @transactions = []
+    @transactions = ::Accounting::Transaction.all
+    @transactions_grid = initialize_grid(@transactions)
   end
 end
