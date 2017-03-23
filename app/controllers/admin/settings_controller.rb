e10f323fc7ef30967e7fe771e70b99119e2b3615
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-03-23 15:05:57 -0400

    6124: Move accounts list to controller

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -4,6 +4,8 @@ class Admin::SettingsController < Admin::AdminController
     authorize :setting, :index?
 
     @division = current_division.root
+    # @accounts = Accounting::Account.all
+    @accounts = Accounting::Transaction.where(qb_transaction_type: 'Account').map{|account| [account.quickbooks_data[:name], account.quickbooks_data[:id]] }
   end
 
 end
