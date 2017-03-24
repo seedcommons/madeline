ad5810818b38b70bafefa0503c038c1cedaabf92
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-03-24 12:25:23 -0400

    6124: Use simple way to get accounts

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -4,8 +4,6 @@ class Admin::SettingsController < Admin::AdminController
     authorize :setting, :index?
 
     @division = current_division.root
-    # @accounts = Accounting::Account.all
-    @accounts = Accounting::Transaction.where(qb_transaction_type: 'Account').map{|account| [account.quickbooks_data[:name], account.quickbooks_data[:id]] }
+    @accounts = ::Accounting::Account.all.map{|account| [account.name, account.qb_id] }
   end
-
 end
