0b257c1450cd4dfdd90233c8321863b9e5749510
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-03-24 14:58:18 -0400

    5833: Export transactions into CSV file

--- app/controllers/concerns/transaction_listable.rb
+++ app/controllers/concerns/transaction_listable.rb
@@ -17,6 +17,16 @@ module TransactionListable
       @transactions = ::Accounting::Transaction.all
     end
 
-    @transactions_grid = initialize_grid(@transactions)
+    @transactions_grid = initialize_grid(@transactions,
+      enable_export_to_csv: true,
+      name: 'transactions'
+    )
+
+    @csv_mode = true
+
+    export_grid_if_requested('transactions': 'admin/accounting/transactions/transactions_grid_definition') do
+      # This block only executes if CSV is not being returned
+      @csv_mode = false
+    end
   end
 end
