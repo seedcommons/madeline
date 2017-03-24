c98c667534180ca170ff3b02385a7433971d1b49
Tom Smyth <tom@sassafras.coop>
2017-03-24 14:38:17 -0400

    Removed old specs

--- spec/models/progress_metric_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-describe ProgressMetric, :type => :model do
-  before { pending 're-implement in new project' }
-  it 'has a valid factory' do
-    expect(create(:progress_metric)).to be_valid
-  end
-end
