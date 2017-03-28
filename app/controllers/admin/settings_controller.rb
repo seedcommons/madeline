763bbd38a6be58d4a85878e50f123c84366b7ca1
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-03-28 13:29:17 -0400

    6124: Save accounts to a division

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -1,9 +1,26 @@
 class Admin::SettingsController < Admin::AdminController
 
   def index
-    authorize :setting, :index?
+    authorize :setting
 
     @division = current_division.root
-    @accounts = ::Accounting::Account.all.map{|account| [account.name, account.qb_id] }
+    @accounts = ::Accounting::Account.all
+  end
+
+  def update
+    authorize :setting
+
+    @division = current_division.root
+    if @division.update(settings_params)
+      redirect_to admin_settings_path, notice: I18n.t(:notice_updated)
+    else
+      @accounts = ::Accounting::Account.all
+      render :index
+    end
+  end
+
+  def settings_params
+    params.require(:division).permit(:principal_account_id, :interest_receivable_account_id,
+      :interest_income_account_id)
   end
 end
