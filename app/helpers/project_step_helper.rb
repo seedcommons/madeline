f73df317279c6766ec8c21f9ced325c56ab6199e
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-03-30 13:56:54 -0400

    5884: Add checkmarks to completed items using helper

--- app/helpers/project_step_helper.rb
+++ app/helpers/project_step_helper.rb
@@ -13,9 +13,11 @@ module ProjectStepHelper
 
     if step.completed?
       if days && days > 0
-        I18n.t('project_step.status.completed_late', days: days)
+        sanitize "<i class='fa fa-check'></i> #{I18n.t('project_step.status.completed_late',
+          days: days)}", tags: %w(i), attributes: %w(class)
       else
-        I18n.t('project_step.status.completed')
+        sanitize "<i class='fa fa-check'></i> #{I18n.t('project_step.status.completed')}",
+          tags: %w(i), attributes: %w(class)
       end
     elsif days
       if days <= 0
