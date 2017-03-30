e63827aa8f9f85c28a3de04f5f930c0ef372f3c7
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-03-30 13:41:23 -0400

    5884: Add initial project step helper method

--- app/helpers/project_step_helper.rb
+++ app/helpers/project_step_helper.rb
@@ -7,4 +7,24 @@ module ProjectStepHelper
       '<i class="fa fa-flag"></i> '.html_safe
     end
   end
+
+  def project_step_status(step)
+    days = step.days_late
+
+    if step.completed?
+      if days && days > 0
+        I18n.t('project_step.status.completed_late', days: days)
+      else
+        I18n.t('project_step.status.completed')
+      end
+    elsif days
+      if days <= 0
+        I18n.t('project_step.status.on_time')
+      else
+        I18n.t('project_step.status.days_late', days: days)
+      end
+    else
+      I18n.t(:none)
+    end
+  end
 end
