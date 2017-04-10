180e7e2ac970d2b186f71e9e6f75435ed326544f
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-04-10 12:36:36 -0400

    6017: Truncate text

--- app/helpers/media_helper.rb
+++ app/helpers/media_helper.rb
@@ -13,7 +13,7 @@ module MediaHelper
 
   def media_title(media_item)
     ext = File.extname(media_item.item.file.path)
-    file_name = File.basename(media_item.item.file.path, ext)
+    file_name = truncate(File.basename(media_item.item.file.path, ext), length: 15)
     content_tag(:span, "#{file_name}#{ext.downcase}")
   end
 end
