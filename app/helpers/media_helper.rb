4b580d847682583401cf416764f1b66ab5e63c8c
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-04-10 12:32:26 -0400

    6017: Add file name to bottom of media item

--- app/helpers/media_helper.rb
+++ app/helpers/media_helper.rb
@@ -10,4 +10,10 @@ module MediaHelper
       # end
     end
   end
+
+  def media_title(media_item)
+    ext = File.extname(media_item.item.file.path)
+    file_name = File.basename(media_item.item.file.path, ext)
+    content_tag(:span, "#{file_name}#{ext.downcase}")
+  end
 end
