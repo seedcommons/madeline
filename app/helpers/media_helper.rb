3f1834400897fbc536a67c21f411880d9bde6089
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-04-11 07:19:44 -0400

    6017: Adjust media helper

--- app/helpers/media_helper.rb
+++ app/helpers/media_helper.rb
@@ -3,17 +3,21 @@ module MediaHelper
     if media_item.thumbnail?
       image_tag(media_item.item.thumb.url)
     else
-      media_extension = File.extname(media_item.item.file.path).downcase
-      # content_tag(:div, class: "generic-thumbnail") do
-        content_tag(:div, media_extension, class: "extension")
-        # concat(image_tag("file-blank.png"))
-      # end
+      # media_extension = File.extname(media_item.item.file.path).downcase
+      # # content_tag(:div, class: "generic-thumbnail") do
+      # content_tag(:div, media_extension, class: "extension")
+      # # concat(image_tag("file-blank.png"))
+      # # end
+      content_tag(:div, media_item.kind_value.capitalize, class: "media-block")
     end
   end
 
   def media_title(media_item)
     ext = File.extname(media_item.item.file.path)
     file_name = truncate(File.basename(media_item.item.file.path, ext), length: 15)
-    content_tag(:span, "#{file_name}#{ext.downcase}")
+    content_tag(:div, class: "media-title") do
+      concat(content_tag(:span, file_name))
+      concat(content_tag(:div, ext.downcase))
+    end
   end
 end
