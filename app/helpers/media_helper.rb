d8a6355a517f43be804f50772a92696c386402d3
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-04-11 09:30:12 -0400

    6017: Clean up media items

--- app/helpers/media_helper.rb
+++ app/helpers/media_helper.rb
@@ -3,11 +3,6 @@ module MediaHelper
     if media_item.thumbnail?
       image_tag(media_item.item.thumb.url)
     else
-      # media_extension = File.extname(media_item.item.file.path).downcase
-      # # content_tag(:div, class: "generic-thumbnail") do
-      # content_tag(:div, media_extension, class: "extension")
-      # # concat(image_tag("file-blank.png"))
-      # # end
       content_tag(:div, class: "media-block") do
         concat(content_tag(:div, media_item.kind_value.capitalize))
         concat(sanitize("<i class='fa fa-#{media_icon_class(media_item)}'></i>", tags: %w(i),
