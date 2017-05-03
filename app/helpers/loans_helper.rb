f8bff93c34abea97c7ed30faec16a608270d3948
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-05-03 12:23:43 -0400

    5802: Show health status message on hover

--- app/helpers/loans_helper.rb
+++ app/helpers/loans_helper.rb
@@ -5,9 +5,17 @@ module LoansHelper
 
   def health_status_icon(loan)
     if loan.loan_health_check.healthy?
-      sanitize "<i class='fa fa-check-circle'></i> ", tags: %w(i), attributes: %w(class)
+      sanitize "<i class='fa fa-check-circle' data-toggle='tooltip' data-placement='right' title=#{health_status_message(loan)}></i> ", tags: %w(i), attributes: %w(class data title)
     else
-      sanitize "<i class='fa fa-exclamation-triangle'></i> ", tags: %w(i), attributes: %w(class)
+      sanitize "<i class='fa fa-exclamation-triangle' data-toggle='tooltip' data-placement='right' title=#{health_status_message(loan)}></i> ", tags: %w(i), attributes: %w(class data title)
+    end
+  end
+
+  def health_status_message(loan)
+    if loan.loan_health_check.healthy?
+      I18n.t("health_status.healthy")
+    else
+      "Unhealthy"
     end
   end
 end
