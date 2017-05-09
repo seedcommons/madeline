b1dd4449117fca43387950bcd2f11ec3143207d0
Nathan Lee <nathan@globalphobia.com>
2017-05-09 16:04:48 -0600

    6280 refactor specs to test for proper loan and customer refs

--- spec/models/accounting/quickbooks/transaction_creator_spec.rb
+++ spec/models/accounting/quickbooks/transaction_creator_spec.rb
@@ -6,24 +6,63 @@ RSpec.describe Accounting::Quickbooks::TransactionCreator, type: :model do
   let(:class_service) { instance_double(Quickbooks::Service::Class, find_by: [class_ref]) }
   let(:connection) { instance_double(Accounting::Quickbooks::Connection) }
   let(:account) { instance_double(Accounting::Account, qb_id: 98) }
-  subject { described_class.new(instance_double(Division, qb_connection: connection, principal_account: account)) }
+  let(:creator) { described_class.new(instance_double(Division, qb_connection: connection, principal_account: account)) }
 
   before do
-    allow(subject).to receive(:service).and_return(generic_service)
-    allow(subject).to receive(:class_service).and_return(class_service)
+    allow(generic_service).to receive(:create)
+    allow(creator).to receive(:service).and_return(generic_service)
+    allow(creator).to receive(:class_service).and_return(class_service)
   end
 
-  it 'should create class ref, and create journal entry' do
-    expect(generic_service).to receive(:create)
-    expect(class_service).to receive(:find_by)
+  context 'when qb customer does exist' do
+    let(:qb_customer_id) { 3 }
+    let(:loan_id) { 2 }
 
-    subject.add_disbursement(
-      amount: 12.09,
-      loan_id: 2,
-      memo: 'I am memo',
-      description: 'I am a line item description',
-      qb_bank_account_id: 89,
-      qb_customer_id: 3
-    )
+    subject do
+      creator.add_disbursement(
+        amount: 12.09,
+        loan_id: loan_id,
+        memo: 'I am memo',
+        description: 'I am a line item description',
+        qb_bank_account_id: 89,
+        qb_customer_id: qb_customer_id
+      )
+    end
+
+    context 'generic_service' do
+      it 'creates JournalEntry with 2 line items' do
+        expect(generic_service).to receive(:create) do |arg|
+          expect(arg.line_items.count).to eq 2
+        end
+
+        subject
+      end
+
+      it 'creates JournalEntry with 2 line items with customer refs' do
+        expect(generic_service).to receive(:create) do |arg|
+          arg.line_items.each do |item|
+            expect(item.journal_entry_line_detail.entity.type).to eq 'Customer'
+          end
+        end
+
+        subject
+      end
+
+      it 'creates JournalEntry with 2 line items with proper customer id' do
+        expect(generic_service).to receive(:create) do |arg|
+          arg.line_items.each do |item|
+            expect(item.journal_entry_line_detail.entity.entity_ref.value).to eq qb_customer_id
+          end
+        end
+
+        subject
+      end
+    end
+
+    it 'creates JournalEntry with a reference to the existing loan' do
+      expect(class_service).to receive(:find_by).with(:name, loan_id)
+
+      subject
+    end
   end
 end
