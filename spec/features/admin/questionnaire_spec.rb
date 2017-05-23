f258e315b44dee5a08ecf66afd2365d014241f04
Adam Konner <linesarefuzzy@gmail.com>
2017-05-23 15:04:37 -0400

    6274 misc code review changes

--- spec/features/admin/questionnaire_spec.rb
+++ spec/features/admin/questionnaire_spec.rb
@@ -4,28 +4,29 @@ feature 'questionnaire', js: true do
   let(:division) { create(:division) }
   let(:user) { create_member(division) }
   let(:loan) { create(:loan, division: division) }
-  # let(:loan_question_set) { create(:loan_question_set, :loan_criteria) }
+  let!(:loan_question_set) { create(:loan_question_set, :loan_criteria) }
 
   before do
     login_as(user, scope: :user)
-    create(:loan_question_set, :loan_criteria)
   end
 
   context 'with conflicting changes' do
     let(:response_set) { loan.create_criteria }
 
     before do
-      response_set.summary = { text: 'dragon' }
+      response_set.summary = {text: 'dragon'}
       response_set.save!
+
+      visit admin_loan_tab_path(loan, 'questions')
+      click_link('Edit Responses')
+      fill_in('loan_response_set[summary[text]]', with: 'gnashing teeth')
+      response_set.touch
+      click_button 'Save Responses'
+      save_and_open_page
     end
 
     it "raises an error and discard button works" do
       # Check that warning message is displayed
-      visit admin_loan_tab_path(loan, 'questions')
-      click_link('Edit Responses')
-      fill_in('loan_response_set[summary[text]]', with: 'breath of fire')
-      response_set.touch
-      click_button 'Save Responses'
       expect(page).to have_content 'Warning'
       expect(page).to have_selector 'input[type="submit"][name="overwrite"]'
 
@@ -37,11 +38,6 @@ feature 'questionnaire', js: true do
     end
 
     it "overwrite button works" do
-      visit admin_loan_tab_path(loan, 'questions')
-      click_link('Edit Responses')
-      fill_in('loan_response_set[summary[text]]', with: 'gnashing teeth')
-      response_set.touch
-      click_button 'Save Responses'
       find('input[type="submit"][name="overwrite"]').click
       expect(page).to have_content 'successfully updated'
       expect(page).to have_content 'gnashing teeth'
