b2da5a8d19f0104a6e6835de3afbd9f78624ab66
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-05-23 09:04:01 -0400

    5802: Fail gracefully if warnings are requsted

--- app/helpers/loans_helper.rb
+++ app/helpers/loans_helper.rb
@@ -7,7 +7,8 @@ module LoansHelper
     if loan.loan_health_check.healthy?
       status_message = {
         status: "healthy",
-        message: I18n.t("health_status.healthy")
+        message: I18n.t("health_status.healthy"),
+        warnings: []
       }
     else
       warnings = loan.loan_health_check.health_warnings.collect {|w| I18n.t("health_status.warnings.#{w}")}
