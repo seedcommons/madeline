4c2f5b18e55b3168e8172623741cc3bf908df77b
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-05-26 08:51:08 -0400

    5802: Only share loan health check with helpers
    Pass loan health check to helpers instead of sharing full loan
    information.

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,21 +1,19 @@
 module ProjectsHelper
-  def health_status_info(project)
-    if project.health_status_available?
-      render partial: "admin/dashboard/loan_health_message", locals: {
-        status_message: health_status_message(project)
-      }
-    end
+  def health_status_info(health_check)
+    render partial: "admin/dashboard/loan_health_message", locals: {
+      status_message: health_status_message(health_check)
+    }
   end
 
-  def health_status_message(project)
-    if project.loan_health_check.healthy?
+  def health_status_message(health_check)
+    if health_check.healthy?
       status_message = {
         status: "healthy",
         message: I18n.t("health_status.healthy"),
         warnings: []
       }
     else
-      warnings = project.loan_health_check.health_warnings.collect {|w| I18n.t("health_status.warnings.#{w}")}
+      warnings = health_check.health_warnings.collect {|w| I18n.t("health_status.warnings.#{w}")}
       status_message = {
         status: "unhealthy",
         message: I18n.t('health_status.unhealthy'),
