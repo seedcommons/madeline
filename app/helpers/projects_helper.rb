fe5cd5c6cd61ddbc12345c260fd6673335860470
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-05-26 12:48:47 -0400

    5802: Add color to icons

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -8,14 +8,14 @@ module ProjectsHelper
   def health_status_message(health_check)
     if health_check.healthy?
       status_message = {
-        icon: "fa-check-circle",
+        icon: "fa-check-circle healthy",
         message: I18n.t("health_status.healthy"),
         warnings: []
       }
     else
       warnings = health_check.health_warnings.map { |w| I18n.t("health_status.warnings.#{w}") }
       status_message = {
-        icon: "fa-exclamation-triangle",
+        icon: "fa-exclamation-triangle unhealthy",
         message: I18n.t('health_status.unhealthy'),
         warnings: warnings
       }
