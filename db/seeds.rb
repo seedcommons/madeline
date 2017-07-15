dab2a88161d03be4a1829e9d7eb633f52068452b
Adam Konner <linesarefuzzy@gmail.com>
2017-07-15 17:31:08 -0400

    6666 loan_transaction_type migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -100,6 +100,16 @@ media_kind.options.create(value: 'video', label_translations: {en: 'Video', es: 
 media_kind.options.create(value: 'document', label_translations: {en: 'Document', es: 'Documento'})
 media_kind.options.create(value: 'contract', label_translations: {en: 'Contract', es: 'Contrato'})
 
+loan_transaction_type = OptionSet.find_or_create_by(division: Division.root, model_type: Accounting::Transaction.name,
+  model_attribute: 'loan_transaction_type')
+loan_transaction_type.options.destroy_all
+loan_transaction_type.options.create(value: 'interest', position: 1,
+  label_translations: {en: 'Interest', es: 'Interés'})
+loan_transaction_type.options.create(value: 'disbursement', position: 2,
+  label_translations: {en: 'Disbursement', es: 'Desembolso'})
+loan_transaction_type.options.create(value: 'repayment', position: 3,
+  label_translations: {en: 'Repayment', es: 'Reembolso'})
+
 # Need to leave room for migrated loan questions
 # Can remove this line once migration is over with.
 LoanQuestion.recalibrate_sequence(id: 300)
