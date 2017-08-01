a77cc8587068f402a7916f7874540f58073d694b
Ijeoma Arisah <ijeomarisah@gmail.com>
2017-08-01 23:54:03 +0100

    set log notification from header

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -5,8 +5,20 @@ class NotificationMailer < ApplicationMailer
   #
   #   en.notification_mailer.new_log.subject
   #
+
+  def from_address(log)
+    if log.agent.email.present?
+      %Q("#{log.agent.name} <#{log.agent.email}>")
+    else
+      ApplicationMailer.default[:from]
+    end
+  end
+
   def new_log(log, user)
     @log = log
-    mail to: user.email, subject: I18n.t('notification_mailer.new_log.subject', project: log.project.display_name)
+    mail(to: user.email,
+         from: from_address(log) ,
+         subject: I18n.t('notification_mailer.new_log.subject', project: log.project.display_name)
+    )
   end
 end
