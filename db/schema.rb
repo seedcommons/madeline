0e81be3e11ca039cafc9f6277b06f3b6f32929a5
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-08-16 11:45:54 -0400

    6940: Make division id column be an integer and not null

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170816144924) do
+ActiveRecord::Schema.define(version: 20170816150506) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -192,7 +192,7 @@ ActiveRecord::Schema.define(version: 20170816144924) do
   create_table "loan_questions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string   "data_type"
-    t.string   "division_id"
+    t.integer  "division_id", null: false
     t.boolean  "has_embeddable_media", default: false, null: false
     t.string   "internal_name"
     t.integer  "loan_question_set_id"
