3256db5381f18f06f1c0b79ad8e0df4dc7866078
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-08-16 10:03:38 -0400

    6940: Pass current division to new question

--- app/controllers/admin/loan_questions_controller.rb
+++ app/controllers/admin/loan_questions_controller.rb
@@ -14,7 +14,8 @@ class Admin::LoanQuestionsController < Admin::AdminController
   def new
     set = LoanQuestionSet.find_by(internal_name: "loan_#{params[:set]}")
     parent = params[:parent_id].present? ? LoanQuestion.find(params[:parent_id]) : set.root_group
-    @loan_question = LoanQuestion.new(loan_question_set_id: set.id, parent: parent)
+    @loan_question = LoanQuestion.new(loan_question_set_id: set.id, parent: parent,
+      division: current_division)
     authorize @loan_question
     @loan_question.build_complete_requirements
     render_form
