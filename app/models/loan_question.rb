3bac29f68df1d2961157902897219cc75d784868
Adam Konner <linesarefuzzy@gmail.com>
2017-08-17 16:30:16 -0400

    6939 Move `required_for?` to decorator

--- app/models/loan_question.rb
+++ app/models/loan_question.rb
@@ -109,25 +109,6 @@ class LoanQuestion < ActiveRecord::Base
     !response_set.tree_unanswered?(self)
   end
 
-  # Resolves if this particular question is considered required for the provided loan, based on
-  # presence of association records in the loan_questions_options relation table, and the
-  # 'override_associations' flag.
-  # - If override is true and join records are present, question is required for those loan types
-  #   and optional for all others
-  # - If override is true and no records are present, all are optional
-  # - If override is false, inherit from parent
-  # - Top level nodes (those with depth = 1 are direct children of the root) effectively have
-  #   override always true
-  # Note, loan type association records are ignored for questions without the 'override_assocations'
-  # flag assigned.
-  def required_for?(loan)
-    if override_associations || depth == 1
-      loan_types.include?(loan.loan_type_option)
-    else
-      parent && parent.required_for?(loan)
-    end
-  end
-
   def name
     "#{loan_question_set.internal_name}-#{internal_name}"
   end
