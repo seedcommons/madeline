d02083172d6136455f94f0bd0bc54044900b5795
Nathan Lee <nathan@globalphobia.com>
2017-08-23 22:26:02 -0600

    6685 remove old comments

--- db/migrate/20170821035759_add_original_to_project.rb
+++ db/migrate/20170821035759_add_original_to_project.rb
@@ -1,8 +1,5 @@
 class AddOriginalToProject < ActiveRecord::Migration
   def change
-    add_column :projects, :original_id, :integer, polymorphic: true, foreign_key: { references: :projects}
-    # add_reference :projects, :original
-    # add_foreign_key :projects, :original, column: :original_id
-    # add_index :projects, [:id, :original_id]
+    add_column :projects, :original_id, :integer, foreign_key: { references: :projects }
   end
 end
