60031629e746edd9efa2b682fe9e05005193d4be
Tom Smyth <tom@sassafras.coop>
2017-08-31 16:21:36 -0400

    6928: Added more specs

--- app/jobs/recalculate_loan_health_job.rb
+++ app/jobs/recalculate_loan_health_job.rb
@@ -4,8 +4,8 @@ class RecalculateLoanHealthJob < ActiveJob::Base
   def perform(loan_id:)
     check = LoanHealthCheck.where(loan_id: loan_id).first
 
-    unless check
-      Rails.logger.debug "Creating health check for loan #{loan_id}"
+    if check.nil?
+      return unless Loan.where(id: loan_id).exists?
       check = LoanHealthCheck.create!(loan_id: loan_id)
     end
 
