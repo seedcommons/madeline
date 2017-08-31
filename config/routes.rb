5739508cec60df4d799661d84c2ec4d5d68b0cb6
Ijeoma Arisah <ijeomarisah@gmail.com>
2017-08-31 16:33:15 +0100

    add blank show page

--- config/routes.rb
+++ config/routes.rb
@@ -95,6 +95,7 @@ Rails.application.routes.draw do
     get '/basic-projects/:id/:tab' => 'basic_projects#show', as: 'basic_project_tab'
     get 'dashboard' => 'dashboard#dashboard', as: 'dashboard'
     get '/loans/:id/:tab' => 'loans#show', as: 'loan_tab'
+    get '/loans/:loan_id/transactions/:id' => 'accounting/transactions#show', as: 'loan_transaction'
   end
 
   localized do
