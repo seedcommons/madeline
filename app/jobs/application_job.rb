76c1782556bdca8dc55be9893dbdb34b712fe5a7
Tom Smyth <tom@sassafras.coop>
2017-09-01 10:33:56 -0400

    6928: Added comments about job retry stuff and set max_attempts to one

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,6 +1,9 @@
 class ApplicationJob < ActiveJob::Base
   queue_as :default
 
+  # Note: rescue_from appears to immediately delete the job from the queue, so DelayedJob's retry
+  # functionality will not work. If we need retries, consider using ActiveJob's retry_job method or
+  # https://github.com/isaacseymour/activejob-retry.
   rescue_from(StandardError) do |exception|
     ExceptionNotifier.notify_exception(exception, data: {job: to_yaml})
   end
