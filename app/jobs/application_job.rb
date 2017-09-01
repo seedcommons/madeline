ab8f27fd6da429ecb04edad9583a4116b4ed7747
Tom Smyth <tom@sassafras.coop>
2017-09-01 10:12:33 -0400

    6928: Added more data to error emails

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -2,12 +2,6 @@ class ApplicationJob < ActiveJob::Base
   queue_as :default
 
   rescue_from(StandardError) do |exception|
-    ExceptionNotifier.notify_exception(exception, data: error_report_data)
-  end
-
-  protected
-
-  def error_report_data
-    nil
+    ExceptionNotifier.notify_exception(exception, data: {job: to_yaml})
   end
 end
