9ef384379700c1751776a7f2a73007ccaf672560
Adam Konner <linesarefuzzy@gmail.com>
2017-09-13 19:24:25 -0400

    6939 Replace `@top_level_questions` with single `@root`

--- app/controllers/concerns/questionnaire_renderable.rb
+++ app/controllers/concerns/questionnaire_renderable.rb
@@ -5,7 +5,6 @@ module QuestionnaireRenderable
     @attrib = params[:filter] || "criteria"
     @response_set ||= @loan.send(@attrib) || LoanResponseSet.new(kind: @attrib, loan: @loan)
     @root = LoanFilteredQuestion.new(@response_set.loan_question_set.root_group_preloaded, loan: @loan)
-    @top_level_questions = @root.children
-    @questions_json = @top_level_questions.map { |q| LoanQuestionSerializer.new(q) }.to_json if json
+    @questions_json = @root.children.map { |q| LoanQuestionSerializer.new(q) }.to_json if json
   end
 end
