eff72c084e26eec0336a850212c4f1149d3d8ecd
Adam Konner <linesarefuzzy@gmail.com>
2017-10-24 15:31:04 -0400

    Revert "6878 interest calculator"

--- spec/factories/accounting_transactions.rb
+++ spec/factories/accounting_transactions.rb
@@ -1,11 +1,5 @@
 FactoryGirl.define do
   factory :accounting_transaction, class: 'Accounting::Transaction', aliases: [:journal_entry_transaction] do
-    # division is not an attribute here but we need to access the accounts associated
-    # this is only for some traits below
-    transient do
-      division nil
-    end
-
     sequence(:qb_id)
     qb_transaction_type 'JournalEntry'
     quickbooks_data { {} }
@@ -14,43 +8,5 @@ FactoryGirl.define do
     amount { Faker::Number.decimal(4, 2) }
     account
     project
-
-    trait :with_interest do
-      loan_transaction_type_value 'interest'
-      amount 3
-
-      after(:create) do |txn, evaluator|
-        create(:line_item, parent_transaction: txn, account: evaluator.division.interest_receivable_account,
-          posting_type: 'debit', amount: evaluator.amount)
-        create(:line_item, parent_transaction: txn, account: evaluator.division.interest_income_account,
-          posting_type: 'credit', amount: evaluator.amount)
-      end
-    end
-
-    trait :with_disbursement do
-      loan_transaction_type_value 'disbursement'
-      amount 100
-
-      after(:create) do |txn, evaluator|
-        create(:line_item, parent_transaction: txn, account: txn.account,
-          posting_type: 'credit', amount: evaluator.amount)
-        create(:line_item, parent_transaction: txn, account: evaluator.division.principal_account,
-          posting_type: 'debit', amount: evaluator.amount)
-      end
-    end
-
-    trait :with_repayment do
-      loan_transaction_type_value 'repayment'
-      amount 23.7
-
-      after(:create) do |txn, evaluator|
-        create(:line_item, parent_transaction: txn, account: txn.account,
-          posting_type: 'debit', amount: evaluator.amount)
-        create(:line_item, parent_transaction: txn, account: evaluator.division.interest_receivable_account,
-          posting_type: 'credit', amount: evaluator.amount.to_f / 2)
-        create(:line_item, parent_transaction: txn, account: evaluator.division.principal_account,
-          posting_type: 'credit', amount: evaluator.amount.to_f / 2)
-      end
-    end
   end
 end
