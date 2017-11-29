1435e40a0817094a5525acb21c51248ee4a6ef4b
Ijeoma Arisah <ijeomarisah@gmail.com>
2017-11-29 13:39:56 +0100

    fix story scope

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171030181830) do
+ActiveRecord::Schema.define(version: 20171128182754) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -190,7 +190,7 @@ ActiveRecord::Schema.define(version: 20171030181830) do
 
   create_table "loan_questions", force: :cascade do |t|
     t.datetime "created_at", null: false
-    t.string   "data_type"
+    t.string   "data_type", null: false
     t.integer  "division_id", null: false
     t.boolean  "has_embeddable_media", default: false, null: false
     t.string   "internal_name"
