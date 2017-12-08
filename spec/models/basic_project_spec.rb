6bddf43510b0f4d348d490ff7ccebd665c100822
Ijeoma Arisah <ijeomarisah@gmail.com>
2017-12-08 13:31:53 +0100

    update basic project model specs

--- spec/models/basic_project_spec.rb
+++ spec/models/basic_project_spec.rb
@@ -11,14 +11,16 @@ describe BasicProject, type: :model do
   end
 
   context 'primary and secondary agents' do
-    it 'raises error if agents are the same' do
-      error = 'The point person for this project cannot be the same as the second point person'
-      expect(p_1).not_to be_valid
-      expect(p_1.errors[:primary_agent].join).to match(error)
-    end
 
-    it 'does not raise error for different agents' do
-      expect(p_2).to be_valid
+    context 'create' do
+      it 'raises error if agents are the same' do
+        expect(p_1).not_to be_valid
+        expect(p_1.errors[:primary_agent].join).to match(error)
+      end
+
+      it 'does not raise error for different agents' do
+        expect(p_2).to be_valid
+      end
     end
   end
 end
--- spec/support/helpers/project_spec_helpers.rb
+++ spec/support/helpers/project_spec_helpers.rb
@@ -4,5 +4,17 @@ module ProjectSpecHelpers
     let(:person_2) { create(:person) }
     let(:p_1) { build(:basic_project, primary_agent_id: person_1.id, secondary_agent_id: person_1.id) }
     let(:p_2) { build(:basic_project, primary_agent_id: person_1.id, secondary_agent_id: person_2.id) }
+    let(:project) { create(:basic_project, primary_agent: person_1, secondary_agent: person_2) }
+    let(:error) { 'The point person for this project cannot be the same as the second point person' }
+
+    context 'update' do
+      it 'raises error if agents are the same' do
+        project.secondary_agent = person_1
+        project.save
+
+        expect(project).not_to be_valid
+        expect(project.errors[:primary_agent].join).to match(error)
+      end
+    end
   end
 end
