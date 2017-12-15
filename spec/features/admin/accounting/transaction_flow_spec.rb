16eb4efbc93a58093bb83d80433ede3a04d26c2b
Tom Smyth <tom@sassafras.coop>
2017-12-15 12:18:47 -0500

    7505: Refactored specs

--- spec/features/admin/accounting/transaction_flow_spec.rb
+++ spec/features/admin/accounting/transaction_flow_spec.rb
@@ -23,46 +23,43 @@ feature 'transaction flow' do
       OptionSetCreator.new.create_loan_transaction_type
     end
 
+    describe 'transaction listing' do
+      scenario 'with qb error during Updater' do
+        Rails.configuration.x.test.set_invalid_model_error = 'qb fail on index'
+        visit "/admin/loans/#{loan.id}/transactions"
+        expect(page).to have_alert('Some data may be out of date. (Error: qb fail on index)')
+      end
+    end
+
     describe 'new transaction' do
       # This spec does not test TransactionBuilder at all because stubbing out
       # all the necessary things was not practical at the time.
       # Eventually we should refactor the Quickbooks code such that stubbing is easier.
       scenario 'creates new transaction' do
         visit "/admin/loans/#{loan.id}/transactions"
-        fill_some_part_of_txn_form
-        fill_in 'Amount', with: '12.34'
+        fill_txn_form
         page.find('a[data-action="submit"]').click
         expect(page).to have_content('Palm trees')
         expect(page).to have_content("Interest Accrual for Loan ##{loan.id}")
       end
 
-      scenario 'with errors' do
+      scenario 'with validation error' do
         visit "/admin/loans/#{loan.id}/transactions"
-        fill_some_part_of_txn_form
+        fill_txn_form(omit_amount: true)
         page.find('a[data-action="submit"]').click
         expect(page).to have_content("Amount can't be blank")
       end
-    end
 
-    describe 'error handling' do
-      let!(:qb_model_error) { 'Some data may be out of date. (Error: qb model error)' }
-
-      scenario 'error thrown on index page load is displayed' do
-        Rails.configuration.x.test.set_invalid_model_error = 'qb model error'
-        visit "/admin/loans/#{loan.id}/transactions"
-        expect(page).to have_alert(qb_model_error)
-      end
-
-      scenario 'error thrown during transaction creation is displayed' do
+      scenario 'with qb error during Updater' do
         # This process should not create any transactions (disbursement OR interest)
         # because it errors out.
         expect do
           visit "/admin/loans/#{loan.id}/transactions"
-          fill_some_part_of_txn_form
-          fill_in 'Amount', with: '12.34'
-          Rails.configuration.x.test.set_invalid_model_error = 'qb model error'
+          fill_txn_form
+          Rails.configuration.x.test.set_invalid_model_error = 'qb fail on create'
           page.find('a[data-action="submit"]').click
-          expect(page).to have_alert(qb_model_error, container: '.transaction-form')
+          expect(page).to have_alert('Some data may be out of date. (Error: qb fail on create)',
+            container: '.transaction-form')
         end.to change { Accounting::Transaction.count }.by(0)
       end
     end
@@ -78,10 +75,11 @@ feature 'transaction flow' do
     end
   end
 
-  def fill_some_part_of_txn_form
+  def fill_txn_form(omit_amount: false)
     click_on 'Add Transaction'
     select 'Disbursement', from: 'Type of Transaction'
     fill_in 'Date', with: Date.today.to_s
+    fill_in 'Amount', with: '12.34' unless omit_amount
     select accounts.sample.name, from: 'Bank Account'
     fill_in 'Description', with: 'Palm trees'
     fill_in 'Memo', with: 'Chunky monkey'
