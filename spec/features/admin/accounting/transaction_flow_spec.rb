6b5bbe4db5a99f3d557c117e8262dbbc9496b982
Ijeoma Arisah <ijeomarisah@gmail.com>
2017-12-15 14:08:31 +0100

    add test for invalid txn

--- spec/features/admin/accounting/transaction_flow_spec.rb
+++ spec/features/admin/accounting/transaction_flow_spec.rb
@@ -29,26 +29,37 @@ feature 'transaction flow' do
       # Eventually we should refactor the Quickbooks code such that stubbing is easier.
       scenario 'creates new transaction' do
         visit "/admin/loans/#{loan.id}/transactions"
-        fill_txn_form
+        fill_some_part_of_txn_form
+        fill_in 'Amount', with: '12.34'
         page.find('a[data-action="submit"]').click
         expect(page).to have_content('Palm trees')
         expect(page).to have_content("Interest Accrual for Loan ##{loan.id}")
       end
+
+      scenario 'with errors' do
+        visit "/admin/loans/#{loan.id}/transactions"
+        fill_some_part_of_txn_form
+        page.find('a[data-action="submit"]').click
+        expect(page).to have_content("Amount can't be blank")
+      end
     end
 
     describe 'error handling' do
+      let!(:qb_model_error) { 'Some data may be out of date. (Error: qb model error)' }
+
       scenario 'error thrown on index page load is displayed' do
         Rails.configuration.x.test.set_invalid_model_error = 'qb model error'
         visit "/admin/loans/#{loan.id}/transactions"
-        expect(page).to have_alert('Some data may be out of date. (Error: qb model error)')
+        expect(page).to have_alert(qb_model_error)
       end
 
       scenario 'throws error in txn creation modal' do
         visit "/admin/loans/#{loan.id}/transactions"
-        fill_txn_form
+        fill_some_part_of_txn_form
+        fill_in 'Amount', with: '12.34'
         Rails.configuration.x.test.set_invalid_model_error = 'qb model error'
         page.find('a[data-action="submit"]').click
-        expect(page).to have_alert('Some data may be out of date. (Error: qb model error)')
+        expect(page).to have_alert(qb_model_error)
       end
     end
   end
@@ -63,12 +74,11 @@ feature 'transaction flow' do
     end
   end
 
-  def fill_txn_form
+  def fill_some_part_of_txn_form
     click_on 'Add Transaction'
     select 'Disbursement', from: 'Type of Transaction'
     fill_in 'Date', with: Date.today.to_s
     select accounts.sample.name, from: 'Bank Account'
-    fill_in 'Amount', with: '12.34'
     fill_in 'Description', with: 'Palm trees'
     fill_in 'Memo', with: 'Chunky monkey'
   end
