4d5a77528187211708bbd2c7473ce7a72a30a69a
Adam Konner <linesarefuzzy@gmail.com>
2017-12-24 01:40:40 -0500

    Add Rails version to migrations

--- db/migrate/20171030233947_add_default_qb_transaction_type.rb
+++ db/migrate/20171030233947_add_default_qb_transaction_type.rb
@@ -1,4 +1,4 @@
-class AddDefaultQbTransactionType < ActiveRecord::Migration
+class AddDefaultQbTransactionType < ActiveRecord::Migration[4.2]
   def change
     change_column_default :accounting_transactions, :qb_transaction_type, 'JournalEntry'
   end
--- db/migrate/20171115184346_add_null_false_to_qb_connections.rb
+++ db/migrate/20171115184346_add_null_false_to_qb_connections.rb
@@ -1,4 +1,4 @@
-class AddNullFalseToQbConnections < ActiveRecord::Migration
+class AddNullFalseToQbConnections < ActiveRecord::Migration[4.2]
   def change
     change_column_null :accounting_quickbooks_connections, :division_id, false
     change_column_null :accounting_quickbooks_connections, :realm_id, false
