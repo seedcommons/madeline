c1d104cb741715b1ccd0a3a387ca03bbae63623f
Tom Smyth <tom@sassafras.coop>
2018-01-31 16:22:24 -0500

    7796: Reorganized layouts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,6 @@ class StaticPagesController < ApplicationController
   include WordpressEmbeddable
 
   def test
-    render inline: "<% provide :title, 'Test' %><h1>Test Successful</h1>", layout: 'application'
+    render inline: "<% provide :title, 'Test' %><h1>Test Successful</h1>", layout: 'public/main'
   end
 end
