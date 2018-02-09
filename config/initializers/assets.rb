0c16651786d1619c5af06c704334526d9ff7f704
Tom Smyth <tom@sassafras.coop>
2018-02-09 16:10:12 -0500

    7796: Reorganized javascript

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,7 +10,8 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( raw_crud.css )
+Rails.application.config.assets.precompile += %w(admin.js public.js)
+
 Rails.application.config.assets.precompile += %w( admin/* )
 Rails.application.config.assets.precompile += %w( admin/*/* )
 Rails.application.config.assets.precompile += %w( embedded/* )
