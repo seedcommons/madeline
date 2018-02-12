724700f09fb5e1e17aea3d1c002f12247d038a73
Elizabeth Mitchell <elizabeth@sassafras.coop>
2018-02-12 07:08:23 -0500

    3690: Update syntax of repo_url

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,11 @@ lock '3.10.1'
 
 set :application, 'madeline'
 
+<<<<<<< Updated upstream
 set :repo_url, 'git@github.com:sassafrastech/madeline.git'
+=======
+set :repo_url, 'https://github.com/sassafrastech/madeline.git'
+>>>>>>> Stashed changes
 
 set :tmp_dir, '/home/deploy/tmp'
 
