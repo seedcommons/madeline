c71db4d8db952ab0f617478728502fc1fc0b3e06
Elizabeth Mitchell <elizabeth@sassafras.coop>
2018-03-07 09:13:09 -0500

    8109: Expand form elements inside full-width form to wider width

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -111,7 +111,7 @@ SimpleForm.setup do |config|
     b.use :label, class: 'col-sm-3 control-label'
 
     b.wrapper tag: 'div', class: 'col-sm-9' do |ba|
-      ba.use :input, class: 'form-element form-control inline'
+      ba.use :input, class: 'form-element form-control'
       ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
       ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
     end
