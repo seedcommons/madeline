a8c1c3a9ed01b4d4618285a160795ede58c9f86f
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-03-07 14:58:27 +0100

    remove string; haven't changed string to text on hash

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180130145027) do
+ActiveRecord::Schema.define(version: 20180307120347) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
