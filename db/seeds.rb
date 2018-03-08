d72fd6d9797d87af36c8e1a2d04f1d776b549dcf
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-03-08 14:07:13 +0100

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,11 +13,17 @@ Currency.find_or_create_by(id: 3,name: 'British Pound', code: 'GBP', symbol: 'GB
   short_symbol: '£', country_code: 'GB')
 Currency.find_or_create_by(id: 4,name: 'Nicaraguan Cordoba', code: 'NIO', symbol: 'NIC$',
   short_symbol: 'C$', country_code: 'NI')
+Currency.find_or_create_by(id: 5,name: 'Mexican Peso', code: 'MXN', symbol: 'MX$',
+  short_symbol: '$', country_code: 'MX')
+Currency.find_or_create_by(id: 6,name: 'Guatemalan Quetzal', code: 'GTQ', symbol: 'GTQ',
+  short_symbol: 'Q', country_code: 'GT')
 Currency.recalibrate_sequence
 
 Country.find_or_create_by(id: 1, name: 'Argentina', iso_code: 'AR', default_currency_id: 1)
 Country.find_or_create_by(id: 2, name: 'Nicaragua', iso_code: 'NI', default_currency_id: 4)
 Country.find_or_create_by(id: 3, name: 'United States', iso_code: 'US', default_currency_id: 2)
+Country.find_or_create_by(id: 4, name: 'Mexico', iso_code: 'MX', default_currency_id: 5)
+Country.find_or_create_by(id: 5, name: 'Guatemala', iso_code: 'GT', default_currency_id: 6)
 Country.recalibrate_sequence
 
 OptionSetCreator.new.create_all
