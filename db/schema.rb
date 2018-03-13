15f20ecb88310d3477a688b617fcdaa37b11864d
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-03-13 15:37:00 +0100

    add countries and currencies

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +13,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20180308114407) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -86,12 +87,12 @@ ActiveRecord::Schema.define(version: 20180308114407) do
 
   create_table "currencies", id: :serial, force: :cascade do |t|
     t.string "code"
+    t.string "country_code"
     t.datetime "created_at", null: false
     t.string "name"
     t.string "short_symbol"
     t.string "symbol"
     t.datetime "updated_at", null: false
-    t.string "country_code"
   end
 
   create_table "delayed_jobs", id: :serial, force: :cascade do |t|
