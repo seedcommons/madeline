6950e2c271f6f4a64da69c1df64247f799417b90
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-03-13 18:00:01 +0100

    fix currency in spanish

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -71,7 +71,9 @@ module AdminHelper
         class="currency_symbol" title="#{currency.try(:plural_name)}">#{display_symbol}</a>).html_safe
     end
 
-    return number_to_currency(amount, unit: display_symbol, negative_format: "(%u%n)")
+    # since we want to display all amounts with the symbols in front and dot notation
+    # we're adding locale: :en to set it to the English format by default
+    number_to_currency(amount, unit: display_symbol, negative_format: "(%u%n)", locale: :en)
   end
 
   # This should be updated whenever columns are added/removed to the timeline table
