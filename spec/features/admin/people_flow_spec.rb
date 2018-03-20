be6c2a05efb21a4765b61ae5f05b0a3089b01702
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-03-20 17:22:54 +0100

    fix

--- spec/features/admin/people_flow_spec.rb
+++ spec/features/admin/people_flow_spec.rb
@@ -7,7 +7,7 @@ feature 'people flow' do
   let(:user_2) { create_member(division) }
   let!(:person_1) { user_1.profile }
   let!(:person_2) { user_2.profile }
-  let!(:log) { create(:project_log, agent: person_1) }
+  let!(:log) { create(:project_log, agent: person_2) }
 
   before do
     login_as(user_1, scope: :user)
@@ -32,20 +32,5 @@ feature 'people flow' do
     # person no longer exists
     expect(page).to have_content('Record was successfully deleted.')
     expect(page).not_to have_content(person_2.first_name)
-  end
-
-  scenario 'person with log should show warning before deletion' do
-    visit admin_people_path
-
-    # delete actiopn
-    click_on person_1.id
-    click_on 'Delete Member'
-
-    error_msg = 'This member has logs. If you choose to delete the member, the logs will remain with no author.
-                If you wish to keep the member as the author of the logs, do not delete this member. Instead,
-                if the member is no longer active, uncheck the "Has system access" option.'
-
-    # person no longer exists
-    expect(page).to have_content(error_msg)
   end
 end
