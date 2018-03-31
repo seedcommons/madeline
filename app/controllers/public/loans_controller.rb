7e1f08736bcf2233c5ffb84dfca6631c51514a31
Melody <meltheadorable@gmail.com>
2018-03-30 21:18:12 -0400

    7799: wrap up basics

--- app/controllers/public/loans_controller.rb
+++ app/controllers/public/loans_controller.rb
@@ -1,5 +1,6 @@
 class Public::LoansController < Public::PublicController
   include WordpressEmbeddable
+  after_action :verify_authorized
 
   def index
     params[:division] = get_division_from_url
@@ -8,6 +9,7 @@ class Public::LoansController < Public::PublicController
           includes(:organization, division: :parent).
           page(params[:pg]).per(20).
           order('signing_date DESC'))
+    authorize @loans
     @countries = Country.order(:iso_code).pluck(:iso_code)
 
 
@@ -16,15 +18,19 @@ class Public::LoansController < Public::PublicController
   end
 
   def show
-    @loan = Loan.status('all').find(params[:id])
+    @loan = Loan.find(params[:id])
     @pictures = @loan.featured_pictures(5) # for slideshow
-    @other_loans = @loan.cooperative.loans.status('all').order("SigningDate DESC") if @loan.cooperative
-    @repayments = @loan.repayments.order('DateDue')
+    @other_loans = policy_scope(Loan.related_loans(@loan))
+    
+    authorize @loan
+    # @other_loans = @loan.cooperative.loans.status('all').order("SigningDate DESC") if @loan.cooperative
+    # @repayments = @loan.repayments.order('DateDue')
   end
 
   def gallery
-    @loan = Loan.status('all').find(params[:id])
+    @loan = Loan.find(params[:id])
     @coop_media = @loan.coop_media(100, true).in_groups_of(4, false)
     @loan_media = (@loan.loan_media(100, true) + @loan.log_media(100, true)).in_groups_of(4, false)
+    authorize @loan
   end
 end
