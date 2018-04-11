4f887009e51b275009a2b1e2118e956622683e6c
Elizabeth Mitchell <elizabeth@sassafras.coop>
2018-04-11 08:38:59 -0400

    8223: Adjust migration

--- db/migrate/20180331230509_add_loan_public_level_options.rb
+++ db/migrate/20180331230509_add_loan_public_level_options.rb
@@ -2,17 +2,25 @@ class AddLoanPublicLevelOptions < ActiveRecord::Migration[5.1]
   def up
     loan_public_level = OptionSet.find_or_create_by(division: Division.root, model_type: Loan.name,
       model_attribute: 'public_level')
-    loan_public_level.options.destroy_all
-    loan_public_level.options.create(value: 'featured', position: 1,
-      label_translations: {en: 'Featured', es: 'Destacado'})
-    loan_public_level.options.create(value: 'hidden', position: 2,
-      label_translations: {en: 'Hidden', es: 'Oculto'})
+    loan_public_level.options.each do |option|
+      update_values(option)
+      option.save
+    end
   end
 
   def down
-    loan_public_level = OptionSet.find_or_create_by(division: Division.root, model_type: Loan.name,
-      model_attribute: 'public_level')
-    loan_public_level.options.destroy_all
-    loan_public_level.destroy
+  end
+
+  private
+
+  def update_values(o)
+    case o.value
+      when 'featured'
+        o.label_translations.en = 'Featured'
+        o.label_translations.es ='Destacado'
+      when 'hidden'
+        o.label_translations.en = 'Hidden'
+        o.label_translations.es = 'Oculto'
+    end
   end
 end
