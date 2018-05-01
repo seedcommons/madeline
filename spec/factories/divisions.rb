5e0adcd0c79787145742e159a4ce0005dea2ba1a
Melody <meltheadorable@gmail.com>
2018-05-01 15:55:27 -0400

    8359: Fix specs

--- spec/factories/divisions.rb
+++ spec/factories/divisions.rb
@@ -57,6 +57,7 @@ FactoryBot.define do
     description { Faker::Lorem.sentence }
     name { Faker::Address.city }
     parent { root_division }
+    public true
     sequence(:short_name) { |n| "#{Faker::Lorem.word}-#{n}" }
 
     trait :with_accounts do
