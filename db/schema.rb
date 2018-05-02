e67fa053d89ab270912811be8575241a170482fa
Adam Konner <linesarefuzzy@gmail.com>
2018-05-02 16:23:42 -0400

    Set projects with blank (but not nil) public level to public

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180501183920) do
+ActiveRecord::Schema.define(version: 20180502201923) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
