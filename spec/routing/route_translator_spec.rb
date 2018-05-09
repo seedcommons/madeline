24e02e395713322c3c8ccf9171339b3d9df46d42
Adam Konner <linesarefuzzy@gmail.com>
2018-05-09 19:57:47 -0400

    8044 Add tests for public loans pages

--- spec/routing/route_translator_spec.rb
+++ spec/routing/route_translator_spec.rb
@@ -27,4 +27,33 @@ describe RouteTranslator do
       site: "nicaragua",
     )
   end
+
+  it "should route US loans page correctly" do
+    expect(get "/us/loans").to route_to(
+      controller: "public/loans",
+      action: "index",
+      locale: "en",
+      site: "us",
+    )
+  end
+
+  it "should route Nicaragua loans page correctly" do
+    expect(get "/nicaragua/prestamos").to route_to(
+      controller: "public/loans",
+      action: "index",
+      locale: "es",
+      site: "nicaragua",
+    )
+  end
+
+  it "should route Nicaragua gallery page correctly" do
+    expect(get "/nicaragua/prestamos/123/galeria").to route_to(
+      controller: "public/loans",
+      action: "gallery",
+      locale: "es",
+      site: "nicaragua",
+      id: "123",
+    )
+  end
+
 end
