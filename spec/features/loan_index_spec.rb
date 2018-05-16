8fa5168f2bd63c06b19cdca71eddfe36ad984b81
Melody <meltheadorable@gmail.com>
2018-05-16 10:57:20 -0400

    8340: Show loan descendants

--- spec/features/loan_index_spec.rb
+++ spec/features/loan_index_spec.rb
@@ -1,20 +1,23 @@
 require 'rails_helper'
 
 feature 'visit loan index page' do
-  before do
-    @loan_1 = create :loan, :active, :featured, name: 'First loan'
-    @loan_2 = create :loan, :active, :featured, name: 'Second loan'
-    @loans =  [@loan_1, @loan_2]
-    create(:division, name: 'United States', short_name: 'us', loans: [@loan_1])
-    create(:division, name: 'pokemon', short_name: 'pikachu', loans: [@loan_2])
-    create(:division, name: 'kale', short_name: 'kk', public: false)
-  end
+  let!(:loan_us) { create(:loan, :active, :featured, name: 'US loan') }
+  let!(:loan_pkmn) { create(:loan, :active, :featured, name: 'Pokémon loan') }
+  let!(:loan_pika) { create(:loan, :active, :featured, name: 'Pikachu Loan') }
+  let!(:loan_kale) { create(:loan, :active, :featured, name: 'Kale Loan') }
+
+  let!(:div_us) { create(:division, name: "United States", short_name: "us", loans: [loan_us]) }
+  let!(:div_pkmn) { create(:division, name: "Pokémon", short_name: "pkmn", loans: [loan_pkmn]) }
+  let!(:div_pika) { create(:division, name: "Pikachu", short_name: "pika", loans: [loan_pika], parent: div_pkmn) }
+  let!(:div_kale) { create(:division, name: "Kale", short_name: "kale", loans: [loan_kale], public: false) }
+
+  let!(:loans) { [loan_us, loan_pkmn, loan_pika, loan_kale] }
 
   context 'on the loan index page' do
     before { visit public_loans_path("us", division: "all") }
 
     it 'shows active loans' do
-      active_loans = @loans.select{ |loan| loan.status_value == 'active' }
+      active_loans = loans.select{ |loan| loan.status_value == 'active' }.reject{ |loan| loan.name == 'Kale Loan' }
       expect(active_loans).to be_present
       active_loans.each do |loan|
         check_loan_content(loan)
@@ -22,11 +25,11 @@ feature 'visit loan index page' do
     end
 
     context 'with both active and completed loans' do
-      before { @loans = create_list(:loan, 10, :featured) }
+      let!(:loans) { create_list(:loan, 10, :featured, division: div_us) }
 
       it 'shows completed loans on their tab' do
         click_link 'Completed'
-        completed_loans = @loans.select{ |loan| loan.status_value == 'completed' }
+        completed_loans = loans.select{ |loan| loan.status_value == 'completed' }
         completed_loans.each do |loan|
           check_loan_content(loan)
         end
@@ -34,14 +37,14 @@ feature 'visit loan index page' do
 
       it 'shows all loans on all tab' do
         click_link 'All'
-        @loans.each do |loan|
+        loans.each do |loan|
           check_loan_content(loan)
         end
       end
     end
 
     context 'with many loans' do
-      before { @loans = create_list(:loan, 25, :active, :featured) }
+      let!(:loans) { create_list(:loan, 25, :active, :featured, division: div_us) }
       it 'paginates loan list' do
         visit public_loans_path("us", division: 'all')
         expect(page).to have_selector('div.pagination ul.pagination li a[rel="next"]')
@@ -51,20 +54,22 @@ feature 'visit loan index page' do
     end
 
     context 'with translations' do
-      before { @loans = create_list(:loan, 3, :with_translations, :featured) }
+      let!(:loans) { create_list(:loan, 3, :with_translations, :featured, division: div_us) }
+
       it 'renders translated loan description' do
         click_link 'All'
-        @loans.each do |loan|
+        loans.each do |loan|
           expect(page).to have_content loan.summary
         end
       end
     end
 
     context 'with no local translations' do
-      before { @loans = create_list(:loan, 3, :with_foreign_translations, :featured) }
+      let!(:loans) { create_list(:loan, 3, :with_foreign_translations, :featured, division: div_us) }
+
       it 'renders loan description with translation hint' do
         click_link 'All'
-        @loans.each do |loan|
+        loans.each do |loan|
           expect(page).to have_content loan.summary
         end
         expect(page).to have_selector '.loans_items span.translation.foreign_language', count: 3
@@ -72,31 +77,48 @@ feature 'visit loan index page' do
     end
 
     context 'with divisions' do
-      scenario 'filters with division' do
-        visit public_loans_path("us", division: 'us')
-        expect(page).not_to have_content(@loan_2.name)
-        expect(page).to have_content(@loan_1.name)
+      context 'with us division' do
+        scenario 'filters loans from other divisions' do
+          visit public_loans_path("us", division: 'us')
+          expect(page).not_to have_content(loan_pkmn.name)
+          expect(page).not_to have_content(loan_kale.name)
+          expect(page).to have_content(loan_us.name)
+        end
+      end
 
-        # when another division is filtered
-        visit public_loans_path("us", division: 'pikachu')
-        expect(page).not_to have_content(@loan_1.name)
-        expect(page).to have_content(@loan_2.name)
+      context 'with pkmn division' do
+        scenario 'includes only descendants' do
+          # when another division is filtered
+          visit public_loans_path("us", division: 'pkmn')
+          expect(page).not_to have_content(loan_us.name)
+          expect(page).not_to have_content(loan_kale.name)
+          expect(page).to have_content(loan_pkmn.name)
+          expect(page).to have_content(loan_pika.name)
+        end
+      end
 
-        # when 'all' is selected
-        visit public_loans_path("us", division: 'all')
-        expect(page).to have_content(@loan_2.name)
-        expect(page).to have_content(@loan_1.name)
+      context 'with all divisions' do
+        scenario 'only include public divisions' do
+          visit public_loans_path("us", division: 'all')
+          expect(page).to have_content(loan_pkmn.name)
+          expect(page).to have_content(loan_us.name)
+          expect(page).to have_content(loan_pika.name)
+          expect(page).not_to have_content(loan_kale.name)
+        end
+      end
 
-        # when no division is selected, site division is default
-        visit public_loans_path("us")
-        expect(page).not_to have_content(@loan_2.name)
-        expect(page).to have_content(@loan_1.name)
+      context 'with no selection' do
+        scenario 'use site division by default' do
+          visit public_loans_path("us")
+          expect(page).not_to have_content(loan_pkmn.name)
+          expect(page).to have_content(loan_us.name)
+        end
       end
     end
 
     context 'show only public divisions on dropdown' do
       scenario 'non-public divisions do not show' do
-        expect(page.all('select#division option').map(&:value)).to eq %w(all us pikachu)
+        expect(page.all('select#division option').map(&:value)).to eq %w(all us pkmn pika)
       end
     end
   end
