5c5183f30de8e9f3ba5946bffec85f92348af8af
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-07-13 14:30:57 +0100

    add failing spec

--- spec/features/admin/division_flow_spec.rb
+++ spec/features/admin/division_flow_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 feature 'division flow' do
 
-  let(:division) { create(:division) }
-  let(:user) { create_admin(division) }
+  let(:division) { create(:division, name: 'Cream') }
+  let!(:jay_division) { create(:division, name: 'Jayita', parent: division) }
+  let(:person) { create(:person, :with_admin_access, :with_password) }
+  let(:user) { person.user }
 
   before do
     login_as(user, scope: :user)
@@ -11,5 +13,11 @@ feature 'division flow' do
 
   include_examples :flow do
     subject { division }
+  end
+
+  scenario 'division name does not show in parent division dropdown' do
+    visit admin_division_path(division)
+    find('.edit-action').click
+    expect(page).to have_select('division_parent_id', visible: true, options: %w(- Jayita))
   end
 end
