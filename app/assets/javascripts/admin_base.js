1873f6c5de53a023f56d20c7e67d8815a0393507
Elizabeth Mitchell <elizabeth@sassafras.coop>
2018-07-13 11:55:16 -0400

    8240: Add base JS file
    jQuery needs to be loaded in both the public and admin sites for
    the JS files to work properly.

--- app/assets/javascripts/admin_base.js
+++ app/assets/javascripts/admin_base.js
@@ -1,7 +1,4 @@
 // JavaScript files that need to be loaded into the admin site only before other JS files
-// jQuery and Bootstrap are already included in the public site
-
-//= require jquery
-//= require jquery_ujs
+// Bootstrap is already included in the public site
 
 //= require bootstrap-sprockets
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w(admin_base.js admin.js public.js admin.css public.css)
+Rails.application.config.assets.precompile += %w(base.js admin_base.js admin.js public.js admin.css public.css)
