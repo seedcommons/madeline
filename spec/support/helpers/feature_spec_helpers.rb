4504075bfe38a6dac580afbb31fb1b3f7754f45c
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-08-03 16:55:06 +0100

    remove unused method from feature_spec_helpers.rb

--- spec/support/helpers/feature_spec_helpers.rb
+++ spec/support/helpers/feature_spec_helpers.rb
@@ -1,21 +1,9 @@
 module FeatureSpecHelpers
-  def reload_page
-    page.evaluate_script("window.location.reload()")
-  end
-
-  # Waits for the loading indicator to appear and then disappear.
+# Waits for the loading indicator to appear and then disappear.
   def wait_for_loading_indicator
     expect(page).to have_content('Loading...')
     expect(page).not_to have_content('Loading...')
     expect(page).not_to have_css('div#glb-load-ind')
-  end
-
-  # Fills in the given value into the box with given ID, then selects the first matching option.
-  # Assumes a dropdown-style select2 box. Works with a remote data source.
-  def select2(value, from:)
-    execute_script("$('##{from}').select2('open')")
-    find(".select2-search__field").set(value)
-    find(".select2-results li", text: /#{value}/).click
   end
 
   def have_alert(msg, container: 'body')
