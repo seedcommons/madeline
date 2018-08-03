b2dbfaba9bed56d450ff829de0a9903bfff66281
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-08-03 17:28:10 +0100

    remove unused method from media_item_uploader.rb

--- app/uploaders/media_item_uploader.rb
+++ app/uploaders/media_item_uploader.rb
@@ -4,7 +4,6 @@ class MediaItemUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   IMAGE_REGEX = %r{\Aimage\/.*\z}i
-  VIDEO_REGEX = %r{\Avideo\/.*\z}i
 
   # the kind of storage to use for this uploader
   storage :file
@@ -25,11 +24,11 @@ class MediaItemUploader < CarrierWave::Uploader::Base
   def store_dir
     owner_type = model.media_attachable_type.underscore
     owner_id = model.media_attachable_id
-    path = File.join("uploads", Rails.env, owner_type, "#{owner_id}", "#{model.id}")
-    path
+    File.join("uploads", Rails.env, owner_type, "#{owner_id}", "#{model.id}")
   end
 
   protected
+
   def image?(new_file)
     new_file.content_type =~ IMAGE_REGEX
   end
@@ -44,31 +43,4 @@ class MediaItemUploader < CarrierWave::Uploader::Base
       model.item_width, model.item_height = ::MiniMagick::Image.open(file.file)[:dimensions]
     end
   end
-
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
-
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Add a whitelist of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
 end
