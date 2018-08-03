4c9b78dd5af1a68950b0a6fa89472f0200320337
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-08-03 18:29:09 +0100

    remove unused model repayment

--- spec/models/repayment_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-describe Repayment, :type => :model do
-  before { pending 're-implement in new project' }
-  it 'has a valid factory' do
-    expect(create(:repayment)).to be_valid
-  end
-end
