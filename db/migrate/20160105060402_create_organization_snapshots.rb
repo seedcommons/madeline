41b0a90027d9e1cf4db4b4004b5200e14c8abe32
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-08-06 12:51:58 +0100

    remove todo in old migration in 20160105060402_create_organization_snapshots.rb

--- db/migrate/20160105060402_create_organization_snapshots.rb
+++ db/migrate/20160105060402_create_organization_snapshots.rb
@@ -2,7 +2,6 @@ class CreateOrganizationSnapshots < ActiveRecord::Migration
   def change
     create_table :organization_snapshots do |t|
       # note, disabling this foreign key to ease migration handling.
-      # todo: consider dropping all foreign keys until schema is stabilized
       t.references :organization, index: true, foreign_key: false
       t.date :date
       t.integer :organization_size
