0d4b5afbd2029abc98e529f480dec926547080aa
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-08-07 17:39:05 +0100

    more refactoring

--- app/helpers/currency_helper.rb
+++ app/helpers/currency_helper.rb
@@ -1,11 +1,10 @@
 module CurrencyHelper
   def format_currency(amount, currency, tooltip: true)
-    currency_name = currency_name(currency, count: 2)
     display_symbol = currency ? "#{currency.country_code} #{currency.short_symbol}" : ''
 
     if tooltip
       display_symbol = %Q(<a href="#" onclick="return false" data-toggle="tooltip"
-        class="currency_symbol" title="#{currency_name}">#{display_symbol}</a>).html_safe
+        class="currency_symbol" title="#{currency_name(currency, count: 2)}">#{display_symbol}</a>).html_safe
     end
 
     # since we want to display all amounts with the symbols in front and dot notation
