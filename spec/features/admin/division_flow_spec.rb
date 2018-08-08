bcb25e078103d191cbc5031171f25499be5b6d7d
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-08-08 19:28:34 +0100

    add failing spec

--- spec/features/admin/division_flow_spec.rb
+++ spec/features/admin/division_flow_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'division flow' do
 
-  let(:division) { create(:division, name: 'Cream') }
+  let!(:division) { create(:division, name: 'Cream', short_name: 'cream') }
   let(:person) { create(:person, :with_admin_access, :with_password) }
   let(:user) { person.user }
 
@@ -20,5 +20,14 @@ feature 'division flow' do
     select 'Cream', from: 'division_parent_id'
     click_on 'Update Division'
     expect(page).to have_content('Division and Parent Division cannot be the same')
+  end
+
+  scenario 'divisions can not have duplicate short names' do
+    visit admin_divisions_path
+    click_on 'New Division'
+    fill_in 'Name', with: 'Jay'
+    fill_in 'Short name', with: 'cream'
+    click_on 'Create Division'
+    expect(page).to have_content('Errors prevented the record from being saved')
   end
 end
