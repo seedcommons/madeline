84d58cc6927a477e2a1f5053cf222fdbee6120be
Elizabeth Mitchell <elizabeth@sassafras.coop>
2018-08-21 15:50:30 -0400

    8759: Limit date range for filtered steps

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -51,7 +51,10 @@ class Admin::DashboardController < Admin::AdminController
     # TODO: Filter steps by current division. See ProjectLog in_division and policy scopes.
     # There is no existing project step filter by division that I saw so far.
     @assigned_steps = ProjectStep.where(agent_id: @person.id)
-    @filtered_assigned_steps = @assigned_steps.where(is_finalized: true, actual_end_date: nil)
+    # Show only project steps with a scheduled start date one year before today's date up
+    # one month after today's date. Extremely late steps are not displayed.
+    @filtered_assigned_steps = @assigned_steps.where(is_finalized: true, actual_end_date: nil,
+      scheduled_start_date: (Time.now.midnight - 1.years)..(Time.now.midnight + 1.month))
   end
 
   private
