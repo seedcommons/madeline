3e5e14d82e0a643a13f1c1d83ba30864612f060f
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-08-21 21:04:36 +0100

    adjust failing spec

--- spec/features/admin/documentation_spec.rb
+++ spec/features/admin/documentation_spec.rb
@@ -4,7 +4,7 @@ feature 'documentation' do
   let(:user) { create_admin(create(:division)) }
 
   scenario 'creation' do
-    visit new_documentation_path(caller: 'loans#new', html_identifier: 'food')
+    visit new_admin_documentation_path(caller: 'loans#new', html_identifier: 'food')
 
     expect(page).to have_field('HTML Identifier', with: 'food')
   end
