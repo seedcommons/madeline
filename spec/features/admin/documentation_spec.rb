8d016f25db6b955fe6513961dedb8cc2518cc12d
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-08-22 12:42:28 +0100

    add failing spec for assigning controller and action name

--- spec/features/admin/documentation_spec.rb
+++ spec/features/admin/documentation_spec.rb
@@ -10,5 +10,9 @@ feature 'documentation' do
 
     # html identifier is pre-filled
     expect(page).to have_field('HTML Identifier', with: 'food')
+
+    # calling actions are pre-filled
+    expect(page).to have_field('Calling Controller', with: 'loans')
+    expect(page).to have_field('Calling Action', with: 'new')
   end
 end
