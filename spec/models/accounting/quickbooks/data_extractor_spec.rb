62596c5ceaa628ba1ad2ea05acb4e63b19b846a0
Fuzzy Konner <linesarefuzzy@gmail.com>
2018-08-22 17:13:04 -0400

    8701 Add other transaction types to test

--- spec/models/accounting/quickbooks/data_extractor_spec.rb
+++ spec/models/accounting/quickbooks/data_extractor_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 describe Accounting::Quickbooks::DataExtractor, type: :model do
   context '#extract!' do
-    it "calls the right extractor class" do
-      txn = create(:accounting_transaction, qb_object_type: "JournalEntry")
-      expect(Accounting::Quickbooks::TransactionExtractor).to receive(:new).with(txn)
-      described_class.new(txn)
+    %w(JournalEntry Purchase Deposit Bill).each do |obj_type|
+      it "calls the right extractor class for #{obj_type}" do
+        txn = create(:accounting_transaction, qb_object_type: obj_type)
+        expect(Accounting::Quickbooks::TransactionExtractor).to receive(:new).with(txn)
+        described_class.new(txn)
+      end
     end
   end
 end
