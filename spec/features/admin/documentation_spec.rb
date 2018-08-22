0da956b675672b93be49eb26e02fa24c87866573
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-08-22 12:59:10 +0100

    add failing spec for filling in content

--- spec/features/admin/documentation_spec.rb
+++ spec/features/admin/documentation_spec.rb
@@ -8,11 +8,20 @@ feature 'documentation' do
   scenario 'creation' do
     visit new_admin_documentation_path(caller: 'loans#new', html_identifier: 'food')
 
-    # html identifier is pre-filled
+    # fields are pre-filled correctly
     expect(page).to have_field('HTML Identifier', with: 'food')
-
-    # calling actions are pre-filled
     expect(page).to have_field('Calling Controller', with: 'loans')
     expect(page).to have_field('Calling Action', with: 'new')
+
+    # add content
+    fill_in 'Summary Content', with: 'This is my sample summary content'
+    fill_in 'Page Content', with: 'This is my sample page content'
+
+    click_on 'Save'
+
+    # after saving, the contents are saved correctly
+    visit admin_documentation_path(Documentation.last)
+    expect(page).to have_field('Summary Content', with: 'This is my sample summary content')
+    expect(page).to have_field('Page Content', with: 'This is my sample page content')
   end
 end
