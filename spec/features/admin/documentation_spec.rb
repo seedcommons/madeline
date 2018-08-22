dec67b7bd1a419e546b6cd5fd5abde3683031cc0
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-08-22 12:39:39 +0100

    make html identifier prefill test pass

--- spec/features/admin/documentation_spec.rb
+++ spec/features/admin/documentation_spec.rb
@@ -3,9 +3,12 @@ require 'rails_helper'
 feature 'documentation' do
   let(:user) { create_admin(create(:division)) }
 
+  before { login_as user }
+
   scenario 'creation' do
     visit new_admin_documentation_path(caller: 'loans#new', html_identifier: 'food')
 
+    # html identifier is pre-filled
     expect(page).to have_field('HTML Identifier', with: 'food')
   end
 end
