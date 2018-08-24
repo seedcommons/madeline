2bc7e1aa77f5278fea26e82132ce00f001023037
Elizabeth Mitchell <elizabeth@sassafras.coop>
2018-08-24 09:33:33 -0400

    8759: Adjust variables

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -50,10 +50,10 @@ class Admin::DashboardController < Admin::AdminController
     @context = "dashboard"
     # TODO: Filter steps by current division. See ProjectLog in_division and policy scopes.
     # There is no existing project step filter by division that I saw so far.
-    @assigned_steps = ProjectStep.where(agent_id: @person.id)
-    # Show only project steps with a scheduled start date one year before today's date up
-    # one month after today's date. Extremely late steps are not displayed.
-    @filtered_assigned_steps = @assigned_steps.where(is_finalized: true, actual_end_date: nil,
+    assigned_steps = ProjectStep.where(agent_id: @person.id)
+    # Show only incomplete project steps with a scheduled start date one year before today's date
+    # up to one month after today's date. Extremely late steps are not displayed.
+    @filtered_assigned_steps = assigned_steps.where(is_finalized: true, actual_end_date: nil,
       scheduled_start_date: (Time.now.midnight - 1.years)..(Time.now.midnight + 1.month))
     @recent_project_steps_grid = initialize_grid(
       @filtered_assigned_steps,
