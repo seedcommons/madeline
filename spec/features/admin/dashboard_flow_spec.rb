953646d269f2592dc7ded0ad8b759e04bb29447f
Elizabeth Mitchell <elizabeth@sassafras.coop>
2018-08-24 12:30:09 -0400

    8759: Add tests for assigned steps in dashboard

--- spec/features/admin/dashboard_flow_spec.rb
+++ spec/features/admin/dashboard_flow_spec.rb
@@ -17,11 +17,24 @@ feature 'dashboard flow' do
       expect(page).to have_content('Project Steps Assigned to Me')
     end
 
+    scenario "show no assigned steps" do
+      visit admin_dashboard_path
+      expect(page).to have_content('There are no project steps assigned to you.')
+    end
+
     scenario "show assigned steps" do
-      step1 = create(:project_step, agent: @person, project: @loan1, scheduled_start_date: Date.today - 2.years)
-      step2 = create(:project_step, agent: @person, project: @loan2, scheduled_start_date: Date.today - 1.month)
-      step3 = create(:project_step, agent: @person, project: @loan2, scheduled_start_date: Date.today + 2.weeks)
-      step4 = create(:project_step, agent: @person, project: @loan1, scheduled_start_date: Date.today + 6.months)
+      step1 = create(:project_step, agent: @person, project: @loan1, scheduled_start_date: Date.today - 2.years, is_finalized: true, actual_end_date: nil)
+      step2 = create(:project_step, agent: @person, project: @loan1, scheduled_start_date: Date.today - 1.month, is_finalized: true, actual_end_date: nil)
+      step3 = create(:project_step, agent: @person, project: @loan1, scheduled_start_date: Date.today + 2.weeks, is_finalized: true, actual_end_date: nil)
+      step4 = create(:project_step, agent: @person, project: @loan1, scheduled_start_date: Date.today + 6.months, is_finalized: true, actual_end_date: nil)
+
+      visit admin_dashboard_path
+
+      expect(page).to have_content(step2.summary)
+      expect(page).to have_content(step3.summary)
+
+      expect(page).not_to have_content(step1.summary)
+      expect(page).not_to have_content(step4.summary)
     end
   end
 end
