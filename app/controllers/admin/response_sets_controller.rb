bf713f09495cf077eef82e8d9a857d1cc45195ea
Fuzzy Konner <linesarefuzzy@gmail.com>
2018-08-31 14:39:27 -0400

    6921 Display error when conflicting set exists

--- app/controllers/admin/response_sets_controller.rb
+++ app/controllers/admin/response_sets_controller.rb
@@ -3,10 +3,27 @@ class Admin::ResponseSetsController < Admin::AdminController
 
   def create
     @response_set = ResponseSet.new(current_user: current_user)
-    @response_set.assign_attributes(response_set_params)
+    @response_set.assign_attributes(response_set_params.merge(updater_id: current_user.id))
     authorize @response_set
-    @response_set.save!
-    redirect_to display_path, notice: I18n.t(:notice_created)
+
+    # Check if loan already has a response set (e.g. created in another tab)
+    @conflicting_response_set = @response_set.loan.send(@response_set.kind)
+    if @conflicting_response_set
+      @response_set_from_db = {
+        updater: @conflicting_response_set.updater,
+        updated_at: @conflicting_response_set.updated_at,
+        lock_version: @conflicting_response_set.lock_version,
+      }
+
+      @conflict = true
+      @tab = 'questions'
+      @loan = @response_set.loan
+      prep_questionnaire
+      render 'admin/loans/show'
+    else
+      @response_set.save!
+      redirect_to display_path, notice: I18n.t(:notice_created)
+    end
   end
 
   def update
