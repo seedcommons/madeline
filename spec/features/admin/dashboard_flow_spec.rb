dfc766d0966545c1142a46ba101d5bdc52713f25
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-09-05 20:44:47 +0100

    refactor spec

--- spec/features/admin/dashboard_flow_spec.rb
+++ spec/features/admin/dashboard_flow_spec.rb
@@ -1,15 +1,13 @@
 require 'rails_helper'
 
 feature 'dashboard flow' do
-  before do
-    @division = create(:division)
-    @user = create_member(@division)
-    @person = Person.find(@user.profile_id)
-    @loan1 = create(:loan, division: @division)
-    @loan2 = create(:loan, division: create(:division))
+  let(:division) { create(:division) }
+  let(:user) { create_member(division) }
+  let(:person) { Person.find(user.profile_id) }
+  let(:loan) { create(:loan, division: division) }
+  let(:loan2) { create(:loan, division: create(:division)) }
 
-    login_as(@user, scope: :user)
-  end
+  before { login_as(user, scope: :user) }
 
   describe "assigned steps" do
     scenario "section exists on page" do
@@ -23,16 +21,16 @@ feature 'dashboard flow' do
     end
 
     scenario "show assigned steps", js: true do
-      step1 = create(:project_step, agent: @person, project: @loan1, scheduled_start_date: Date.today - 2.years, is_finalized: true, actual_end_date: nil)
-      step2 = create(:project_step, agent: @person, project: @loan2, scheduled_start_date: Date.today - 1.month, is_finalized: true, actual_end_date: nil)
-      step3 = create(:project_step, agent: @person, project: @loan1, scheduled_start_date: Date.today + 2.weeks, is_finalized: true, actual_end_date: nil)
-      step4 = create(:project_step, agent: @person, project: @loan1, scheduled_start_date: Date.today + 6.months, is_finalized: true, actual_end_date: nil)
+      step1 = create(:project_step, agent: person, project: loan, scheduled_start_date: Date.today - 2.years, is_finalized: true, actual_end_date: nil)
+      step2 = create(:project_step, agent: person, project: loan2, scheduled_start_date: Date.today - 1.month, is_finalized: true, actual_end_date: nil)
+      step3 = create(:project_step, agent: person, project: loan, scheduled_start_date: Date.today + 2.weeks, is_finalized: true, actual_end_date: nil)
+      step4 = create(:project_step, agent: person, project: loan, scheduled_start_date: Date.today + 6.months, is_finalized: true, actual_end_date: nil)
 
 
 
       visit admin_dashboard_path
       # Change to specific division, and ensure the page reloads properly
-      select_division(@division.name)
+      select_division(division.name)
 
       # visit admin_dashboard_path
 
