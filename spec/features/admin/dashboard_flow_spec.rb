f90199ca8802058599f9e8b66703ad214adb18eb
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-09-05 20:55:33 +0100

    refactor spec

--- spec/features/admin/dashboard_flow_spec.rb
+++ spec/features/admin/dashboard_flow_spec.rb
@@ -7,38 +7,39 @@ feature 'dashboard flow' do
   let(:loan) { create(:loan, division: division) }
   let(:loan2) { create(:loan, division: create(:division)) }
 
-  before { login_as(user, scope: :user) }
+  before do
+    login_as(user, scope: :user)
+    visit admin_dashboard_path
+  end
 
   describe "assigned steps" do
     scenario "section exists on page" do
-      visit admin_dashboard_path
       expect(page).to have_content('Project Steps Assigned to Me')
     end
 
     scenario "show no assigned steps" do
-      visit admin_dashboard_path
       expect(page).to have_content('There are no project steps assigned to you.')
     end
 
-    scenario "show assigned steps", js: true do
-      step1 = create(:project_step, agent: person, project: loan, scheduled_start_date: Date.today - 2.years, is_finalized: true, actual_end_date: nil)
-      step2 = create(:project_step, agent: person, project: loan2, scheduled_start_date: Date.today - 1.month, is_finalized: true, actual_end_date: nil)
-      step3 = create(:project_step, agent: person, project: loan, scheduled_start_date: Date.today + 2.weeks, is_finalized: true, actual_end_date: nil)
-      step4 = create(:project_step, agent: person, project: loan, scheduled_start_date: Date.today + 6.months, is_finalized: true, actual_end_date: nil)
+    describe "steps" do
+      # steps are already created
+      let!(:step1) { create(:project_step, agent: person, project: loan, scheduled_start_date: Date.today - 2.years, is_finalized: true, actual_end_date: nil) }
+      let!(:step2) { create(:project_step, agent: person, project: loan2, scheduled_start_date: Date.today - 1.month, is_finalized: true, actual_end_date: nil) }
+      let!(:step3) { create(:project_step, agent: person, project: loan, scheduled_start_date: Date.today + 2.weeks, is_finalized: true, actual_end_date: nil) }
+      let!(:step4) { create(:project_step, agent: person, project: loan, scheduled_start_date: Date.today + 6.months, is_finalized: true, actual_end_date: nil) }
 
+      scenario "show assigned steps", js: true do
+        # Change to specific division, and ensure the page reloads properly
+        select_division(division.name)
 
+        expect(page).to have_content(step3.summary)
 
-      visit admin_dashboard_path
-      # Change to specific division, and ensure the page reloads properly
-      select_division(division.name)
+        expect(page).not_to have_content(step1.summary)
+        expect(page).not_to have_content(step4.summary)
 
-      # visit admin_dashboard_path
-
-      expect(page).not_to have_content(step2.summary)
-      expect(page).to have_content(step3.summary)
-
-      expect(page).not_to have_content(step1.summary)
-      expect(page).not_to have_content(step4.summary)
+        # different division from selected division
+        expect(page).not_to have_content(step2.summary)
+      end
     end
   end
 end
