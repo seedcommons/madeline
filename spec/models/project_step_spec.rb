de9de967bce1900fb75a2332804c3f82b92d2177
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-09-17 16:21:50 +0100

    add failing spec for project step

--- spec/models/project_step_spec.rb
+++ spec/models/project_step_spec.rb
@@ -63,13 +63,17 @@ describe ProjectStep, type: :model do
         end
       end
     end
-    # summary, scheduled start date and details
+
     it 'can not save without scheduled start date' do
       expect{ create(:project_step, scheduled_start_date: nil) }.to raise_error(ActiveRecord::RecordInvalid)
     end
 
     it 'can not save without summary' do
       expect{ create(:project_step, summary: nil) }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'can not save without details' do
+      expect{ create(:project_step, details: nil) }.to raise_error(ActiveRecord::RecordInvalid)
     end
   end
 
