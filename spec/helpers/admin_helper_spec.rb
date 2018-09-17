2df610919dfa81e4db550294e480086369b67ed6
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-09-17 17:33:52 +0100

    fix some failing spec

--- spec/helpers/admin_helper_spec.rb
+++ spec/helpers/admin_helper_spec.rb
@@ -5,8 +5,8 @@ describe AdminHelper, type: :helper do
     let(:root) { create(:root_project_group, :with_descendants) }
 
     it "should return space-padded label text" do
-      g1 = root.filtered_children[0]
-      g1_s1 = root.filtered_children[0].filtered_children[0]
+      g1 = root.filtered_children[1]
+      g1_s1 = g1.filtered_children[0]
 
       expect(indented_option_label(root, :summary_or_none)).to eq "[None]"
       expect(indented_option_label(g1, :summary_or_none)).to eq g1.summary.to_s
