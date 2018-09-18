0bbc726ffa566419641df537818742de1009efff
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-09-18 14:29:00 +0100

    remove unneeded code

--- app/models/project_duplicator.rb
+++ app/models/project_duplicator.rb
@@ -39,7 +39,7 @@ class ProjectDuplicator
   end
 
   def copy_schedule_info
-    scheduled_entries = step_map.keys.select(&:has_date?).sort_by(&:scheduled_start_date)
+    scheduled_entries = step_map.keys.reject { |k| k.is_a?(ProjectGroup) }.sort_by(&:scheduled_start_date)
 
     scheduled_entries.each do |old_step|
       new_step = step_map[old_step]
