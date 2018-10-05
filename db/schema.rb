67b7bd052c8348c95775ae2cf48393b2ce05ffd8
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-10-05 14:34:14 +0100

    add migration file to make default featured image

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_06_135259) do
+ActiveRecord::Schema.define(version: 2018_11_06_135911) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
