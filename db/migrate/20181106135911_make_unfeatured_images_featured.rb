2091b1941d71abaeda570af04dcbcb2c0121d090
Melody <meltheadorable@gmail.com>
2018-11-12 10:52:31 -0500

    8883: Fixes migration

--- db/migrate/20181106135911_make_unfeatured_images_featured.rb
+++ db/migrate/20181106135911_make_unfeatured_images_featured.rb
@@ -2,7 +2,7 @@ class MakeUnfeaturedImagesFeatured < ActiveRecord::Migration[5.2]
   def up
     Loan.find_each do |loan|
       loan_images = loan&.media&.images_only
-      loan_images.first.update(featured: true) if loan_images&.none?(&:featured)
+      loan_images&.first&.update(featured: true) if loan_images&.none?(&:featured)
     end
   end
 
