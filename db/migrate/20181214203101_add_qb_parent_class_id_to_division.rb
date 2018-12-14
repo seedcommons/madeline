33bd51899d4092c1e53ab3bcb414e889de22207f
Devney <devneyhamilton@gmail.com>
2018-12-14 15:37:37 -0500

    8031: migration to add qb_parent_class_id to division

---
+++ db/migrate/20181214203101_add_qb_parent_class_id_to_division.rb
@@ -0,0 +1,9 @@
+# Quickbooks has a 'class' attribute on transactions. These can have parent classes.
+# Madeline assigns each transaction it makes a quickbooks class, and that
+# class is assigned the parent class associated with the division. 
+
+class AddQbParentClassIdToDivision < ActiveRecord::Migration[5.2]
+  def change
+    add_column :divisions, :qb_parent_class_id, :string
+  end
+end
