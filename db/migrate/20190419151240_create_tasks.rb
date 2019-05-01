1258ca97a582d14c2e9ed46ff4cbe52c35c0b342
Devney <devneyhamilton@gmail.com>
2019-05-01 14:38:00 -0400

    9573: update task migration to have num_attempts and change job_started_at to job_first_started_at

--- db/migrate/20190419151240_create_tasks.rb
+++ db/migrate/20190419151240_create_tasks.rb
@@ -5,11 +5,11 @@ class CreateTasks < ActiveRecord::Migration[5.2]
       t.string :provider_job_id # unknown at creation
       t.string :job_type_value, null: false, limit: 255
       t.string :activity_message_value, null: false, limit:  64.kilobytes # may have backtraces
-      t.datetime :job_started_at
+      t.datetime :job_first_started_at
       t.datetime :job_last_failed_at
       t.datetime :job_enqueued_for_retry_at
       t.datetime :job_succeeded_at
-      t.boolean :job_dead, null: false, default: false
+      t.integer :num_attempts, null: false, default: 0
     end
   end
 end
