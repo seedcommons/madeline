9b3caa5b556f03d87a62358aa116e56d9a4f53ac
Devney <devneyhamilton@gmail.com>
2019-05-03 16:54:32 -0400

    9573: pass job params to task for enqueuing, define class inside taskjob spec

--- spec/factories/task_factory.rb
+++ spec/factories/task_factory.rb
@@ -16,8 +16,8 @@
 
 FactoryBot.define do
   factory :task do
-    job_type_value { "recalculate_loan_health_job" }
-    activity_message_value { "recalculating loan health" }
-    job_class { RecalculateLoanHealthJob }
+    job_type_value { "monthly_interest_accrual_job" }
+    activity_message_value { "running monthly interest accrual" }
+    job_class { MonthlyInterestAccrualJob }
   end
 end
--- spec/jobs/task_job_spec.rb
+++ spec/jobs/task_job_spec.rb
@@ -1,28 +1,41 @@
 require "rails_helper"
 
 describe TaskJob do
-  let(:task) { create(:task, job_class: job_class) }
+  let(:task) { create(:task, job_class: Class) }
 
   describe "#perform" do
+
     context "job succeeds" do
-      let(:job_class) { TestJob }
+      subject(:task_job) do
+        Class.new(described_class) do
+          def perform(task_data, *args)
+          end
+        end
+      end
+
       it "task is started" do
         # use expect any instance of because the task is necessarily reloaded from db in the job callback
         expect_any_instance_of(Task).to receive(:start)
-        task.job_class.constantize.perform_now(task_id: task.id)
+        subject.perform_now(task_id: task.id)
       end
 
       it "task succeeds" do
-        task.job_class.constantize.perform_now(task_id: task.id)
+        subject.perform_now(task_id: task.id)
         expect(task.reload.status).to eq :succeeded
       end
     end
 
     context "job fails" do
-      let(:job_class) { TestFailureJob }
+      subject(:task_job) do
+        Class.new(described_class) do
+          def perform(task_data, *args)
+            raise StandardError
+          end
+        end
+      end
       it "records failure" do
         expect_any_instance_of(Task).to receive(:record_failure)
-        expect {task.job_class.constantize.perform_now(task_id: task.id)}.to raise_error StandardError
+        expect {subject.perform_now(task_id: task.id)}.to raise_error StandardError
       end
     end
   end
