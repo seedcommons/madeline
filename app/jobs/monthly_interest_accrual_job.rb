9d6d5c2ee40b4dc1b0420d9882315df76f4e65e1
Devney <devneyhamilton@gmail.com>
2019-05-15 14:18:32 -0400

    9724: underscore job_params to indicate not used in perform

--- app/jobs/monthly_interest_accrual_job.rb
+++ app/jobs/monthly_interest_accrual_job.rb
@@ -1,5 +1,5 @@
 class MonthlyInterestAccrualJob < TaskJob
-  def perform(job_params)
+  def perform(_job_params)
     # The updater should only be run for active loans in divisions connected to QuickBooks.
     divisions = Division.qb_accessible_divisions
     loans = divisions.map { |i| i.loans.active }.flatten.compact
