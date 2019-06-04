a4d6680d3cf0169a6b056fb2e6552fb700e2afcd
Devney <devneyhamilton@gmail.com>
2019-06-04 15:19:27 -0400

    9669: custom input field appends percentage appropriately on edit

---
+++ app/inputs/percentage_input.rb
@@ -0,0 +1,21 @@
+class PercentageInput < SimpleForm::Inputs::NumericInput
+  def input(wrapper_options = nil)
+      # copied from source
+      input_html_classes.unshift("numeric")
+      if html5?
+        input_html_options[:type] ||= "number"
+        input_html_options[:step] ||= integer? ? 1 : "any"
+      end
+
+      merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
+      # end copied from source
+
+      # out = ActiveSupport::SafeBuffer.new
+      # out << @builder.text_field(attribute_name, merged_input_options)
+      # out << @builder.label("%", class: "percentage_input")
+      out = ActiveSupport::SafeBuffer.new
+      out << @builder.label(attribute_name, class: "percentage_input") {
+        @builder.text_field(attribute_name, merged_input_options) + " %"
+      }
+  end
+end
