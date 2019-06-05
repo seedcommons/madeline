8ed073bc77c0b5c3203a2a85bfc18a0d89f21e26
Devney <devneyhamilton@gmail.com>
2019-06-05 09:08:48 -0400

    9669: custom percentage input is working

--- app/inputs/percentage_input.rb
+++ app/inputs/percentage_input.rb
@@ -1,22 +1,23 @@
 class PercentageInput < SimpleForm::Inputs::NumericInput
   def input(wrapper_options = nil)
-      # copied from source
-      input_html_classes.unshift("numeric")
-      if html5?
-        input_html_options[:type] ||= "number"
-        input_html_options[:step] ||= integer? ? 1 : "any"
-      end
+    # copied from source NumericInput (https://github.com/plataformatec/simple_form/blob/master/lib/simple_form/inputs/numeric_input.rb)
+    input_html_classes.unshift("numeric")
+    if html5?
+      input_html_options[:type] ||= "number"
+      input_html_options[:step] ||= integer? ? 1 : "any"
+    end
 
-      merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
-      # end copied from source
+    merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
+    # end copied from source
+    template.content_tag(:div, class: 'percentage-input form-element') do
+      template.concat @builder.text_field(attribute_name, merged_input_options)
+      template.concat span_percentage
+    end
+  end
 
-      # out = ActiveSupport::SafeBuffer.new
-      # out << @builder.text_field(attribute_name, merged_input_options)
-      # out << @builder.label("%", class: "percentage_input")
-      out = ActiveSupport::SafeBuffer.new
-      # including form-element class means all of this is hidden on edit
-      out << @builder.label(attribute_name, class: "percentage_input form-element") {
-        @builder.text_field(attribute_name, merged_input_options) + " %"
-      }
+  def span_percentage
+    template.content_tag(:span, class: 'percentage') do
+      template.concat "%"
+    end
   end
 end
