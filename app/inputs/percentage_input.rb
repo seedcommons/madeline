9dc56c8996f3dab590dce72185354095f4e69187
Devney <devneyhamilton@gmail.com>
2019-06-05 11:10:34 -0400

    9669: generalize css class names, shift margin to postfix span instead of input

--- app/inputs/percentage_input.rb
+++ app/inputs/percentage_input.rb
@@ -10,14 +10,14 @@ class PercentageInput < SimpleForm::Inputs::NumericInput
     merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
     # end copied from source
 
-    template.content_tag(:div, class: 'percentage-input form-element') do
+    template.content_tag(:div, class: 'input-with-postfix form-element') do
       template.concat @builder.text_field(attribute_name, merged_input_options)
       template.concat span_percentage
     end
   end
 
   def span_percentage
-    template.content_tag(:span, class: 'percentage') do
+    template.content_tag(:span, class: 'input-postfix') do
       template.concat "%"
     end
   end
