94c0b549633973c7c0a92cd9baa8136f43b7de72
Devney <devneyhamilton@gmail.com>
2019-06-05 12:04:17 -0400

    9743: passing prefix and postfix to custom input works

---
+++ app/inputs/decorated_numeric_input.rb
@@ -0,0 +1,26 @@
+class DecoratedNumericInput < SimpleForm::Inputs::NumericInput
+  def input(wrapper_options = nil)
+    # copied from source NumericInput (https://github.com/plataformatec/simple_form/blob/master/lib/simple_form/inputs/numeric_input.rb)
+    input_html_classes.unshift("numeric")
+    if html5?
+      input_html_options[:type] ||= "number"
+      input_html_options[:step] ||= integer? ? 1 : "any"
+    end
+
+    merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
+    # end copied from source
+
+    prefix = options[:prefix]
+    postfix = options[:postfix]
+
+    template.content_tag(:div, class: 'decorated-numeric-input form-element') do
+      template.concat(template.content_tag(:span, class: 'input-prefix') do
+        template.concat prefix
+      end)
+      template.concat @builder.text_field(attribute_name, merged_input_options)
+      template.concat(template.content_tag(:span, class: 'input-postfix') do
+        template.concat postfix
+      end)
+    end
+  end
+end
