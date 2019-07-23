840071895be163248a3a8a00e811d02bcd2baf5c
Devney <devneyhamilton@gmail.com>
2019-07-23 09:45:21 -0400

    9872: fix translations for headers, increase pagination to 20

--- app/controllers/admin/tasks_controller.rb
+++ app/controllers/admin/tasks_controller.rb
@@ -2,6 +2,6 @@ class Admin::TasksController < Admin::AdminController
   def index
     authorize :'task', :index?
     @params = {status: params[:status], pg: params[:pg]}
-    @tasks = Task.all.page(params[:pg]).per(3).order('created_at DESC')
+    @tasks = Task.all.page(params[:pg]).per(20).order('created_at DESC')
   end
 end
