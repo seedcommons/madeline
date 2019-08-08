170b0b65a17d36ae5d1f1b7602e213c3192cb331
Devney <devneyhamilton@gmail.com>
2019-08-08 14:05:36 -0400

    9876: update documentations behavior such that a user may see documentations that belong to their division or any of its descendents or ancestors but may only edit a documentation if any of the user's divisions match the its division or any of its ancestors

--- app/policies/documentation_policy.rb
+++ app/policies/documentation_policy.rb
@@ -1,6 +1,12 @@
 class DocumentationPolicy < ApplicationPolicy
   def show?
-    # show if any of the user's divisions match the record's division or any of its ancestors
+    # a user may see documentations that belong to any of their divisions or any ancestors of their division
+    user_divisions = user.accessible_division_ids + user.division.self_and_ancestor_ids
+    user_divisions.include?(record.division.id)
+  end
+
+  def update?
+    # allow edit if any of the user's divisions match the record's division or any of its ancestors
     user_divisions = user.accessible_division_ids
     record_divisions = record&.division&.self_and_ancestor_ids
     intersection = (user_divisions & record_divisions)
