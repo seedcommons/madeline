e5f02254aaebdefbda7b8d6c7eb0d7f1e4904c2e
Devney <devneyhamilton@gmail.com>
2019-08-15 15:31:22 -0400

    10008: undo updated version in schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_24_161606) do
+ActiveRecord::Schema.define(version: 2019_06_11_164918) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
