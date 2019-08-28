a4e53649021e6ab875b946a6dc2a7ddec4c17cb9
Devney <devneyhamilton@gmail.com>
2019-08-28 12:12:47 -0400

    10009: more finegrained activity messages in update_all_loans task, translate status values

--- app/jobs/update_all_loans_job.rb
+++ app/jobs/update_all_loans_job.rb
@@ -2,9 +2,10 @@ class UpdateAllLoansJob < TaskJob
   def perform(job_params)
     errors_by_loan = []
     loans = Loan.all
-    Sidekiq::Logging.logger.info("There are #{loans.count} loans")
     updater = Accounting::Quickbooks::Updater.new
+    Task.find(job_params[:task_id]).update_attribute(:activity_message_value, "syncing_with_quickbooks")
     updater.qb_sync_for_loan_update
+    Task.find(job_params[:task_id]).update_attribute(:activity_message_value, "updating_all_loans")
     loans.each do |loan|
       begin
         updater.update_loan(loan)
@@ -13,7 +14,6 @@ class UpdateAllLoansJob < TaskJob
         next
       end
     end
-    Sidekiq::Logging.logger.info(errors_by_loan.to_s)
     Task.find(job_params[:task_id]).update_attributes(
       custom_error_data: errors_by_loan,
       activity_message_value: "finished_with_custom_error_data"
