e1b456c40b7bf74e6636fb0641f101e69d08ad1f
Devney <devneyhamilton@gmail.com>
2019-09-24 11:52:49 -0400

    10015: ensure data export service creates csv even if a data export error is raised

--- app/services/data_export_service.rb
+++ app/services/data_export_service.rb
@@ -1,6 +1,11 @@
 class DataExportService
   def self.run(data_export)
-    data_export.process_data
+    begin
+      data_export.process_data
+    rescue DataExportError => e
+      data_export.to_csv!
+      raise e
+    end
     data_export.to_csv!
   end
 end
--- spec/services/data_export_service_spec.rb
+++ spec/services/data_export_service_spec.rb
@@ -9,5 +9,22 @@ describe DataExportService do
       expect(data_export.reload.data.present?).to be true
       expect(data_export.attachments.size).to be 1
     end
+
+    context "data export error is raised" do
+      before do
+        expect(data_export).to receive(:process_data) do
+          data_export.update(data: [
+            ['name', 'date', 'description'],
+            ['Cool Loan', '2013-07-09', 'A loan for a cool thing that was a good idea to fund']
+          ])
+          raise DataExportError
+        end
+      end
+
+      it "creates csv and re-raises error" do
+        expect { DataExportService.run(data_export) }.to raise_error(DataExportError)
+        expect(data_export.attachments.size).to be 1
+      end
+    end
   end
 end
