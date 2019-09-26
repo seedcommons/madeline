8ec467a8cae8a845731fb9942220789e7527af61
Devney <devneyhamilton@gmail.com>
2019-09-26 16:27:45 -0400

    10191: don't raise error from perform if child errors are captured. Improve activity message when data export is running.

--- app/jobs/data_export_task_job.rb
+++ app/jobs/data_export_task_job.rb
@@ -2,11 +2,11 @@ class DataExportTaskJob < TaskJob
   def perform(job_params)
     data_export = DataExport.find(job_params[:data_export_id])
     begin
+      task_for_job(self).set_activity_message("data_export_in_progress")
       ::DataExportService.run(data_export)
     rescue DataExportError => e
       task_for_job(self).update(custom_error_data: e.child_errors)
       task_for_job(self).set_activity_message("finished_with_custom_error_data")
-      raise e
     end
   end
 end
