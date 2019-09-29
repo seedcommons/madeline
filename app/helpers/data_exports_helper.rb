bb82b24259a2b23c8be69bbe4e49bc3663a3d7d8
Elizabeth Mitchell <elizabeth@sassafras.coop>
2019-09-29 14:35:34 -0400

    10018: Refine helper

--- app/helpers/data_exports_helper.rb
+++ app/helpers/data_exports_helper.rb
@@ -1,15 +1,14 @@
 module DataExportsHelper
   def display_attachments_list(data_export)
-    text = ""
-
     if data_export.attachments.length > 0
+      text = ""
       media_count = 1
       data_export.attachments.each do |attachment|
-        text += "<p><a href='#{attachment.item}'>#{t('data_export.file', n: media_count)}</a></p>"
-        media_count  += 1
+        text += "<p><a href='#{attachment.item}'>#{t('data_export.file', n: media_count.to_s)}"
+        text += "</a></p>"
+        media_count += 1
       end
+      return text.html_safe
     end
-
-    return text.html_safe
   end
 end
