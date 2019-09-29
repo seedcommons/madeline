908e8406dd237226d5be7e6523ba3e6404204dd3
Elizabeth Mitchell <elizabeth@sassafras.coop>
2019-09-29 14:05:30 -0400

    10018: Refine display of attachments

--- app/helpers/data_exports_helper.rb
+++ app/helpers/data_exports_helper.rb
@@ -1,16 +1,13 @@
 module DataExportsHelper
   def display_attachments_list(data_export)
     text = ""
+
     if data_export.attachments.length > 0
       media_count = 1
-      text = "<ul>"
       data_export.attachments.each do |attachment|
-        text += "<li><a href='#{attachment.item}'>File #{media_count}</a></li>"
+        text += "<p><a href='#{attachment.item}'>File #{media_count}</a></p>"
         media_count  += 1
       end
-      text += "</ul>"
-    else
-      text = "No Attachments"
     end
 
     return text.html_safe
