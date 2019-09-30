1eca4b55e3514e939b13d711e8ff24420b1dc7c7
Devney <devneyhamilton@gmail.com>
2019-09-30 16:26:59 -0400

    10016: generalize flow spec identifiers

--- spec/features/admin/data_export_flow_spec.rb
+++ spec/features/admin/data_export_flow_spec.rb
@@ -9,8 +9,8 @@ feature 'data export flow' do
   scenario "create export with custom name and choose dates" do
     visit new_admin_data_export_path
     click_on "Standard Loan Data Export"
-    fill_in 'Start date', with: Time.zone.today.beginning_of_year.to_s
-    fill_in 'End date', with: Time.zone.today.to_s
+    fill_in 'data_export_start_date', with: Time.zone.today.beginning_of_year.to_s
+    fill_in 'data_export_end_date', with: Time.zone.today.to_s
     fill_in 'Name', with: "Test"
     expect(page).to have_field('Locale code', with: 'en')
     click_on "Create Data export"
