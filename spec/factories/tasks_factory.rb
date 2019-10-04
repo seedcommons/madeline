f1705d27f1904f14f3210ad5bc10cd47d9cf6091
melody <meltheadorable@gmail.com>
2019-10-04 15:16:26 -0400

    10215: Tasks UX Tweaks

--- spec/factories/tasks_factory.rb
+++ spec/factories/tasks_factory.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :task do
-    job_type_value { "monthly_interest_accrual_job" }
+    job_type_value { "monthly_interest_accrual" }
     activity_message_value { "test_activity_message_value" }
     job_class { MonthlyInterestAccrualJob }
   end
