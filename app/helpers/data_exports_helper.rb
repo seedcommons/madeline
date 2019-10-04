c14fd451afcced59cbd0a8a9b20ef3d7172b016a
Elizabeth Mitchell <elizabeth@sassafras.coop>
2019-10-04 13:40:17 -0400

    10207: Adjust attachment link

--- app/helpers/data_exports_helper.rb
+++ app/helpers/data_exports_helper.rb
@@ -3,12 +3,12 @@ module DataExportsHelper
     if data_export.attachments.length > 0
       text = ""
       data_export.attachments.each do |attachment|
-        text += "<p><a href='#{attachment.item}'>"
+        text += "<p class='attachment'><a href='#{attachment.item}'>"
         text += "<i class='fa fas fa-download'></i>"
         text += "<span>#{media_title(attachment, shorten: true, length: 8)}</span>"
         text += "</a></p>"
       end
-      return sanitize(text, tags: %w(a p i), attributes: %w(class href))
+      return sanitize(text, tags: %w(a p i span), attributes: %w(class href))
     end
   end
 end
