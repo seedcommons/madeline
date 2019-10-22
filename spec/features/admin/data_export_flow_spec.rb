fe7ae55d5460911b2d7361f2876b23fd96fea938
DevneyHamilton <devneyhamilton@gmail.com>
2019-10-22 11:57:25 -0400

    10221: update feature spec to use index page first

--- spec/features/admin/data_export_flow_spec.rb
+++ spec/features/admin/data_export_flow_spec.rb
@@ -8,7 +8,9 @@ feature 'data export flow' do
   scenario "create export with custom name and choose dates and choose a different locale" do
     start_date = Time.zone.today.beginning_of_year
     end_date = Time.zone.today
-    visit new_admin_data_export_path
+    visit admin_data_exports_path
+    click_on "New Data Export"
+
     # TODO: find better way to test the special-case behavior when there is only one type
     # click_on "Standard Loan Data Export"
     fill_in 'data_export_start_date', with: Time.zone.today.beginning_of_year.to_s
@@ -16,7 +18,7 @@ feature 'data export flow' do
     fill_in 'Name', with: "Test"
     expect(page).to have_field('Locale code', with: 'en')
     select "es", from: 'Locale code'
-    click_on "Create Data export"
+    click_on "Create Data export" #TODO get Elizabeth's help making this upper case in markup, not just css
     expect(page).to have_content "Successfully queued data export."
     expect(page).to have_content "Test"
     expect(page).to have_content "Pending"
@@ -29,13 +31,8 @@ feature 'data export flow' do
 
   scenario "dates and name are optional and name has reasonable default" do
     visit new_admin_data_export_path
-    # TODO: find better way to test the special-case behavior when there is only one type
-    # click_on "Standard Loan Data Export"
     expect(page).to have_field('Locale code', with: 'en')
-    click_on "Create Data Export"
-
-    # TODO replace with show page check
-    saved_data_export = DataExport.first
-    expect(saved_data_export.name).to include("Data Export")
+    click_on "Create Data export"
+    expect(page).to have_content('Standard Loan Data Export on')
   end
 end
