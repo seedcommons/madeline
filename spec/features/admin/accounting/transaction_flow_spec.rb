d5b6c051c86f82a80901e0f1d4a1819debf1cf45
Elizabeth Mitchell <elizabeth@sassafras.coop>
2020-01-15 11:49:02 -0500

    9877: Fix tests

--- spec/features/admin/accounting/transaction_flow_spec.rb
+++ spec/features/admin/accounting/transaction_flow_spec.rb
@@ -49,7 +49,7 @@ feature 'transaction flow', :accounting do
         visit "/admin/loans/#{loan.id}/transactions"
         fill_txn_form(omit_amount: true)
         page.find('a[data-action="submit"]').click
-        expect(page).to have_content("Amount can't be blank")
+        expect(page).to have_content("Amount #{loan.currency.code} can't be blank")
       end
 
       scenario 'with qb error during Updater' do
@@ -84,7 +84,7 @@ feature 'transaction flow', :accounting do
     click_on 'Add Transaction'
     select 'Disbursement', from: 'Type of Transaction'
     fill_in 'Date', with: Date.today.to_s
-    fill_in 'Amount', with: '12.34' unless omit_amount
+    fill_in 'accounting_transaction[amount]', with: '12.34' unless omit_amount
     select accounts.sample.name, from: 'Bank Account'
     fill_in 'Description', with: 'Palm trees'
     fill_in 'Memo', with: 'Chunky monkey'
