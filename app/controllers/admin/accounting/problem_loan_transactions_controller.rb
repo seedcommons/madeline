1c98990d4b7a8cb33e6fcce0050298125064cc42
DevneyHamilton <devneyhamilton@gmail.com>
2020-02-27 09:57:42 -0500

    10481: add translations and update titles of problem loan transaction index pages

--- app/controllers/admin/accounting/problem_loan_transactions_controller.rb
+++ app/controllers/admin/accounting/problem_loan_transactions_controller.rb
@@ -4,10 +4,11 @@ module Admin
       def index
         authorize :'accounting/problem_loan_transaction', :index?
         if params[:loan_id]
-          txn_ids = ::Accounting::ProblemLoanTransaction.where(project_id: params[:loan_id]).map(&:accounting_transaction_id)
+          @loan_id = params[:loan_id]
+          txn_ids = ::Accounting::ProblemLoanTransaction.where(project_id: @loan_id).map(&:accounting_transaction_id)
           @plts = ::Accounting::ProblemLoanTransaction.where(accounting_transaction_id: txn_ids).group_by(&:txn_id)
         else
-          @plts = ::Accounting::ProblemLoanTransaction.all.group_by(&:project_id)
+          @plts = ::Accounting::ProblemLoanTransaction.all.sort_by(&:project_id).group_by(&:project_id)
           render :index_by_loan
         end
       end
