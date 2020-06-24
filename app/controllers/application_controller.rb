017faf526d6bd11145f99edccc186859b33947ce
melody <meltheadorable@gmail.com>
2020-06-24 10:09:51 -0400

    10858: hound fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,6 @@ class ApplicationController < ActionController::Base
 
   before_action :set_raven_context
 
-
   def admin_controller?
     false
   end
@@ -75,8 +74,8 @@ class ApplicationController < ActionController::Base
     end
   end
 
-   def set_raven_context
-     Raven.user_context(id: current_user.id, email: current_user.email) # or anything else in session
-     Raven.extra_context(params: params.to_unsafe_h, url: request.url)
-   end
+  def set_raven_context
+    Raven.user_context(id: current_user.id, email: current_user.email) # or anything else in session
+    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+  end
 end
