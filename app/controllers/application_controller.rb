f4a87ec762527314df797253ce1a295d375d7647
melody <meltheadorable@gmail.com>
2020-06-25 15:57:23 -0400

    10858: fixes bugs when no user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -75,7 +75,7 @@ class ApplicationController < ActionController::Base
   end
 
   def set_raven_context
-    Raven.user_context(id: current_user.id, email: current_user.email) # or anything else in session
+    Raven.user_context(id: current_user&.id, email: current_user&.email) # or anything else in session
     Raven.extra_context(params: params.to_unsafe_h, url: request.url)
   end
 end
