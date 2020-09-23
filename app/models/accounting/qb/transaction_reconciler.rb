148bd3f8dfc95e9897cc412a997b5b9b95dae37b
DevneyHamilton <devneyhamilton@gmail.com>
2020-09-23 10:55:25 -0400

    11042: remove debug, hide check number when switch to repayment

--- app/models/accounting/qb/transaction_reconciler.rb
+++ app/models/accounting/qb/transaction_reconciler.rb
@@ -10,22 +10,13 @@ module Accounting
 
       # Creates or updates a transaction in QB based on a Transaction object created in Madeline.
       def reconcile(transaction)
-        Rails::Debug.logger.ap("RECONCILING . . . ")
-        Rails::Debug.logger.ap(transaction)
         return unless transaction.needs_qb_push?
-        if transaction.qb_id.present?
-          current_qb_je = service(transaction).find_by(:id, transaction.qb_id).first
-          Rails::Debug.logger.ap("doc num from qb: #{current_qb_je.doc_number}")
-        end
-
         qb_txn = builder.build_for_qb(transaction)
-        Rails::Debug.logger.ap("doc num from builder: #{qb_txn.doc_number}")
 
         # If the transaction already has a qb_id then it already exists in QB, so we should update it.
         # Otherwise we create it.
         raise StandardError, "DO NOT WRITE IN READ ONLY MODE" if @qb_division.qb_read_only
         qb_txn = transaction.qb_id ? service(transaction).update(qb_txn, sparse: true) : service(transaction).create(qb_txn)
-        Rails::Debug.logger.ap("doc num after qb update: #{qb_txn.doc_number}")
 
         transaction.set_qb_push_flag!(false)
 
