9e65b8924ee21e130bbb7f558dd8b86684c944ea
melody <meltheadorable@gmail.com>
2020-10-14 09:45:44 -0400

    draft: help system index

--- app/policies/documentation_policy.rb
+++ app/policies/documentation_policy.rb
@@ -1,4 +1,8 @@
 class DocumentationPolicy < ApplicationPolicy
+  def index?
+    division_admin(division: Division.root)
+  end
+
   def show?
     user.division.self_and_ancestors.include?(record.division) ||
       record.division.ancestors.include?(user.division)
