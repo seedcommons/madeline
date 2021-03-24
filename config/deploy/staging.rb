892cdd67d50ed24318723c010d389686088db697
DevneyHamilton <devneyhamilton@gmail.com>
2021-03-24 10:31:29 -0400

    Update cap files to always deploy to home/deploy/madeline

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,8 +7,6 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 server '104.131.40.32', user: 'deploy', roles: %w{app db web}
-set :deploy_to, -> { "/home/deploy/madeline/" }
-set :linked_files, %w{config/database.yml config/secrets.yml config/scout_apm.yml .rbenv-vars}
 
 # role-based syntax
 # ==================
