21ca8f49e7d3117d2c5a24765a74df144d9f710c
DevneyHamilton <devneyhamilton@gmail.com>
2021-04-09 16:49:22 -0400

    11608: redirect old loan path to nearest division page

--- app/controllers/public/loans_controller.rb
+++ app/controllers/public/loans_controller.rb
@@ -9,33 +9,20 @@ class Public::LoansController < Public::PublicController
 
   def division_for_site(site)
     case site.downcase
-      when "us"
-        "seed-commons"
-      when "nicaragua"
-        "la-base-nicaragua"
-      when "argentina"
-        "la-base-argentina"
-      else
-        "seed-commons"
-      end
+    when "us"
+      "seed-commons"
+    when "nicaragua"
+      "la-base-nicaragua"
+    when "argentina"
+      "la-base-argentina"
+    else
+      "seed-commons"
+    end
   end
 
   def show
     @loan = Loan.find(params[:id])
-    authorize @loan
-
-    @pictures = @loan.featured_pictures(limit: 5) # for slideshow
-    @other_loans = policy_scope(Loan.related_loans(@loan).active_or_completed.order('signing_date desc'))
-  end
-
-  def gallery
-    @loan = Loan.find(params[:id])
-    authorize @loan
-
-    @coop_media = @loan.coop_media(limit: 100, images_only: true).in_groups_of(4, false)
-    @loan_media = (
-      @loan.loan_media(limit: 100, images_only: true) +
-      @loan.log_media(limit: 100, images_only: true)
-    ).in_groups_of(4, false)
+    division_shortname = @loan.division.short_name
+    redirect_to controller: "divisions", action: "show", short_name: division_shortname
   end
 end
