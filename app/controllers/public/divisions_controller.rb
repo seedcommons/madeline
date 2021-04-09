0af5d156733d2019038a37bb3d68cdf6206b4f1b
DevneyHamilton <devneyhamilton@gmail.com>
2021-04-09 17:10:11 -0400

    11608: partial step twds showing all descendent public loans . . policy work needed still

--- app/controllers/public/divisions_controller.rb
+++ app/controllers/public/divisions_controller.rb
@@ -1,10 +1,21 @@
 class Public::DivisionsController < Public::PublicController
   layout 'public/main'
 
+  rescue_from Pundit::NotAuthorizedError, with: :find_public_parent
+
   def show
     @division = Division.find_by(short_name: params[:short_name])
     @selected_division = @division
     authorize @division
-    @loans = policy_scope(@division.loans.active_or_completed).page(params[:page]).per(5)
+    # assume this division is public . ..
+    divisions_to_show = @division.self_and_descendants.pluck(:id)
+    @loans = policy_scope(Loan.where(division: divisions_to_show ).active_or_completed).page(params[:page]).per(5)
+  end
+
+  private
+
+  def find_public_parent
+    # find nearest public parent
+    # redirect to taht page
   end
 end
