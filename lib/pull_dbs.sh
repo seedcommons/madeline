042707abf0bbe2c31b97d138b81711270c08b740
Tom Smyth <tom@gather.coop>
2021-04-19 11:12:32 -0400

    10486: Pull db script

---
+++ lib/pull_dbs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+dropdb madeline_migration
+createdb madeline_migration
+ssh madeline-prod "sudo -u postgres pg_dump madeline_system_production" | psql madeline_migration
+
+mysqladmin -u root -f drop tww_rails
+mysqladmin -u root create tww_rails
+echo "Copying legacy database from server"
+ssh ubuntu@52.206.58.37 "ssh sassafras@72.32.43.226 \"sudo mysqldump -u root base\"" | mysql -u root tww_rails
