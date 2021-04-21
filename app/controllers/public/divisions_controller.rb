7590e82a69c628dfd2f90a8a99a4b1a4032b7208
DevneyHamilton <devneyhamilton@gmail.com>
2021-04-21 17:11:27 -0400

    11478: status filter working, needs specs

--- app/controllers/public/divisions_controller.rb
+++ app/controllers/public/divisions_controller.rb
@@ -3,14 +3,19 @@ class Public::DivisionsController < Public::PublicController
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  ALLOWED_STATUSES = %w(active completed)
+
   def show
+    @params = { status: params[:status], pg: params[:pg], division: params[:division] }
     @division = Division.find_by(short_name: params[:short_name])
-    @selected_division = @division
+    @selected_division = @division # used for custom colors
     authorize @division
     # assume this division is public . ..
     # loan scope handles filtering loans based on whether their division is public, their status, and their public level value
     divisions_to_include = @division.self_and_descendants&.map(&:id)
-    @loans = LoanPolicy::Scope.new(:public, Loan.where(division: divisions_to_include)).resolve.page(params[:page]).per(5)
+    loans_requested = Loan.where(division: divisions_to_include)
+    loans_requested = loans_requested.status(params[:status]) if ALLOWED_STATUSES.include?(params[:status])
+    @loans = LoanPolicy::Scope.new(:public, loans_requested).resolve.page(params[:page]).per(5)
   end
 
   private
