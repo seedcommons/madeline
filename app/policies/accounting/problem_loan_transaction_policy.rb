91434b0d34aebd926a518f78adb3b14896a29fea
DevneyHamilton <devneyhamilton@gmail.com>
2021-04-30 19:55:07 -0400

    11760: spec covering only root admins see plts

--- app/policies/accounting/problem_loan_transaction_policy.rb
+++ app/policies/accounting/problem_loan_transaction_policy.rb
@@ -1,2 +1,24 @@
 class Accounting::ProblemLoanTransactionPolicy < ApplicationPolicy
+  # TODO if we move forward with division-based qb connections, PLTs should belong to
+  # a qb division as well as a loan (and PLTs will likely need more refactoring)
+  # For now, only root div admins
+  def index?
+    division_admin(division: Division.root)
+  end
+
+  def show?
+    division_admin(division: Division.root)
+  end
+
+  def create?
+    false
+  end
+
+  def update?
+    false
+  end
+
+  def destroy?
+    false
+  end
 end
