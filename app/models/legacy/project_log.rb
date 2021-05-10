a1bfbd2faffb1ee0325e7abf171e7054ed2f9501
Tom Smyth <tom@gather.coop>
2021-05-10 13:05:48 -0400

    10486: Fix log migration

--- app/models/legacy/project_log.rb
+++ app/models/legacy/project_log.rb
@@ -1,99 +1,103 @@
 # -*- SkipSchemaAnnotations
 module Legacy
-
   class ProjectLog < ApplicationRecord
     establish_connection :legacy
     include LegacyModel
 
+    DEFAULT_STEP_NAME = '[Paso para contener nota emigratada]'
+    NULLIFY_MEMBER_IDS = [0, 249, 280, 282]
+
+    def self.migratable
+      where("ProjectTable NOT IN ('BasicProjects', 'ProjectTemplates')")
+    end
 
     def project
-      project_class.find(self.project_id)
+      Loan.find(project_id)
     end
-
-
-    def migrated_loan
-      # beware the legacy db has inconsistent casing of the project table name
-      if project_table.downcase == 'loans'
-        result = ::Loan.find_by(id: project_id)
-        unless result
-          #JE todo: send warnings also to separate log
-          $stderr.puts "WARNING: ignoring ProjectLog[#{id}] pointing to invalid Loan ID: #{project_id}"
-        end
-      else
-        puts "ignoring non-loan project reference"
-        nil
-      end
-    end
-
 
     # note, legacy data includes 44 invalid references to a '0' member_id
     def agent_id
-      if member_id == 0
-        $stderr.puts "ProjectLog[#{id}] - mapping 0 MemberId ref to null"
-        nil
-      else
-        if Person.where(id: member_id).count > 0
-          member_id
+      return @agent_id if defined?(@agent_id)
+      @agent_id =
+        if NULLIFY_MEMBER_IDS.include?(member_id)
+          Migration.skip_log << ["ProjectLog", id, "Nullified agent_id b/c member #{member_id} not found"]
+          nil
         else
-          $stderr.puts "ProjectEvent[#{id}] - Person not found for MemberId: #{member_id}, mapping MemberId ref to null"
+          if (person = Person.find_by(legacy_id: member_id))
+            person.id
+          else
+            Migration.unexpected_errors << "Person not found for MemberId: #{member_id} on Log #{id}"
+            nil
+          end
         end
-      end
+    end
+
+    def date
+      return @date if defined?(@date)
+      return @date = self[:Date] unless self[:Date].nil?
+      @date = Loan.find_by(id: project_id).signing_date
+      Migration.skip_log << ["ProjectLog", id, "Date was NULL, set to Loan signing date of #{@date}"]
+      @date
     end
 
     def migration_data
-      # note the legacy db has 814 invalid references to either a 0 or 1 PasoID
-      if paso_id && paso_id > 1
-        project_step_id = paso_id
-        if ::ProjectStep.where(id: project_step_id).count == 0
-          #JE todo: send warnings also to separate log
-          puts "WARNING: ignoring ProjectLog[#{id}] pointing to invalid PasoID: #{paso_id}"
+      # note the legacy db has many invalid references to either a 0 or 1 PasoID
+      if paso_id && paso_id > 1 && (step = ProjectStep.find_by(legacy_id: paso_id))
+        project_step_id = step.id
+      else
+        loan = ::Loan.find_by(id: project_id)
+        if loan.nil?
+          Migration.unexpected_errors <<
+            "ProjectLog has no associated step and Loan #{project_id} doesn't exist"
           return nil
         end
-      else
-        loan = migrated_loan
-        return nil  unless loan  # skip invalid record
-        project_step_id = loan.default_step.id
-        puts "orphan steplog[#{id}] - using default loan step: #{paso_id}"
+        project_step_id = default_step(loan).id
       end
 
       data = {
-          id: self.id,
-          project_step_id: project_step_id,
-          agent_id: agent_id,
-          progress_metric_value: ::ProjectLog.progress_metric_option_set.value_for_migration_id(progress_metric),
-          date: date,
+        legacy_id: id,
+        project_step_id: project_step_id,
+        agent_id: agent_id,
+        progress_metric_value:
+          ::ProjectLog.progress_metric_option_set.value_for_migration_id(progress_metric),
+        date: date
       }
+      data[:summary] = explanation.strip if explanation&.strip.present?
+      data[:details] = detailed_explanation.strip if detailed_explanation&.strip.present?
+      data[:additional_notes] = additional_notes.strip if additional_notes&.strip.present?
+      data[:private_notes] = notas_privadas.strip if notas_privadas&.strip.present?
       data
     end
 
     def migrate
+      if project_table.downcase != 'loans'
+        Migration.skip_log << ["ProjectLog", id, "Not migrating b/c ProjectTable = #{project_table}"]
+        return
+      end
+      if project_id <= 0
+        Migration.skip_log << ["ProjectLog", id, "Not migrating b/c ProjectID = #{project_table}"]
+        return
+      end
       data = migration_data
       if data
-        # puts "#{data[:id]}: step id: #{data[:project_step_id]}"
-        ::ProjectLog.create(data)
+        pp(data)
+        log = ::ProjectLog.create!(data)
       end
     end
 
-
-    def self.migrate_all
-      puts "project logs: #{self.where('ProjectTable' => 'loans').count}"
-      # note, there is one more record with a wacked out date (2005-01-00)
-      self.where("ProjectTable = 'loans' and ProjectId > 0 and #{malformed_date_clause('Date')}").each &:migrate
-      ::ProjectLog.recalibrate_sequence(gap: 1000)
-
-      # note, there will be a notable number of unneeded translations from basic project logs, consider pruning somehow
-      puts "projectlog translations: #{ Legacy::Translation.where('RemoteTable' => 'ProjectLogs').count }"
-      Legacy::Translation.where("RemoteTable = 'ProjectLogs' and RemoteID > 0").each &:migrate
-      # note, translations table no longer needs recalibrating
+    # creates / reuses a default step when migrating ProjectLogs without a proper owning step
+    def default_step(loan)
+      puts "Creating default step for Loan #{loan.id} to house Log #{id}"
+      step = ProjectStep.create!(
+        parent: ProjectGroup.find_or_create_by!(project_id: project_id),
+        project: loan,
+        scheduled_start_date: date,
+        agent_id: agent_id,
+        summary: DEFAULT_STEP_NAME,
+        step_type_value: "milestone" # Old system didn't have 'checkin' type
+      )
+      pp(step)
+      step
     end
-
-    def self.purge_migrated
-      # note, not complete, but sufficient for purpose
-      puts "ProjectLog.delete_all"
-      ::ProjectLog.delete_all
-    end
-
-
   end
-
 end
