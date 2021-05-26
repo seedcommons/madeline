4ba1eb7abd1cad4ec25621b604fbba3349af7a82
DevneyHamilton <devneyhamilton@gmail.com>
2021-05-26 09:24:59 -0400

    11780: more settings flow specs

--- spec/features/admin/accounting/settings_flow_spec.rb
+++ spec/features/admin/accounting/settings_flow_spec.rb
@@ -48,14 +48,54 @@ feature 'settings flow', :accounting do
     end
   end
 
-  # describe "setting details" do
-  #   # accounts
-  #   # closed books date
-  #   # read-only
-  #   # submit form
-  # end
-  #
-  describe "disconnect" do
+  describe "setting details" do
+    let!(:accounts) { create_list(:account, 4) }
 
+    context "last full fetch of QB data was successful" do
+      before do
+        Task.create(job_type_value: :full_fetcher, activity_message_value: "x", job_class: "FullFetcherJob", job_succeeded_at: Time.current)
+      end
+
+      scenario do
+        visit "/admin/accounting/settings"
+        save_and_open_page
+        prin_acct_name = accounts.first.name
+        int_rcv_acct_name = accounts[1].name
+        int_inc_acct_name = accounts[2].name
+        select prin_acct_name, from: 'division[principal_account_id]'
+        select int_rcv_acct_name, from: 'division[interest_receivable_account_id]'
+        select int_inc_acct_name, from: 'division[interest_income_account_id]'
+        cbd = Time.zone.today.to_s
+        fill_in 'Closed Books Date', with: Time.zone.today.to_s
+        check 'division[qb_read_only]'
+        click_on "Save"
+        expect(page).to have_content 'Connected to '
+        expect(page).to have_select('division[principal_account_id]', selected: prin_acct_name)
+        expect(page).to have_select('division[interest_receivable_account_id]', selected: int_rcv_acct_name)
+        expect(page).to have_select('division[interest_income_account_id]', selected: int_inc_acct_name)
+        expect(page).to have_field('Closed Books Date', with: cbd)
+        expect(page).to have_checked_field('division[qb_read_only]')
+      end
+    end
+
+    context "connected but qb full fetch is pending" do
+      before do
+        Task.create(job_type_value: :full_fetcher, activity_message_value: "x", job_class: "FullFetcherJob")
+      end
+
+      scenario do
+        visit "/admin/accounting/settings"
+        expect(page).to have_content("QuickBooks data import pending.")
+      end
+    end
+  end
+
+  describe "disconnect" do
+    scenario do
+      visit "/admin/accounting/settings"
+      click_on 'Disconnect'
+      visit "/admin/accounting/settings"
+      expect(page).to have_content 'Not Connected'
+    end
   end
 end
