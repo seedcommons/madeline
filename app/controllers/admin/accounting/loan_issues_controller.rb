b97df133a00d1a406d87104ae6361d3097f37c17
Tom Smyth <tom@gather.coop>
2021-05-27 13:55:57 -0400

    11803: Remove unnecessary namespacing

--- app/controllers/admin/accounting/loan_issues_controller.rb
+++ app/controllers/admin/accounting/loan_issues_controller.rb
@@ -5,16 +5,16 @@ module Admin
         authorize :'accounting/loan_issue', :index?
         if params[:loan_id]
           @loan_id = params[:loan_id]
-          txn_ids = ::Accounting::LoanIssue.where(project_id: @loan_id).map(&:accounting_transaction_id)
-          @plts = ::Accounting::LoanIssue.where(accounting_transaction_id: txn_ids).group_by(&:txn_id)
+          txn_ids = LoanIssue.where(project_id: @loan_id).map(&:accounting_transaction_id)
+          @plts = LoanIssue.where(accounting_transaction_id: txn_ids).group_by(&:txn_id)
         else
-          @plts = ::Accounting::LoanIssue.all.sort_by(&:project_id).group_by(&:project_id)
+          @plts = LoanIssue.all.sort_by(&:project_id).group_by(&:project_id)
           render :index_by_loan
         end
       end
 
       def show
-        @plt = ::Accounting::LoanIssue.find(params[:id])
+        @plt = LoanIssue.find(params[:id])
         authorize @plt
       end
     end
