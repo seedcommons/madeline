28499e62b31bfa51e2d6d033ca5e9145c13355be
Tom Smyth <tom@gather.coop>
2021-05-28 16:53:56 -0400

    11803: Make issue pages work with no txn/loan

--- app/controllers/admin/accounting/loan_issues_controller.rb
+++ app/controllers/admin/accounting/loan_issues_controller.rb
@@ -5,10 +5,15 @@ module Admin
         authorize :'accounting/loan_issue', :index?
         if params[:loan_id]
           @loan_id = params[:loan_id]
-          txn_ids = ::Accounting::LoanIssue.where(project_id: @loan_id).map(&:accounting_transaction_id)
-          @issues = ::Accounting::LoanIssue.where(accounting_transaction_id: txn_ids).group_by(&:txn_id)
+          @issues = ::Accounting::LoanIssue.where(project_id: @loan_id)
+
+          # We include issues with no project ID for all projects.
+          @issues = @issues.or(::Accounting::LoanIssue.where(project_id: nil))
+
+          # Group by transactions so that we can do row spans.
+          @issues = @issues.group_by(&:txn_id)
         else
-          @issues = ::Accounting::LoanIssue.all.sort_by(&:project_id).group_by(&:project_id)
+          @issues = ::Accounting::LoanIssue.order(:project_id).group_by(&:project_id)
           render :index_by_loan
         end
       end
--- app/models/accounting/loan_issue.rb
+++ app/models/accounting/loan_issue.rb
@@ -4,14 +4,22 @@ module Accounting
     belongs_to :accounting_transaction, class_name: "Accounting::Transaction"
 
     delegate :id, :display_name, to: :loan, prefix: :loan
-    delegate :txn_date, :qb_id, :amount, :change_in_interest, :change_in_principal, :currency, :quickbooks_data, to: :accounting_transaction
-    delegate :id, :description, to: :accounting_transaction, prefix: :txn
+    delegate :txn_date, :qb_id, :amount, :change_in_interest, :change_in_principal, :currency, :quickbooks_data, to: :accounting_transaction, allow_nil: true
+    delegate :id, :description, to: :accounting_transaction, prefix: :txn, allow_nil: true
     delegate :division, to: :loan
+
+    def txn?
+      accounting_transaction.present?
+    end
+
+    def loan?
+      loan.present?
+    end
 
     def associated_loan_ids
       self.class.where(
         accounting_transaction_id: self.accounting_transaction_id
-      ).map(&:project_id)
+      ).map(&:project_id).compact
     end
   end
 end
