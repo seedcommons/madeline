67fc517d629c6b1a6400b6f130e8b1bc0e0b8e35
Tom Smyth <tom@gather.coop>
2021-05-28 16:13:51 -0400

    11803: Handle StandardError and add LoanIssue

--- app/jobs/quickbooks_job.rb
+++ app/jobs/quickbooks_job.rb
@@ -5,4 +5,11 @@ class QuickbooksJob < TaskJob
     Accounting::LoanIssue.create!(level: :error, message: :quickbooks_unavailable)
     record_failure_and_raise_error(error)
   end
+
+  # Override the default TaskJob handling of StandardError to create a LoanIssue
+  rescue_from(StandardError) do |error|
+    Accounting::LoanIssue.create!(level: :error, message: :system_error_notified)
+    task.set_activity_message("failed")
+    record_failure_and_raise_error(error)
+  end
 end
