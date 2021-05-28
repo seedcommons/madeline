df79dfaa8db4ddf880ed1aab0cb3e107a3c09185
Tom Smyth <tom@gather.coop>
2021-05-28 13:39:23 -0400

    11803: Reinstate namespacing :(

--- app/controllers/admin/accounting/loan_issues_controller.rb
+++ app/controllers/admin/accounting/loan_issues_controller.rb
@@ -5,16 +5,16 @@ module Admin
         authorize :'accounting/loan_issue', :index?
         if params[:loan_id]
           @loan_id = params[:loan_id]
-          txn_ids = LoanIssue.where(project_id: @loan_id).map(&:accounting_transaction_id)
-          @issues = LoanIssue.where(accounting_transaction_id: txn_ids).group_by(&:txn_id)
+          txn_ids = ::Accounting::LoanIssue.where(project_id: @loan_id).map(&:accounting_transaction_id)
+          @issues = ::Accounting::LoanIssue.where(accounting_transaction_id: txn_ids).group_by(&:txn_id)
         else
-          @issues = LoanIssue.all.sort_by(&:project_id).group_by(&:project_id)
+          @issues = ::Accounting::LoanIssue.all.sort_by(&:project_id).group_by(&:project_id)
           render :index_by_loan
         end
       end
 
       def show
-        @issue = LoanIssue.find(params[:id])
+        @issue = ::Accounting::LoanIssue.find(params[:id])
         authorize @issue
       end
     end
