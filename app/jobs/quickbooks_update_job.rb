c3866dea060cb18da52f0ddd9023351c7c0f08a8
Tom Smyth <tom@gather.coop>
2021-05-31 10:42:48 -0400

    11803: Change some errors to warnings and improve messages

--- app/jobs/quickbooks_update_job.rb
+++ app/jobs/quickbooks_update_job.rb
@@ -9,14 +9,17 @@ class QuickbooksUpdateJob < QuickbooksJob
       begin
         updater.update_loan(loan)
       rescue StandardError => e
-        # If QB is down, no point in continuing, so re-raise
-        raise if e.is_a?(Quickbooks::ServiceUnavailable)
+        if e.is_a?(Quickbooks::ServiceUnavailable)
+          # This is an error b/c we may have been in the middle of creating interest txns
+          Accounting::LoanIssue.create!(level: :error, loan: loan, message: :quickbooks_unavailable_recalc)
+          raise # If QB is down, no point in continuing, so re-raise
+        end
 
         # If there is an unhandled error updating an individual loan, we don't want the whole process to fail.
         # We let the user know that there was a system error and we've been notified.
         # But we don't expose the original error message to the user since it won't be intelligble
         # and could be a security issue.
-        Accounting::LoanIssue.create!(level: :error, loan: loan, message: :system_error_notified)
+        Accounting::LoanIssue.create!(level: :error, loan: loan, message: :system_error_recalc)
 
         # We want to receive a loud notification about an unhandled error.
         # If we find this is often generating a lot of similar errors
@@ -41,18 +44,13 @@ class QuickbooksUpdateJob < QuickbooksJob
     task.set_activity_message("completed")
   end
 
-  rescue_from(Accounting::QB::NotConnectedError) do |error|
-    Accounting::LoanIssue.create!(level: :error, message: :quickbooks_not_connected)
-    record_failure_and_raise_error(error, message: "error_quickbooks_not_connected")
-  end
-
   rescue_from(Accounting::QB::DataResetRequiredError) do |error|
-    Accounting::LoanIssue.create!(level: :error, message: :data_reset_required)
+    Accounting::LoanIssue.create!(level: :warning, message: :data_reset_required)
     record_failure_and_raise_error(error, message: "error_data_reset_required")
   end
 
   rescue_from(Accounting::QB::AccountsNotSelectedError) do |error|
-    Accounting::LoanIssue.create!(level: :error, message: :quickbooks_accounts_not_selected)
+    Accounting::LoanIssue.create!(level: :warning, message: :quickbooks_accounts_not_selected)
     record_failure_and_raise_error(error, message: "error_quickbooks_accounts_not_selected")
   end
 
