108d4db604caecb42c4d3280cb56336a29cafe56
DevneyHamilton <devneyhamilton@gmail.com>
2021-06-07 12:10:38 -0400

    11825: update schedule.rb to work with rbenv (see https://github.com/sassafrastech/madeline/pull/822)

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,25 +1,20 @@
-set :output, 'log/cron.log'
+set :output, "log/cron.log"
 
 job_type :rbenv_rake, %Q{ eval "$(rbenv init -)"; cd :path && bundle exec rake :task --silent :output }
 
-env :PATH, ENV['PATH']
-env :GEM_HOME, ENV['GEM_HOME']
+env :PATH, ENV["PATH"]
+env :GEM_HOME, ENV["GEM_HOME"]
 
-every 1.day, at: '3am' do
-  runner 'RecalculateAllLoanHealthJob.perform_later'
+every 1.day, at: "3am" do
+  runner "RecalculateAllLoanHealthJob.perform_later"
 end
 
-every 1.day, at: '2am' do
-  case @environment
-  when 'production'
-    rake "madeline:enqueue_update_loans_task"
-  when 'staging'
-    rbenv_rake "madeline:enqueue_update_loans_task"
-  end
+every 1.day, at: "2am" do
+  rbenv_rake "madeline:enqueue_update_loans_task"
 end
 
 # built in script job type is not updated for rails 4 and higher
-job_type :script, 'cd :path && RAILS_ENV=:environment bundle exec bin/:task :output'
+job_type :script, "cd :path && RAILS_ENV=:environment bundle exec bin/:task :output"
 every :reboot do
-  script 'sidekiq'
+  script "sidekiq"
 end
