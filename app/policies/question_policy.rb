1cac81d21e762b1a6ac6cbf35643222edc500a2c
Tom Smyth <tom@sassafras.coop>
2021-08-25 12:43:29 -0400

    11891: Simplify QuestionPolicy

--- app/policies/question_policy.rb
+++ app/policies/question_policy.rb
@@ -1,11 +1,23 @@
 class QuestionPolicy < ApplicationPolicy
+  def show?
+    # As a general rule, anyone is permitted to see any question in the system.
+    # There are controller/view-level filters when managing questions or filling out the business plan.
+    true
+  end
+
+  def create?
+    division_admin
+  end
+
+  def update?
+    division_admin
+  end
+
   def move?
     update?
   end
 
-  # Users can see questions from their division, any of its ancestors, and any of its descendents.
-  def show?
-    user.division.self_and_ancestors.include?(record.division) ||
-      record.division.ancestors.include?(user.division)
+  def destroy?
+    division_admin
   end
 end
