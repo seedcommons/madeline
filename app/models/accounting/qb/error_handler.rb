5b45470df98bbf800e4693c52c56827e0a884dd9
DevneyHamilton <devneyhamilton@gmail.com>
2021-09-10 17:06:18 -0400

    11840: refactored error handling

--- app/models/accounting/qb/error_handler.rb
+++ app/models/accounting/qb/error_handler.rb
@@ -1,8 +1,6 @@
-
 module Accounting
   module QB
     class ErrorHandler
-
       def initialize(loan, in_background_job: false)
         @loan = loan
         @in_background_job = in_background_job
@@ -15,64 +13,68 @@ module Accounting
       def handle_qb_errors
         begin
           yield
-        rescue Quickbooks::ServiceUnavailable => e
-          if @in_background_job
-            # This is an error b/c we may have been in the middle of creating interest txns
-            Accounting::SyncIssue.create!(level: :error, loan: @loan, message: :quickbooks_unavailable_recalc)
-            raise if @in_background_job # If QB is down, no point in continuing, so re-raise
-          else
-            Rails.logger.error e
-            error_msg = I18n.t("quickbooks.service_unavailable")
-          end
-        rescue Accounting::QB::DataResetRequiredError => e
+        rescue  Accounting::QB::DataResetRequiredError,
+                Accounting::QB::NotConnectedError,
+                Accounting::QB::AccountsNotSelectedError,
+                Quickbooks::ServiceUnavailable,
+                Quickbooks::MissingRealmError,
+                Quickbooks::AuthorizationFailure,
+                Quickbooks::ThrottleExceeded,
+                Quickbooks::TooManyRequests => e
           Rails.logger.error e
-          error_msg = I18n.t("quickbooks.data_reset_required", settings: settings_link).html_safe
-        rescue Accounting::QB::UnprocessableAccountError => e
-          Accounting::SyncIssue.create!(loan: e.loan, accounting_transaction: e.transaction,
-                                        message: :unprocessable_account, level: :error, custom_data: {})
+          ExceptionNotifier.notify_exception(e)
+          # TODO change message based on error
+          Accounting::SyncIssue.create!(level: :error, loan: nil, message: :quickbooks_unavailable_recalc)
+          if @in_background_job
+            # any of the above issues mean the bg job should stop
+            raise
+          else
+            # in controller, so return error message to be displayed
+            return error_msg = case e
+                               when Accounting::QB::DataResetRequiredError
+                                 I18n.t("quickbooks.data_reset_required", settings: settings_link).html_safe
+                               when Quickbooks::AuthorizationFailure, Quickbooks::MissingRealmError, Accounting::QB::NotConnectedError
+                                 I18n.t("quickbooks.authorization_failure", msg: e)
+                               when Quickbooks::ServiceUnavailable
+                                 I18n.t("quickbooks.service_unavailable", msg: e)
+                               else
+                                 I18n.t("quickbooks.misc", msg: e)
+                               end
+          end
+        rescue Accounting::QB::UnprocessableAccountError,
+               Accounting::QB::NegativeBalanceError => e
+          Rails.logger.error e
+          ExceptionNotifier.notify_exception(e)
+          message = case e
+                    when Accounting::QB::UnprocessableAccountError
+                      t("quickbooks.unprocessable_account", date: e.transaction.txn_date, qb_id: e.transaction.qb_id)
+                    when NegativeBalanceError
+                      t("quickbooks.negative_balance", amt: e.prev_balance)
+                    end
+          Accounting::SyncIssue.create!(level: :error, loan: @loan, accounting_transaction: e.transaction, message: message)
+          return message unless @in_background_job
         rescue Accounting::QB::IntuitRequestError => e
           txn = e.transaction
           is_matching_error = e.message.include?("matched to a downloaded transaction")
           intuit_error_type = is_matching_error ? :matched_transaction : :unknown_intuit_request_exception
+          changes_hash = {txn_changes: txn.previous_changes,
+                          line_item_changes: txn.line_items.map { |li| li.previous_changes }}
+          Rails.logger.error("Accounting::QB::IntuitRequestError #{intuit_error_type} #{e.message}. Txn date: #{txn.txn_date}. Txn qb id: #{txn.qb_id}. Changes: #{changes_hash}")
+          ExceptionNotifier.notify_exception(e)
           Accounting::SyncIssue.create!(loan: txn.loan,
                                         accounting_transaction: txn,
                                         message: intuit_error_type,
                                         level: :warning, # want to still see ledger
                                         custom_data: {date: txn.txn_date, qb_id: txn.qb_id})
-          changes_hash = {txn_changes: txn.previous_changes,
-                          line_item_changes: txn.line_items.map { |li| li.previous_changes }}
-          Rails.logger.error("Accounting::QB::IntuitRequestError #{intuit_error_type} #{e.message}. Txn date: #{txn.txn_date}. Txn qb id: #{txn.qb_id}. Changes: #{changes_hash}")
-          error_msg = I18n.t("quickbooks.#{intuit_error_type}",
-                        date: txn.txn_date,
-                        qb_id: txn.qb_id)
-        rescue Quickbooks::MissingRealmError,
-               Accounting::QB::NotConnectedError,
-               Quickbooks::AuthorizationFailure => e
-          Rails.logger.error e
-          error_msg = I18n.t("quickbooks.authorization_failure", settings: settings_link).html_safe
-        rescue Quickbooks::InvalidModelException,
-               Quickbooks::Forbidden,
-               Quickbooks::ThrottleExceeded,
-               Quickbooks::TooManyRequests,
-               Quickbooks::IntuitRequestException => e
+          # if in bg job, keep going; if in ctrlr
+          return I18n.t("quickbooks.misc", msg: e) unless @in_background_job
+        rescue StandardError => e
           Rails.logger.error e
           ExceptionNotifier.notify_exception(e)
-          error_msg =I18n.t("quickbooks.misc", msg: e)
-        rescue Accounting::QB::NegativeBalanceError => e
-          Rails.logger.error e
-          error_msg =I18n.t("quickbooks.negative_balance", amt: e.prev_balance)
-        rescue StandardError => e
-          # If there is an unhandled error updating an individual loan, we don't want the whole process to fail.
-          # We let the user know that there was a system error and we've been notified.
-          # But we don't expose the original error message to the user since it won't be intelligble
-          # and could be a security issue.
-          Accounting::SyncIssue.create!(level: :error, loan: @loan, message: :system_error_recalc)
-          # We want to receive a loud notification about an unhandled error.
-          # If we find this is often generating a lot of similar errors
-          # then we should really start using Sentry or some other service to group them.
-          notify_of_error(e, data: {context: "Unhandled error during loan update", loan_id: @loan.id})
+          Accounting::SyncIssue.create!(level: :error, loan: loan, message: :system_error_recalc)
+          # if in bg job, keep going; if in ctrlr
+          return I18n.t("quickbooks.misc", msg: e) unless @in_background_job
         end
-        error_msg # used in transactions controller but not in QuickbooksUpdateJob
       end
     end
   end
