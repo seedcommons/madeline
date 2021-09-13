f829a4a49b030994ade54c3d8303d638efc65505
Tom Smyth <tom@sassafras.coop>
2021-09-13 16:43:06 -0400

    11890: Pull question sets from correct division

--- app/controllers/concerns/questionnaire_renderable.rb
+++ app/controllers/concerns/questionnaire_renderable.rb
@@ -5,7 +5,7 @@ module QuestionnaireRenderable
   # Some users may set @question_set or @response_set before calling this helper,
   # in which case they will be left alone.
   def prep_questionnaire(json: true)
-    @question_sets = QuestionSet.find_for_division(selected_division_or_root)
+    @question_sets = QuestionSet.find_for_division(@loan.division)
     @question_set ||= params.key?(:qset) ? QuestionSet.find(params[:qset]) : @question_sets.first
     @response_set ||= ResponseSet.find_or_initialize_by(loan: @loan, question_set: @question_set)
     @root = LoanFilteredQuestion.new(@question_set.root_group_preloaded, loan: @loan)
