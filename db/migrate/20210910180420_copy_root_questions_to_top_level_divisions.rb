6e07d184aa2e5f34fcde0f65b678fc4ae3d02db6
Tom Smyth <tom@sassafras.coop>
2021-09-29 15:26:06 -0400

    Fix question copy migration ordering issue

--- db/migrate/20210910180420_copy_root_questions_to_top_level_divisions.rb
+++ db/migrate/20210910180420_copy_root_questions_to_top_level_divisions.rb
@@ -1,6 +1,6 @@
 class CopyRootQuestionsToTopLevelDivisions < ActiveRecord::Migration[6.1]
   ATTR_NAMES = %w(active created_at data_type display_in_summary has_embeddable_media
-                  internal_name override_associations)
+                  internal_name override_associations updated_at)
 
   def up
     # Ensure previous rename of internal_name/kind column is picked up.
@@ -17,8 +17,10 @@ class CopyRootQuestionsToTopLevelDivisions < ActiveRecord::Migration[6.1]
         end
         copy_qset_to_division(src_qset, dest_division)
       end
+      puts "Destroying original question set #{kind}"
       src_qset.destroy
     end
+    Question.rebuild! # Fix hierarchies table since we did manual inserts.
   end
 
   private
@@ -38,30 +40,37 @@ class CopyRootQuestionsToTopLevelDivisions < ActiveRecord::Migration[6.1]
   end
 
   def copy_group_to_division(src_group, dest_qset, dest_division, id_map)
+    position = -1
+    number = 0
     src_group.children.each do |src_question|
       if !question_is_in_ancestor_or_descendant_division?(src_question, dest_division)
         say "Skipping question or group #{src_question.id} since its division #{src_question.division_id} "\
           "is neither the current division, nor an ancestor or descendant of the current division."
         next
       end
-      dest_question = copy_question_to_qset(dest_qset, dest_division, src_question, id_map)
-      copy_question_translations(src_question, dest_question)
+      position += 1
+      number += 1 if src_question.active?
+      copy_question_to_qset(dest_qset, dest_division, src_question, id_map,
+                            position: position, number: number)
       copy_group_to_division(src_question, dest_qset, dest_division, id_map) if src_question.group?
     end
   end
 
-  def copy_question_to_qset(dest_qset, dest_division, src_question, id_map)
-    say "Copying question or group #{src_question.id} to division #{dest_division.id}"
-    attribs = build_question_attribs(dest_qset, dest_division, src_question, id_map)
-    dest_question = Question.create!(attribs)
-    copy_question_translations(src_question, dest_question)
-    copy_question_requirements(src_question, dest_question)
-    id_map[[dest_division.id, src_question.id]] = dest_question.id
-    dest_question
+  def copy_question_to_qset(dest_qset, dest_division, src_question, id_map, position:, number:)
+    say "Copying question or group #{src_question.id} (position #{src_question.position}) "\
+      "to division #{dest_division.id} (position #{position})"
+    attribs = build_question_attribs(dest_qset, dest_division, src_question, id_map,
+                                     position: position, number: number)
+    dest_question_id = Question.insert(attribs)[0]["id"]
+    copy_question_translations(src_question, dest_question_id)
+    copy_question_requirements(src_question, dest_question_id)
+    id_map[[dest_division.id, src_question.id]] = dest_question_id
   end
 
-  def build_question_attribs(dest_qset, dest_division, src_question, id_map)
+  def build_question_attribs(dest_qset, dest_division, src_question, id_map, position:, number:)
     attribs = src_question.attributes.slice(*ATTR_NAMES)
+    attribs["position"] = position
+    attribs["number"] = number if src_question.active?
     attribs["parent_id"] = id_map[[dest_division.id, src_question.parent_id]]
 
     if attribs["parent_id"].nil? && !src_question.parent_id.nil?
@@ -76,11 +85,11 @@ class CopyRootQuestionsToTopLevelDivisions < ActiveRecord::Migration[6.1]
     attribs
   end
 
-  def copy_question_translations(src_question, dest_question)
+  def copy_question_translations(src_question, dest_question_id)
     query = <<~SQL
       INSERT INTO translations (locale, text, translatable_type, translatable_attribute,
         translatable_id, allow_html, created_at, updated_at)
-        SELECT locale, text, translatable_type, translatable_attribute, #{dest_question.id},
+        SELECT locale, text, translatable_type, translatable_attribute, #{dest_question_id},
           allow_html, created_at, updated_at
           FROM translations
           WHERE translatable_type = 'Question' AND translatable_id = #{src_question.id}
@@ -88,10 +97,10 @@ class CopyRootQuestionsToTopLevelDivisions < ActiveRecord::Migration[6.1]
     execute(query)
   end
 
-  def copy_question_requirements(src_question, dest_question)
+  def copy_question_requirements(src_question, dest_question_id)
     query = <<~SQL
       INSERT INTO loan_question_requirements (amount, option_id, question_id)
-        SELECT amount, option_id, #{dest_question.id}
+        SELECT amount, option_id, #{dest_question_id}
         FROM loan_question_requirements
         WHERE question_id = #{src_question.id}
     SQL
