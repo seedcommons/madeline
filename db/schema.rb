eb56c15c171bc0681d2e63b46bed60e6db36bbf7
Tom Smyth <tom@sassafras.coop>
2021-10-07 10:38:40 -0400

    11994: Remove unused notify_on_new_logs field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_04_183116) do
+ActiveRecord::Schema.define(version: 2021_10_07_143811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -515,7 +515,6 @@ ActiveRecord::Schema.define(version: 2021_10_04_183116) do
     t.datetime "last_sign_in_at"
     t.inet "last_sign_in_ip"
     t.string "notification_source", default: "home_only", null: false
-    t.boolean "notify_on_new_logs", default: true
     t.integer "profile_id"
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
