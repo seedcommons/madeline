15596a48c4fd910d8b1b38ce0c8e17bc3de64cc2
Tom Smyth <tom@sassafras.coop>
2021-10-07 10:37:11 -0400

    11994: Respect notification_source in log notification job

--- app/jobs/log_notification_job.rb
+++ app/jobs/log_notification_job.rb
@@ -3,8 +3,14 @@ class LogNotificationJob < ApplicationJob
     division = log.division
     return unless division.notify_on_new_logs?
 
-    division.users.each do |user|
-      NotificationMailer.new_log(log, user).deliver_now
+    ancestors = division.ancestors.pluck(:id)
+    base_scope = Person.with_system_access.joins(:user).where.not(users: {notification_source: "none"})
+    own_division = base_scope.where(division: division)
+    subdivision = base_scope.where(division: ancestors).where(users: {notification_source: "home_and_sub"})
+    people = own_division.or(subdivision)
+
+    people.each do |person|
+      NotificationMailer.new_log(log, person.user).deliver_now
     end
   end
 end
--- spec/jobs/log_notification_job_spec.rb
+++ spec/jobs/log_notification_job_spec.rb
@@ -1,18 +1,28 @@
 require "rails_helper"
 
 describe LogNotificationJob do
-  context "has errors on some loans" do
-    let(:division1) { create(:division) }
-    let(:division2) { create(:division) }
-    let!(:person1) { create(:person, :with_member_access, :with_password, division: division1) }
-    let!(:person2) { create(:person, :with_member_access, :with_password, division: division1) }
-    let!(:decoy) { create(:person, :with_member_access, :with_password, division: division2) }
-    let(:log) { create(:project_log, division: division1) }
-    let(:deliveries) { ActionMailer::Base.deliveries }
+  let(:division) { create(:division, notify_on_new_logs: true) }
+  let(:subdivision) { create(:division, parent: division, notify_on_new_logs: true) }
+  let(:decoy_division) { create(:division) }
+  let(:notification_source) { "home_only" }
+  let!(:person1) { create(:person, :with_member_access, :with_password, division: division) }
+  let!(:person2) { create(:person, :with_member_access, :with_password, division: division) }
+  let!(:decoy1) { create(:person, :with_member_access, :with_password, division: decoy_division) }
+  let!(:decoy2) { create(:person, :with_member_access, :with_password, division: division) }
+  let!(:decoy3) { create(:person, :with_member_access, :with_password, division: division) }
+  let(:log) { create(:project_log, division: log_division) }
+  let(:deliveries) { ActionMailer::Base.deliveries }
+
+  before do
+    [person1, person2, decoy2].each { |p| p.user.update!(notification_source: notification_source) }
+    decoy2.update!(has_system_access: false)
+    decoy3.user.update!(notification_source: "none")
+  end
+
+  context "when log is on main division" do
+    let(:log_division) { division }
 
     context "with division having notifications on" do
-      let(:division1) { create(:division, notify_on_new_logs: true) }
-
       it "sends emails to correct users" do
         expect { described_class.perform_now(log) }.to change { deliveries.size }.by(2)
         expect(deliveries[-2..-1].flat_map(&:to)).to match_array([person1, person2].map(&:email))
@@ -20,10 +30,28 @@ describe LogNotificationJob do
     end
 
     context "with division having notifications off" do
-      let(:division1) { create(:division, notify_on_new_logs: false) }
+      let(:division) { create(:division, notify_on_new_logs: false) }
 
       it "sends no emails" do
         expect { described_class.perform_now(log) }.to change { deliveries.size }.by(0)
+      end
+    end
+  end
+
+  context "when log is on subdivision" do
+    let(:log_division) { subdivision }
+
+    context "when user has notification_source home_only" do
+      it "sends no emails" do
+        expect { described_class.perform_now(log) }.to change { deliveries.size }.by(0)
+      end
+    end
+
+    context "when user has notification_source home_and_sub" do
+      let(:notification_source) { "home_and_sub" }
+
+      it "sends emails" do
+        expect { described_class.perform_now(log) }.to change { deliveries.size }.by(2)
       end
     end
   end
