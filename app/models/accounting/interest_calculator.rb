29289918d3f5759df58360985e1a2844fba14314
DevneyHamilton <devneyhamilton@gmail.com>
2022-02-21 10:57:20 -0600

    12118: set interest to 0 always if prin balance is zero or negative

--- app/models/accounting/interest_calculator.rb
+++ app/models/accounting/interest_calculator.rb
@@ -216,8 +216,8 @@ module Accounting
 
     # Calculates the interest accrued between the date of the last transaction and the current one.
     def accrued_interest(prev_tx, txn)
-      #if prin balance is negative, interest is zero
-      if prev_tx  && prev_tx.principal_balance >= 0
+      #if prin balance is zero or negative, interest is zero
+      if prev_tx  && prev_tx.principal_balance > 0
         # Note: QB doesn't accept fractions of pennies
         (prev_tx.principal_balance * daily_rate * (txn.txn_date - prev_tx.txn_date)).round(2)
       else
