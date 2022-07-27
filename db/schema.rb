f55e1dce875dc6220e15cfed9d1831b46864dfa4
DevneyHamilton <devneyhamilton@gmail.com>
2022-07-27 10:35:06 -0500

    12193: migration changes to schema.rb removing internal name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2022_07_18_191508) do
 
   # These are extensions that must be enabled in order to support this database
@@ -440,7 +439,6 @@ ActiveRecord::Schema.define(version: 2022_07_18_191508) do
     t.integer "question_set_id"
     t.datetime "updated_at", null: false
     t.index "question_set_id, ((parent_id IS NULL))", name: "index_questions_on_question_set_id_parent_id_IS_NULL", unique: true, where: "(parent_id IS NULL)", comment: "Ensures max one root per question set"
-    t.index ["internal_name"], name: "index_questions_on_internal_name", unique: true
     t.index ["question_set_id"], name: "index_questions_on_question_set_id"
   end
 
